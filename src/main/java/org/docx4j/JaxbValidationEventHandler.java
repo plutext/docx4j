f08d3763f3b8cd8367269bf1d485948c41d61d01
Jason Harrop <jason@plutext.org>
2008-01-07 12:49:17 +0000

    Tried to make this less verbose, but JAXB emits a stack trace regardless.

--- src/main/java/org/docx4j/JaxbValidationEventHandler.java
+++ src/main/java/org/docx4j/JaxbValidationEventHandler.java
@@ -4,20 +4,37 @@ import javax.xml.bind.ValidationEvent;
 import javax.xml.bind.ValidationEventHandler;
 import javax.xml.bind.ValidationEventLocator;
 
+import org.apache.log4j.Logger;
+import org.docx4j.openpackaging.parts.WordprocessingML.StyleDefinitionsPart;
+
 public class JaxbValidationEventHandler implements 
 ValidationEventHandler{
     
+	private static Logger log = Logger.getLogger(JaxbValidationEventHandler.class);		
+	
+	
     public boolean handleEvent(ValidationEvent ve) {            
       if (ve.getSeverity()==ValidationEvent.FATAL_ERROR 
        || ve.getSeverity()==ValidationEvent.ERROR){
     	  
           ValidationEventLocator  locator = ve.getLocator();
+          
           //print message from validation event
-          System.out.println(printSeverity(ve) + "Message is " + ve.getMessage());
+          if (log.isDebugEnabled()) {
+        	  log.warn( printSeverity(ve) + ": " + ve.getMessage() );
+          } else {
+        	  /* These messages are long, for example:
+        	   * 
+        	   *    unexpected element (uri:"http://schemas.openxmlformats.org/wordprocessingml/2006/main", local:"style"). 
+        	   *    Expected elements are <{http://schemas.openxmlformats.org/wordprocessingml/2006/main}annotationRef>,
+        	   *    ....
+        	   *  
+        	   * so truncate it.
+        	   */
+        	  log.warn( printSeverity(ve) + ": " + ve.getMessage().substring(0, 120));        	  
+          }
           
-          //ve.getLinkedException().printStackTrace(); 
-          
-          if (ve.getLinkedException()!=null) {
+          if (ve.getLinkedException()!=null && log.isDebugEnabled() ) {
               ve.getLinkedException().printStackTrace();        	   
           }
           
@@ -26,11 +43,17 @@ ValidationEventHandler{
 //                locator.getColumnNumber() + 
 //                " at line number " + locator.getLineNumber());
        } else if (ve.getSeverity()==ve.WARNING) {
-           System.out.println(printSeverity(ve) + "Message is " + ve.getMessage());    	   
+    	   log.warn(printSeverity(ve) + "Message is " + ve.getMessage());    	   
        }
       // JAXB provider should attempt to continue its current operation. 
       // (Marshalling, Unmarshalling, Validating)
+      log.info("continuing (with possible element/attribute loss)");
        return true;
+       
+      // TODO: JAXB then prints a stack trace, from
+      // com.sun.xml.bind.v2.runtime.unmarshaller.UnmarshallingContext.handleEvent
+      // How to suppress this?
+      
      }
     
     public String printSeverity(ValidationEvent ve) {
@@ -38,7 +61,10 @@ ValidationEventHandler{
     	String errorLevel;
     	
     	switch (ve.getSeverity()) {
-    		case ValidationEvent.FATAL_ERROR: { errorLevel="FATAL_ERROR"; break; } 
+    		case ValidationEvent.FATAL_ERROR: { 
+              	// FATAL_ERROR is usually not actually fatal! 
+    			errorLevel="(non)FATAL_ERROR"; 
+    			break; } 
     		case ValidationEvent.ERROR: { errorLevel="ERROR"; break; }
     		case ValidationEvent.WARNING: { errorLevel="WARNING"; break; }
     		default: errorLevel = new Integer (ve.getSeverity()).toString() ;
