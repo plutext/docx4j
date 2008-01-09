828f1954678379958841a6509721252056ceb19a
Jason Harrop <jason@plutext.org>
2008-01-09 02:34:30 +0000

    Now a JaxbXmlPart.

--- docx4j/src/main/java/org/docx4j/openpackaging/parts/WordprocessingML/NumberingDefinitionsPart.java
+++ docx4j/src/main/java/org/docx4j/openpackaging/parts/WordprocessingML/NumberingDefinitionsPart.java
@@ -20,17 +20,18 @@
 package org.docx4j.openpackaging.parts.WordprocessingML;
 
 
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
-import org.docx4j.openpackaging.parts.Dom4jXmlPart;
-import org.docx4j.openpackaging.parts.Part;
 import org.docx4j.openpackaging.parts.PartName;
 import org.docx4j.openpackaging.parts.relationships.Namespaces;
 
-import org.dom4j.Document;
+import org.docx4j.openpackaging.parts.JaxbXmlPart;
 
 
 
-public final class NumberingDefinitionsPart extends Dom4jXmlPart {
+public final class NumberingDefinitionsPart extends JaxbXmlPart {
 	
 	public NumberingDefinitionsPart(PartName partName) throws InvalidFormatException {
 		super(partName);
@@ -43,9 +44,50 @@ public final class NumberingDefinitionsPart extends Dom4jXmlPart {
 		setRelationshipType(Namespaces.NUMBERING);
 	}
 
-	@Override
-	public Document getDocument() {
-		return document;
-	}	
+	
+    /**
+     * Unmarshal XML data from the specified InputStream and return the 
+     * resulting content tree.  Validation event location information may
+     * be incomplete when using this form of the unmarshal API.
+     *
+     * <p>
+     * Implements <a href="#unmarshalGlobal">Unmarshal Global Root Element</a>.
+     * 
+     * @param is the InputStream to unmarshal XML data from
+     * @return the newly created root object of the java content tree 
+     *
+     * @throws JAXBException 
+     *     If any unexpected errors occur while unmarshalling
+     */
+    public Object unmarshal( java.io.InputStream is ) throws JAXBException {
+    	
+		try {
+		    		    
+			Unmarshaller u = jc.createUnmarshaller();
+			
+			javax.xml.validation.SchemaFactory sf = 
+				javax.xml.validation.SchemaFactory.newInstance(
+				      javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+			javax.xml.validation.Schema schema = sf.newSchema(new java.io.File("/home/jharrop/workspace200711/docx4j-001/src/main/resources/wml-local-subset.xsd"));			
+
+			u.setSchema(schema);
+			
+			//u.setValidating( false );
+			
+			u.setEventHandler(new org.docx4j.JaxbValidationEventHandler());
+						
+			jaxbElement = u.unmarshal( is );
+			
+			System.out.println("\n\n" + this.getClass().getName() + " unmarshalled \n\n" );									
+
+		} catch (Exception e ) {
+			e.printStackTrace();
+		}
+    	
+		return jaxbElement;
+    	
+    }
+	
 	
 }
