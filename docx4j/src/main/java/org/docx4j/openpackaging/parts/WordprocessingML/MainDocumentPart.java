1ac7f5810126eccf7d58e9d593dd5f12e2fd87f6
Jason Harrop <jason@plutext.org>
2008-01-11 06:02:42 +0000

    No-arg constructor uses well-known conventional name for part.

--- docx4j/src/main/java/org/docx4j/openpackaging/parts/WordprocessingML/MainDocumentPart.java
+++ docx4j/src/main/java/org/docx4j/openpackaging/parts/WordprocessingML/MainDocumentPart.java
@@ -48,16 +48,21 @@ public class MainDocumentPart extends DocumentPart  {
 	
 	public MainDocumentPart(PartName partName) throws InvalidFormatException {
 		super(partName);
+		init();
+	}
+	public MainDocumentPart() throws InvalidFormatException {
+		super(new PartName("/word/document.xml"));
+		init();
+	}
 		
+	public void init() {
 		// Used if this Part is added to [Content_Types].xml 
 		setContentType(new  org.docx4j.openpackaging.contenttype.ContentType( 
 				org.docx4j.openpackaging.contenttype.ContentTypes.WORDPROCESSINGML_DOCUMENT));
 
 		// Used when this Part is added to a rels 
 		setRelationshipType(Namespaces.DOCUMENT);
-		
-	}
-		
+	}	
 	
     /**
      * Unmarshal XML data from the specified InputStream and return the 
