0f9acd5b8147591aa71000c1855263e595f7b0d5
Jason Harrop <jason@plutext.org>
2008-03-11 09:49:56 +0000

    Temporarily save embedded fonts to temporary fonts directory; read that directory when looking for fonts.

--- src/main/java/org/docx4j/openpackaging/parts/WordprocessingML/ObfuscatedFontPart.java
+++ src/main/java/org/docx4j/openpackaging/parts/WordprocessingML/ObfuscatedFontPart.java
@@ -19,19 +19,66 @@
 
 package org.docx4j.openpackaging.parts.WordprocessingML;
 
+import org.apache.fop.fonts.CustomFont;
+import org.apache.fop.fonts.FontLoader;
+import org.apache.fop.fonts.FontResolver;
+import org.apache.fop.fonts.FontSetup;
+import org.apache.fop.fonts.autodetect.FontFileFinder;
+import org.apache.fop.fonts.autodetect.FontInfoFinder;
 import org.apache.log4j.Logger;
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
 import org.docx4j.openpackaging.parts.PartName;
 import org.docx4j.openpackaging.parts.relationships.Namespaces;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ByteArrayOutputStream;
+import java.net.MalformedURLException;
 
 public class ObfuscatedFontPart extends BinaryPart {
 
 	private static Logger log = Logger.getLogger(ObfuscatedFontPart.class);		
 	
+    /** docx4j's user directory name */
+    private static final String DOCX4J_USER_DIR = ".docx4all"; 
+    	// docx4all already creates a dir; no point creating a second 
+
+    /** font cache file path */
+    private static final String TEMPORARY_FONT_DIR = "temporary embedded fonts";
+	
+    private static File tmpFontDir = null; 
+    
+    static {
+        File userHome = getUserHome();
+        if (userHome != null) {
+            File docx4jUserDir = new File(userHome, DOCX4J_USER_DIR);
+            docx4jUserDir.mkdir();
+            tmpFontDir = new File(docx4jUserDir, TEMPORARY_FONT_DIR);
+            tmpFontDir.mkdir();
+        }    	
+    }
+    
+    public static String getTemporaryEmbeddedFontsDir() {
+    	try {
+			return tmpFontDir.getCanonicalPath();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+    }
+    
+    private static File getUserHome() {
+        String s = System.getProperty("user.home");
+        if (s != null) {
+            File userDir = new File(s);
+            if (userDir.exists()) {
+                return userDir;
+            }
+        }
+        return null;
+    }
+
 	public ObfuscatedFontPart(PartName partName) throws InvalidFormatException {
 		super(partName);
 		init();				
@@ -76,7 +123,15 @@ public class ObfuscatedFontPart extends BinaryPart {
 	   }
 	}	
 	
-	public void deObfuscate(String fontKey) {
+	/**
+	 * deObfuscate this font, and save it using fontName
+	 * 
+	 * @param fontName - the name to save the font as. We
+	 * could read the font name from the deObfuscated data,
+	 * but FontLoader can't readily load from a byte array. 
+	 * @param fontKey
+	 */
+	public void deObfuscate(String fontName, String fontKey ) {
 		
 		// Convert the input stream into a byte array
 		byte[] fontData = null;
@@ -120,9 +175,12 @@ public class ObfuscatedFontPart extends BinaryPart {
 		}
 		
 		// Save the result
-		java.io.File f = new java.io.File("/tmp/fontA" + fontData.length +".ttf"); 
+		java.io.File f = new File(tmpFontDir, fontName +".ttf"); 
+		String path = null; 
+		
 		java.io.FileOutputStream fos = null; 
 		try {
+			path = f.getCanonicalPath();
 			fos = new java.io.FileOutputStream(f);
 			fos.write(fontData);
 			log.debug("wrote: " + fontData.length);
@@ -133,6 +191,42 @@ public class ObfuscatedFontPart extends BinaryPart {
 		} 
 		
 		log.info("Done!");
+		
+		// Save to "Temporary Font Files" directory.
+        FontResolver fontResolver = FontSetup.createMinimalFontResolver();
+
+		if (log.isDebugEnabled()) {
+	        CustomFont customFont = null;
+	        try {
+	        	log.debug("Loading from: " + path);
+	            customFont = FontLoader.loadFont("file:" + path, fontResolver);
+	        } catch (Exception e) {
+				e.printStackTrace();
+	        }
+	        if (customFont!=null) {
+	        	log.info("Successfully reloaded " + customFont.getFontName());
+	        	if (customFont.isEmbeddable()) {
+	        		log.debug("confirmed embeddable");
+	        	} else {
+	        		// Sanity check
+	        		log.error("this embedded font claims it is not embeddable!");        		
+	        	}
+	        }
+		}
+		
+		// Add this font to our known physical fonts  
+        try {
+			FontInfoFinder finder = new FontInfoFinder();
+			org.docx4j.fonts.Substituter.setupPhysicalFont(fontResolver, new java.net.URL("file:" + path), finder);
+			
+			// This needs to be done before populateFontMappings, 
+			// otherwise this font will be ignored, and references
+			// to it mapped to some substitute font!
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
