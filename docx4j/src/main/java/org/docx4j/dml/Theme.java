a229d372dbadb67c9d3ef2deb114bc3fb89f439d
Jason Harrop <jason@plutext.org>
2008-03-13 08:04:20 +0000

    Add license header.

--- docx4j/src/main/java/org/docx4j/dml/Theme.java
+++ docx4j/src/main/java/org/docx4j/dml/Theme.java
@@ -1,3 +1,24 @@
+/*
+ *  Copyright 2007, Plutext Pty Ltd.
+ *   
+ *  This file is part of docx4j.
+
+    docx4j is free software: you can use it, redistribute it and/or modify
+    it under the terms of version 3 of the GNU Affero General Public License 
+    as published by the Free Software Foundation.
+
+    If you need the right to use it under a different license, please
+    contact Plutext.
+
+    docx4j is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License   
+    along with docx4j.  If not, see <http://www.fsf.org/licensing/licenses/>.
+    
+ */
 
 package org.docx4j.dml;
 
