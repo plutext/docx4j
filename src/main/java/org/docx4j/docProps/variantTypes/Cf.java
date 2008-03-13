bf1c7078049996f885d3ab8adf93e82174c586d2
Jason Harrop <jason@plutext.org>
2008-03-13 01:12:18 +0000

    Object model for custom and extended document properties.

---
+++ src/main/java/org/docx4j/docProps/variantTypes/Cf.java
@@ -0,0 +1,106 @@
+/*
+ *  Copyright 2007, Plutext Pty Ltd.
+ *   
+ *  This file is part of docx4j.
+
+    docx4j is free software: you can use it, redistribute it and/or modify
+    it under the terms of version 3 of the GNU Affero General Public License 
+    as published by the Free Software Foundation.
+
+    If you need the right to use it under a different license, please
+    contact Plutext.
+
+    docx4j is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License   
+    along with docx4j.  If not, see <http://www.fsf.org/licensing/licenses/>.
+    
+ */
+
+package org.docx4j.docProps.variantTypes;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+
+/**
+ * <p>Java class for CT_Cf complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="CT_Cf">
+ *   &lt;simpleContent>
+ *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>base64Binary">
+ *       &lt;attribute name="format" type="{http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes}ST_Format" />
+ *     &lt;/extension>
+ *   &lt;/simpleContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "CT_Cf", propOrder = {
+    "value"
+})
+public class Cf {
+
+    @XmlValue
+    protected byte[] value;
+    @XmlAttribute
+    protected String format;
+
+    /**
+     * Gets the value of the value property.
+     * 
+     * @return
+     *     possible object is
+     *     byte[]
+     */
+    public byte[] getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the value of the value property.
+     * 
+     * @param value
+     *     allowed object is
+     *     byte[]
+     */
+    public void setValue(byte[] value) {
+        this.value = ((byte[]) value);
+    }
+
+    /**
+     * Gets the value of the format property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFormat() {
+        return format;
+    }
+
+    /**
+     * Sets the value of the format property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFormat(String value) {
+        this.format = value;
+    }
+
+}
