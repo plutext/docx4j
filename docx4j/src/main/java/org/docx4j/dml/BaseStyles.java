b9b9478965ab5384cde7e5c1b3577dc920e61649
Jason Harrop <jason@plutext.org>
2008-03-17 09:55:57 +0000

    We were discarding everything in the theme part except fontScheme.  Now we round trip the other bits using <xs:any processContents="skip" maxOccurs="unbounded" minOccurs="0" />

--- docx4j/src/main/java/org/docx4j/dml/BaseStyles.java
+++ docx4j/src/main/java/org/docx4j/dml/BaseStyles.java
@@ -22,11 +22,15 @@
 
 package org.docx4j.dml;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+import org.w3c.dom.Element;
 
 
 /**
@@ -39,6 +43,17 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
+ *         &lt;element name="clrScheme">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;any/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
  *         &lt;element name="fontScheme">
  *           &lt;complexType>
  *             &lt;complexContent>
@@ -46,8 +61,41 @@ import javax.xml.bind.annotation.XmlType;
  *                 &lt;sequence>
  *                   &lt;element name="majorFont" type="{http://schemas.openxmlformats.org/drawingml/2006/main}CT_FontCollection"/>
  *                   &lt;element name="minorFont" type="{http://schemas.openxmlformats.org/drawingml/2006/main}CT_FontCollection"/>
+ *                   &lt;element name="extLst" minOccurs="0">
+ *                     &lt;complexType>
+ *                       &lt;complexContent>
+ *                         &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                           &lt;sequence>
+ *                             &lt;any/>
+ *                           &lt;/sequence>
+ *                         &lt;/restriction>
+ *                       &lt;/complexContent>
+ *                     &lt;/complexType>
+ *                   &lt;/element>
  *                 &lt;/sequence>
  *                 &lt;attribute name="name" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *         &lt;element name="fmtScheme">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;any/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *         &lt;element name="extLst" minOccurs="0">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;any/>
+ *                 &lt;/sequence>
  *               &lt;/restriction>
  *             &lt;/complexContent>
  *           &lt;/complexType>
@@ -62,12 +110,44 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "CT_BaseStyles", propOrder = {
-    "fontScheme"
+    "clrScheme",
+    "fontScheme",
+    "fmtScheme",
+    "extLst"
 })
 public class BaseStyles {
 
     @XmlElement(required = true)
+    protected BaseStyles.ClrScheme clrScheme;
+    @XmlElement(required = true)
     protected BaseStyles.FontScheme fontScheme;
+    @XmlElement(required = true)
+    protected BaseStyles.FmtScheme fmtScheme;
+    protected BaseStyles.ExtLst extLst;
+
+    /**
+     * Gets the value of the clrScheme property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BaseStyles.ClrScheme }
+     *     
+     */
+    public BaseStyles.ClrScheme getClrScheme() {
+        return clrScheme;
+    }
+
+    /**
+     * Sets the value of the clrScheme property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BaseStyles.ClrScheme }
+     *     
+     */
+    public void setClrScheme(BaseStyles.ClrScheme value) {
+        this.clrScheme = value;
+    }
 
     /**
      * Gets the value of the fontScheme property.
@@ -93,6 +173,234 @@ public class BaseStyles {
         this.fontScheme = value;
     }
 
+    /**
+     * Gets the value of the fmtScheme property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BaseStyles.FmtScheme }
+     *     
+     */
+    public BaseStyles.FmtScheme getFmtScheme() {
+        return fmtScheme;
+    }
+
+    /**
+     * Sets the value of the fmtScheme property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BaseStyles.FmtScheme }
+     *     
+     */
+    public void setFmtScheme(BaseStyles.FmtScheme value) {
+        this.fmtScheme = value;
+    }
+
+    /**
+     * Gets the value of the extLst property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BaseStyles.ExtLst }
+     *     
+     */
+    public BaseStyles.ExtLst getExtLst() {
+        return extLst;
+    }
+
+    /**
+     * Sets the value of the extLst property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BaseStyles.ExtLst }
+     *     
+     */
+    public void setExtLst(BaseStyles.ExtLst value) {
+        this.extLst = value;
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;any/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "any"
+    })
+    public static class ClrScheme {
+
+        @XmlAnyElement
+        protected List<Element> any;
+
+        /**
+         * Gets the value of the any property.
+         * 
+         * <p>
+         * This accessor method returns a reference to the live list,
+         * not a snapshot. Therefore any modification you make to the
+         * returned list will be present inside the JAXB object.
+         * This is why there is not a <CODE>set</CODE> method for the any property.
+         * 
+         * <p>
+         * For example, to add a new item, do as follows:
+         * <pre>
+         *    getAny().add(newItem);
+         * </pre>
+         * 
+         * 
+         * <p>
+         * Objects of the following type(s) are allowed in the list
+         * {@link Element }
+         * 
+         * 
+         */
+        public List<Element> getAny() {
+            if (any == null) {
+                any = new ArrayList<Element>();
+            }
+            return this.any;
+        }
+
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;any/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "any"
+    })
+    public static class ExtLst {
+
+        @XmlAnyElement
+        protected List<Element> any;
+
+        /**
+         * Gets the value of the any property.
+         * 
+         * <p>
+         * This accessor method returns a reference to the live list,
+         * not a snapshot. Therefore any modification you make to the
+         * returned list will be present inside the JAXB object.
+         * This is why there is not a <CODE>set</CODE> method for the any property.
+         * 
+         * <p>
+         * For example, to add a new item, do as follows:
+         * <pre>
+         *    getAny().add(newItem);
+         * </pre>
+         * 
+         * 
+         * <p>
+         * Objects of the following type(s) are allowed in the list
+         * {@link Element }
+         * 
+         * 
+         */
+        public List<Element> getAny() {
+            if (any == null) {
+                any = new ArrayList<Element>();
+            }
+            return this.any;
+        }
+
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;any/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "any"
+    })
+    public static class FmtScheme {
+
+        @XmlAnyElement
+        protected List<Element> any;
+
+        /**
+         * Gets the value of the any property.
+         * 
+         * <p>
+         * This accessor method returns a reference to the live list,
+         * not a snapshot. Therefore any modification you make to the
+         * returned list will be present inside the JAXB object.
+         * This is why there is not a <CODE>set</CODE> method for the any property.
+         * 
+         * <p>
+         * For example, to add a new item, do as follows:
+         * <pre>
+         *    getAny().add(newItem);
+         * </pre>
+         * 
+         * 
+         * <p>
+         * Objects of the following type(s) are allowed in the list
+         * {@link Element }
+         * 
+         * 
+         */
+        public List<Element> getAny() {
+            if (any == null) {
+                any = new ArrayList<Element>();
+            }
+            return this.any;
+        }
+
+    }
+
 
     /**
      * <p>Java class for anonymous complex type.
@@ -106,6 +414,17 @@ public class BaseStyles {
      *       &lt;sequence>
      *         &lt;element name="majorFont" type="{http://schemas.openxmlformats.org/drawingml/2006/main}CT_FontCollection"/>
      *         &lt;element name="minorFont" type="{http://schemas.openxmlformats.org/drawingml/2006/main}CT_FontCollection"/>
+     *         &lt;element name="extLst" minOccurs="0">
+     *           &lt;complexType>
+     *             &lt;complexContent>
+     *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *                 &lt;sequence>
+     *                   &lt;any/>
+     *                 &lt;/sequence>
+     *               &lt;/restriction>
+     *             &lt;/complexContent>
+     *           &lt;/complexType>
+     *         &lt;/element>
      *       &lt;/sequence>
      *       &lt;attribute name="name" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
      *     &lt;/restriction>
@@ -118,7 +437,8 @@ public class BaseStyles {
     @XmlAccessorType(XmlAccessType.FIELD)
     @XmlType(name = "", propOrder = {
         "majorFont",
-        "minorFont"
+        "minorFont",
+        "extLst"
     })
     public static class FontScheme {
 
@@ -126,6 +446,7 @@ public class BaseStyles {
         protected FontCollection majorFont;
         @XmlElement(required = true)
         protected FontCollection minorFont;
+        protected BaseStyles.FontScheme.ExtLst extLst;
         @XmlAttribute(required = true)
         protected String name;
 
@@ -178,6 +499,30 @@ public class BaseStyles {
         }
 
         /**
+         * Gets the value of the extLst property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link BaseStyles.FontScheme.ExtLst }
+         *     
+         */
+        public BaseStyles.FontScheme.ExtLst getExtLst() {
+            return extLst;
+        }
+
+        /**
+         * Sets the value of the extLst property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link BaseStyles.FontScheme.ExtLst }
+         *     
+         */
+        public void setExtLst(BaseStyles.FontScheme.ExtLst value) {
+            this.extLst = value;
+        }
+
+        /**
          * Gets the value of the name property.
          * 
          * @return
@@ -199,6 +544,66 @@ public class BaseStyles {
          */
         public void setName(String value) {
             this.name = value;
+        }
+
+
+        /**
+         * <p>Java class for anonymous complex type.
+         * 
+         * <p>The following schema fragment specifies the expected content contained within this class.
+         * 
+         * <pre>
+         * &lt;complexType>
+         *   &lt;complexContent>
+         *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+         *       &lt;sequence>
+         *         &lt;any/>
+         *       &lt;/sequence>
+         *     &lt;/restriction>
+         *   &lt;/complexContent>
+         * &lt;/complexType>
+         * </pre>
+         * 
+         * 
+         */
+        @XmlAccessorType(XmlAccessType.FIELD)
+        @XmlType(name = "", propOrder = {
+            "any"
+        })
+        public static class ExtLst {
+
+            @XmlAnyElement
+            protected List<Element> any;
+
+            /**
+             * Gets the value of the any property.
+             * 
+             * <p>
+             * This accessor method returns a reference to the live list,
+             * not a snapshot. Therefore any modification you make to the
+             * returned list will be present inside the JAXB object.
+             * This is why there is not a <CODE>set</CODE> method for the any property.
+             * 
+             * <p>
+             * For example, to add a new item, do as follows:
+             * <pre>
+             *    getAny().add(newItem);
+             * </pre>
+             * 
+             * 
+             * <p>
+             * Objects of the following type(s) are allowed in the list
+             * {@link Element }
+             * 
+             * 
+             */
+            public List<Element> getAny() {
+                if (any == null) {
+                    any = new ArrayList<Element>();
+                }
+                return this.any;
+            }
+
         }
 
     }
--- docx4j/src/main/java/org/docx4j/dml/Theme.java
+++ docx4j/src/main/java/org/docx4j/dml/Theme.java
@@ -22,12 +22,16 @@
 
 package org.docx4j.dml;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
+import org.w3c.dom.Element;
 
 
 /**
@@ -41,6 +45,50 @@ import javax.xml.bind.annotation.XmlType;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
  *         &lt;element name="themeElements" type="{http://schemas.openxmlformats.org/drawingml/2006/main}CT_BaseStyles"/>
+ *         &lt;element name="objectDefaults" minOccurs="0">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;any/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *         &lt;element name="extraClrSchemeLst" minOccurs="0">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;any/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *         &lt;element name="custClrLst" minOccurs="0">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;any/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *         &lt;element name="extLst" minOccurs="0">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;any/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
  *       &lt;/sequence>
  *       &lt;attribute name="name" type="{http://www.w3.org/2001/XMLSchema}string" default="" />
  *     &lt;/restriction>
@@ -52,13 +100,21 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-    "themeElements"
+    "themeElements",
+    "objectDefaults",
+    "extraClrSchemeLst",
+    "custClrLst",
+    "extLst"
 })
 @XmlRootElement(name = "theme")
 public class Theme {
 
     @XmlElement(required = true)
     protected BaseStyles themeElements;
+    protected Theme.ObjectDefaults objectDefaults;
+    protected Theme.ExtraClrSchemeLst extraClrSchemeLst;
+    protected Theme.CustClrLst custClrLst;
+    protected Theme.ExtLst extLst;
     @XmlAttribute
     protected String name;
 
@@ -87,6 +143,102 @@ public class Theme {
     }
 
     /**
+     * Gets the value of the objectDefaults property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Theme.ObjectDefaults }
+     *     
+     */
+    public Theme.ObjectDefaults getObjectDefaults() {
+        return objectDefaults;
+    }
+
+    /**
+     * Sets the value of the objectDefaults property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Theme.ObjectDefaults }
+     *     
+     */
+    public void setObjectDefaults(Theme.ObjectDefaults value) {
+        this.objectDefaults = value;
+    }
+
+    /**
+     * Gets the value of the extraClrSchemeLst property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Theme.ExtraClrSchemeLst }
+     *     
+     */
+    public Theme.ExtraClrSchemeLst getExtraClrSchemeLst() {
+        return extraClrSchemeLst;
+    }
+
+    /**
+     * Sets the value of the extraClrSchemeLst property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Theme.ExtraClrSchemeLst }
+     *     
+     */
+    public void setExtraClrSchemeLst(Theme.ExtraClrSchemeLst value) {
+        this.extraClrSchemeLst = value;
+    }
+
+    /**
+     * Gets the value of the custClrLst property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Theme.CustClrLst }
+     *     
+     */
+    public Theme.CustClrLst getCustClrLst() {
+        return custClrLst;
+    }
+
+    /**
+     * Sets the value of the custClrLst property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Theme.CustClrLst }
+     *     
+     */
+    public void setCustClrLst(Theme.CustClrLst value) {
+        this.custClrLst = value;
+    }
+
+    /**
+     * Gets the value of the extLst property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Theme.ExtLst }
+     *     
+     */
+    public Theme.ExtLst getExtLst() {
+        return extLst;
+    }
+
+    /**
+     * Sets the value of the extLst property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Theme.ExtLst }
+     *     
+     */
+    public void setExtLst(Theme.ExtLst value) {
+        this.extLst = value;
+    }
+
+    /**
      * Gets the value of the name property.
      * 
      * @return
@@ -112,6 +264,246 @@ public class Theme {
      */
     public void setName(String value) {
         this.name = value;
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;any/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "any"
+    })
+    public static class CustClrLst {
+
+        @XmlAnyElement
+        protected List<Element> any;
+
+        /**
+         * Gets the value of the any property.
+         * 
+         * <p>
+         * This accessor method returns a reference to the live list,
+         * not a snapshot. Therefore any modification you make to the
+         * returned list will be present inside the JAXB object.
+         * This is why there is not a <CODE>set</CODE> method for the any property.
+         * 
+         * <p>
+         * For example, to add a new item, do as follows:
+         * <pre>
+         *    getAny().add(newItem);
+         * </pre>
+         * 
+         * 
+         * <p>
+         * Objects of the following type(s) are allowed in the list
+         * {@link Element }
+         * 
+         * 
+         */
+        public List<Element> getAny() {
+            if (any == null) {
+                any = new ArrayList<Element>();
+            }
+            return this.any;
+        }
+
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;any/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "any"
+    })
+    public static class ExtLst {
+
+        @XmlAnyElement
+        protected List<Element> any;
+
+        /**
+         * Gets the value of the any property.
+         * 
+         * <p>
+         * This accessor method returns a reference to the live list,
+         * not a snapshot. Therefore any modification you make to the
+         * returned list will be present inside the JAXB object.
+         * This is why there is not a <CODE>set</CODE> method for the any property.
+         * 
+         * <p>
+         * For example, to add a new item, do as follows:
+         * <pre>
+         *    getAny().add(newItem);
+         * </pre>
+         * 
+         * 
+         * <p>
+         * Objects of the following type(s) are allowed in the list
+         * {@link Element }
+         * 
+         * 
+         */
+        public List<Element> getAny() {
+            if (any == null) {
+                any = new ArrayList<Element>();
+            }
+            return this.any;
+        }
+
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;any/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "any"
+    })
+    public static class ExtraClrSchemeLst {
+
+        @XmlAnyElement
+        protected List<Element> any;
+
+        /**
+         * Gets the value of the any property.
+         * 
+         * <p>
+         * This accessor method returns a reference to the live list,
+         * not a snapshot. Therefore any modification you make to the
+         * returned list will be present inside the JAXB object.
+         * This is why there is not a <CODE>set</CODE> method for the any property.
+         * 
+         * <p>
+         * For example, to add a new item, do as follows:
+         * <pre>
+         *    getAny().add(newItem);
+         * </pre>
+         * 
+         * 
+         * <p>
+         * Objects of the following type(s) are allowed in the list
+         * {@link Element }
+         * 
+         * 
+         */
+        public List<Element> getAny() {
+            if (any == null) {
+                any = new ArrayList<Element>();
+            }
+            return this.any;
+        }
+
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;any/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "any"
+    })
+    public static class ObjectDefaults {
+
+        @XmlAnyElement
+        protected List<Element> any;
+
+        /**
+         * Gets the value of the any property.
+         * 
+         * <p>
+         * This accessor method returns a reference to the live list,
+         * not a snapshot. Therefore any modification you make to the
+         * returned list will be present inside the JAXB object.
+         * This is why there is not a <CODE>set</CODE> method for the any property.
+         * 
+         * <p>
+         * For example, to add a new item, do as follows:
+         * <pre>
+         *    getAny().add(newItem);
+         * </pre>
+         * 
+         * 
+         * <p>
+         * Objects of the following type(s) are allowed in the list
+         * {@link Element }
+         * 
+         * 
+         */
+        public List<Element> getAny() {
+            if (any == null) {
+                any = new ArrayList<Element>();
+            }
+            return this.any;
+        }
+
     }
 
 }
