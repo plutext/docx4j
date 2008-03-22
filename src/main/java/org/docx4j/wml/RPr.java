c1140ab6b0ae535822754e0bc641df334811e789
Jason Harrop <jason@plutext.org>
2008-03-22 03:09:13 +0000

    Add @XmlRootElement annotations

--- src/main/java/org/docx4j/wml/RPr.java
+++ src/main/java/org/docx4j/wml/RPr.java
@@ -23,6 +23,7 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 import org.jvnet.jaxb2_commons.ppp.Child;
@@ -67,6 +68,7 @@ import org.jvnet.jaxb2_commons.ppp.Child;
     "u",
     "vertAlign"
 })
+@XmlRootElement(name = "rPr")
 public class RPr
     implements Child
 {
