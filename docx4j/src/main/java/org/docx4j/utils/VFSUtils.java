482981919890e429bd4ce20dbc491ab1a2ad2649
Jason Harrop <jason@plutext.org>
2008-04-09 07:37:06 +0000

    Change license to Apache v2

--- docx4j/src/main/java/org/docx4j/utils/VFSUtils.java
+++ docx4j/src/main/java/org/docx4j/utils/VFSUtils.java
@@ -1,20 +1,21 @@
 /*
- *  Copyright 2007, Plutext Pty Ltd.
+ *  Copyright 2007-2008, Plutext Pty Ltd.
  *   
- *  This file is part of Docx4all.
+ *  This file is part of docx4j.
 
-    Docx4all is free software: you can redistribute it and/or modify
-    it under the terms of version 3 of the GNU General Public License 
-    as published by the Free Software Foundation.
+    docx4j is licensed under the Apache License, Version 2.0 (the "License"); 
+    you may not use this file except in compliance with the License. 
 
-    Docx4all is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    You may obtain a copy of the License at 
 
-    You should have received a copy of the GNU General Public License   
-    along with Docx4all.  If not, see <http://www.gnu.org/licenses/>.
-    
+        http://www.apache.org/licenses/LICENSE-2.0 
+
+    Unless required by applicable law or agreed to in writing, software 
+    distributed under the License is distributed on an "AS IS" BASIS, 
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+    See the License for the specific language governing permissions and 
+    limitations under the License.
+
  */
 
 package org.docx4j.utils;
