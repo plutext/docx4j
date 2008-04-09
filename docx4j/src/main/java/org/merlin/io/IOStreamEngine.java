8dbad07bfdb0a7e41692009f0fcfe94dc2238d20
Jason Harrop <jason@plutext.org>
2008-04-09 14:28:07 +0000

    Replaced license header (was GPL, now Apache v2) following Merlin Hughes' <merlin@merlin.org> release of it under the Apache license v2 on 9 April 2008.

--- docx4j/src/main/java/org/merlin/io/IOStreamEngine.java
+++ docx4j/src/main/java/org/merlin/io/IOStreamEngine.java
@@ -8,10 +8,20 @@ import java.io.*;
  *
  * @author Copyright (c) 2002 Merlin Hughes <merlin@merlin.org>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * org.merlin.io was released by its author under the Apache License, 
+ * Version 2.0 (the "License") on 9 April 2008; 
+ * you may not use this file except in compliance with the License. 
+ *
+ * You may obtain a copy of the License at 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
  */
 public class IOStreamEngine implements OutputEngine {
   private static final int DEFAULT_BUFFER_SIZE = 8192;
