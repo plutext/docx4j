cfd5b63425574722a4f1b5166674afe32dc11aba
Jason Harrop <jason@plutext.org>
2008-04-10 14:56:50 +0000

    @XmlRootElement SdtPr - Id handling

--- src/main/java/org/docx4j/wml/SdtBlock.java
+++ src/main/java/org/docx4j/wml/SdtBlock.java
@@ -24,6 +24,7 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
@@ -58,6 +59,7 @@ import org.jvnet.jaxb2_commons.ppp.Child;
     "sdtEndPr",
     "sdtContent"
 })
+@XmlRootElement(name = "sdt")
 public class SdtBlock
     implements Child
 {
