2535914be431af138fb996f5e828e0854dc72fd9
Jason Harrop <jason@plutext.org>
2008-06-16 05:31:21 +0000

    Add JAXB objects representing w:ins/w:r, w:del/w:r, and w:r/w:delText. The other commits are just the usual xjc generate shuffle.

--- src/main/java/org/docx4j/wml/CTMarkup.java
+++ src/main/java/org/docx4j/wml/CTMarkup.java
@@ -50,8 +50,7 @@ import org.jvnet.jaxb2_commons.ppp.Child;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "CT_Markup")
-public class CTMarkup
-    implements Child
+public class CTMarkup implements Child
 {
 
     @XmlAttribute(namespace = "http://schemas.openxmlformats.org/wordprocessingml/2006/main", required = true)
