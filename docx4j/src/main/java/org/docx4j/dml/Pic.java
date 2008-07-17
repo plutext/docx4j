1860c1207451cf2211f48cfcdd6d97b658190ae9
Jason Harrop <jason@plutext.org>
2008-07-17 21:11:19 +0000

    Change content model for <a:graphicData> from any to <pic:pic>

---
+++ docx4j/src/main/java/org/docx4j/dml/Pic.java
@@ -0,0 +1,151 @@
+
+package org.docx4j.dml;
+
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+import org.jvnet.jaxb2_commons.ppp.Child;
+
+
+/**
+ * <p>Java class for CT_Picture complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="CT_Picture">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="nvPicPr" type="{http://schemas.openxmlformats.org/drawingml/2006/picture}CT_PictureNonVisual"/>
+ *         &lt;element name="blipFill" type="{http://schemas.openxmlformats.org/drawingml/2006/main}CT_BlipFillProperties"/>
+ *         &lt;element name="spPr" type="{http://schemas.openxmlformats.org/drawingml/2006/main}CT_ShapeProperties"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "CT_Picture", namespace = "http://schemas.openxmlformats.org/drawingml/2006/picture", propOrder = {
+    "nvPicPr",
+    "blipFill",
+    "spPr"
+})
+public class Pic
+    implements Child
+{
+
+    @XmlElement(required = true)
+    protected CTPictureNonVisual nvPicPr;
+    @XmlElement(required = true)
+    protected CTBlipFillProperties blipFill;
+    @XmlElement(required = true)
+    protected CTShapeProperties spPr;
+    @XmlTransient
+    private Object parent;
+
+    /**
+     * Gets the value of the nvPicPr property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link CTPictureNonVisual }
+     *     
+     */
+    public CTPictureNonVisual getNvPicPr() {
+        return nvPicPr;
+    }
+
+    /**
+     * Sets the value of the nvPicPr property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link CTPictureNonVisual }
+     *     
+     */
+    public void setNvPicPr(CTPictureNonVisual value) {
+        this.nvPicPr = value;
+    }
+
+    /**
+     * Gets the value of the blipFill property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link CTBlipFillProperties }
+     *     
+     */
+    public CTBlipFillProperties getBlipFill() {
+        return blipFill;
+    }
+
+    /**
+     * Sets the value of the blipFill property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link CTBlipFillProperties }
+     *     
+     */
+    public void setBlipFill(CTBlipFillProperties value) {
+        this.blipFill = value;
+    }
+
+    /**
+     * Gets the value of the spPr property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link CTShapeProperties }
+     *     
+     */
+    public CTShapeProperties getSpPr() {
+        return spPr;
+    }
+
+    /**
+     * Sets the value of the spPr property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link CTShapeProperties }
+     *     
+     */
+    public void setSpPr(CTShapeProperties value) {
+        this.spPr = value;
+    }
+
+    /**
+     * Gets the parent object in the object tree representing the unmarshalled xml document.
+     * 
+     * @return
+     *     The parent object.
+     */
+    public Object getParent() {
+        return this.parent;
+    }
+
+    public void setParent(Object parent) {
+        this.parent = parent;
+    }
+
+    /**
+     * This method is invoked by the JAXB implementation on each instance when unmarshalling completes.
+     * 
+     * @param parent
+     *     The parent object in the object tree.
+     * @param unmarshaller
+     *     The unmarshaller that generated the instance.
+     */
+    public void afterUnmarshal(Unmarshaller unmarshaller, Object parent) {
+        setParent(parent);
+    }
+
+}
