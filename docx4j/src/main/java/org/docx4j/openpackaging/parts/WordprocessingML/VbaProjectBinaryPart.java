4dc5446513322821be8d1873cb8ec43c3cce83df
Jason Harrop <jason@plutext.org>
2008-09-15 13:33:17 +0000

    VBA parts

---
+++ docx4j/src/main/java/org/docx4j/openpackaging/parts/WordprocessingML/VbaProjectBinaryPart.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright 2007-2008, Plutext Pty Ltd.
+ *   
+ *  This file is part of docx4j.
+
+    docx4j is licensed under the Apache License, Version 2.0 (the "License"); 
+    you may not use this file except in compliance with the License. 
+
+    You may obtain a copy of the License at 
+
+        http://www.apache.org/licenses/LICENSE-2.0 
+
+    Unless required by applicable law or agreed to in writing, software 
+    distributed under the License is distributed on an "AS IS" BASIS, 
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+    See the License for the specific language governing permissions and 
+    limitations under the License.
+
+ */
+
+package org.docx4j.openpackaging.parts.WordprocessingML;
+
+import org.apache.log4j.Logger;
+import org.docx4j.openpackaging.exceptions.InvalidFormatException;
+import org.docx4j.openpackaging.parts.PartName;
+import org.docx4j.openpackaging.parts.relationships.Namespaces;
+
+
+public class VbaProjectBinaryPart extends BinaryPart {
+
+	private static Logger log = Logger.getLogger(VbaProjectBinaryPart.class);		
+	
+	public VbaProjectBinaryPart(PartName partName) throws InvalidFormatException {
+		super(partName);
+		init();				
+	}
+
+	
+	public VbaProjectBinaryPart() throws InvalidFormatException {
+		super( new PartName("/word/vbaProject.bin") );
+		init();				
+	}
+	
+	public void init() {
+		// Used if this Part is added to [Content_Types].xml 
+		setContentType(new  org.docx4j.openpackaging.contenttype.ContentType( 
+				org.docx4j.openpackaging.contenttype.ContentTypes.OFFICEDOCUMENT_VBA_PROJECT));
+
+		// Used when this Part is added to a rels
+		setRelationshipType(Namespaces.VBA_PROJECT);
+	}
+
+	
+	
+}
