0dc30df1ce9261ad5c22ef82dcfa68d0e70ef4b4
Jason Harrop <jason@plutext.org>
2008-09-30 02:03:24 +0000

    Now a JAXB Xml Part.

--- docx4j/src/main/java/org/docx4j/openpackaging/parts/WordprocessingML/FooterPart.java
+++ docx4j/src/main/java/org/docx4j/openpackaging/parts/WordprocessingML/FooterPart.java
@@ -21,18 +21,37 @@
 package org.docx4j.openpackaging.parts.WordprocessingML;
 
 
+import java.io.IOException;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.log4j.Logger;
+import org.docx4j.jaxb.Context;
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
-import org.docx4j.openpackaging.parts.Dom4jXmlPart;
-import org.docx4j.openpackaging.parts.Part;
 import org.docx4j.openpackaging.parts.PartName;
 import org.docx4j.openpackaging.parts.relationships.Namespaces;
-import org.dom4j.Document;
+
+import org.docx4j.openpackaging.parts.JaxbXmlPart;
 
 
-public final class FooterPart extends Dom4jXmlPart {
+public final class FooterPart extends JaxbXmlPart {
+	
+	private static Logger log = Logger.getLogger(FooterPart.class);			
 	
 	public FooterPart(PartName partName) throws InvalidFormatException {
 		super(partName);
+		init();
+		
+	}
+
+	public FooterPart() throws InvalidFormatException {
+		super(new PartName("/word/footer.xml"));  // Not very useful, since normally there is more than one footer part
+		init();		
+	}		
+		
+		public void init() {
 		
 		// Used if this Part is added to [Content_Types].xml 
 		setContentType(new  org.docx4j.openpackaging.contenttype.ContentType( 
@@ -41,10 +60,47 @@ public final class FooterPart extends Dom4jXmlPart {
 		// Used when this Part is added to a rels 
 		setRelationshipType(Namespaces.FOOTER);
 	}
+	
+	    /**
+	     * Unmarshal XML data from the specified InputStream and return the 
+	     * resulting content tree.  Validation event location information may
+	     * be incomplete when using this form of the unmarshal API.
+	     *
+	     * <p>
+	     * Implements <a href="#unmarshalGlobal">Unmarshal Global Root Element</a>.
+	     * 
+	     * @param is the InputStream to unmarshal XML data from
+	     * @return the newly created root object of the java content tree 
+	     *
+	     * @throws JAXBException 
+	     *     If any unexpected errors occur while unmarshalling
+	     */
+	    public Object unmarshal( java.io.InputStream is ) throws JAXBException {
+	    	
+			try {
+				
+//				if (jc==null) {
+//					setJAXBContext(Context.jc);				
+//				}
+			    		    
+				Unmarshaller u = jc.createUnmarshaller();
+				
+				//u.setSchema(org.docx4j.jaxb.WmlSchema.schema);
+				u.setEventHandler(new org.docx4j.jaxb.JaxbValidationEventHandler());
 
-	@Override
-	public Document getDocument() {
-		return document;
-	}	
+				System.out.println("unmarshalling " + this.getClass().getName() + " \n\n" );									
+							
+				jaxbElement = u.unmarshal( is );
+								
+				System.out.println("\n\n" + this.getClass().getName() + " unmarshalled \n\n" );									
+
+			} catch (Exception e ) {
+				e.printStackTrace();
+			}
+	    	
+			return jaxbElement;
+	    	
+	    }
+	
 	
 }
