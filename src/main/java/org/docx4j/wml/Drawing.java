7123007d90182bcce3461809582a66a5aa7dafd0
Jason Harrop <jason@plutext.org>
2009-01-04 19:55:34 +0000

    Add @RootElement

--- src/main/java/org/docx4j/wml/Drawing.java
+++ src/main/java/org/docx4j/wml/Drawing.java
@@ -28,6 +28,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElements;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 import org.docx4j.dml.Anchor;
@@ -59,6 +60,7 @@ import org.jvnet.jaxb2_commons.ppp.Child;
 @XmlType(name = "CT_Drawing", propOrder = {
     "anchorOrInline"
 })
+@XmlRootElement(name ="drawing")
 public class Drawing
     implements Child
 {
