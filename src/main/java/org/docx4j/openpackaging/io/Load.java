34ea1a1939bebdb7ab2b9b2b3f001ce74cc0b163
Jason Harrop <jason@plutext.org>
2009-01-13 13:22:04 +0000

    new method getRawPart from InputStream

--- src/main/java/org/docx4j/openpackaging/io/Load.java
+++ src/main/java/org/docx4j/openpackaging/io/Load.java
@@ -20,16 +20,28 @@
 
 package org.docx4j.openpackaging.io;
 
+import java.io.InputStream;
 import java.io.StringWriter;
 import java.net.URISyntaxException;
 
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.Session;
+
 import org.apache.log4j.Logger;
+import org.docx4j.JcrNodeMapper.NodeMapper;
 import org.docx4j.document.wordprocessingml.Constants;
+import org.docx4j.jaxb.Context;
 import org.docx4j.openpackaging.contenttype.ContentTypeManager;
 import org.docx4j.openpackaging.contenttype.ContentTypes;
+import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
+import org.docx4j.openpackaging.exceptions.PartUnrecognisedException;
 import org.docx4j.openpackaging.packages.Package;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.Part;
+import org.docx4j.openpackaging.parts.PartName;
+import org.docx4j.openpackaging.parts.WordprocessingML.BinaryPart;
 import org.dom4j.Document;
 import org.dom4j.io.OutputFormat;
 import org.dom4j.io.XMLWriter;
@@ -57,5 +69,94 @@ public class Load {
 			e.printStackTrace();
 		}	    
 	}
+	
+	/**
+	 * Get a Part (except a relationships part), but not its relationships part
+	 * or related parts.  Useful if you need quick access to just this part,
+	 * or if you wish to add a foreign part (ie a part from some other package).
+	 * This can be called directly from outside the library, in which case 
+	 * the Part will not be owned by a Package until the calling code makes it so.  
+	 * @see  To get a Part and all its related parts, and add all to a package, use
+	 * getPart.
+	 * @param is
+	 * @param ctm the ContentTypeManager associated with the foreign package
+	 * @param resolvedPartUri  the part name
+	 * @return
+	 * 
+	 * @throws URISyntaxException
+	 * @throws InvalidFormatException
+	 */
+	public static Part getRawPart(InputStream is, ContentTypeManager ctm, String resolvedPartUri)
+			throws Docx4JException {
+		
+		Part part = null;
+
+		try {
+						
+			try {
+				
+				part = ctm.getPart("/" + resolvedPartUri);
+								
+				
+				if (part instanceof org.docx4j.openpackaging.parts.ThemePart) {
+
+					((org.docx4j.openpackaging.parts.JaxbXmlPart)part).setJAXBContext(Context.jcThemePart);
+					((org.docx4j.openpackaging.parts.JaxbXmlPart)part).unmarshal( is );
+					
+				} else if (part instanceof org.docx4j.openpackaging.parts.DocPropsCorePart ) {
+
+					((org.docx4j.openpackaging.parts.JaxbXmlPart)part).setJAXBContext(Context.jcDocPropsCore);
+					((org.docx4j.openpackaging.parts.JaxbXmlPart)part).unmarshal( is );
+					
+				} else if (part instanceof org.docx4j.openpackaging.parts.DocPropsCustomPart ) {
+
+					((org.docx4j.openpackaging.parts.JaxbXmlPart)part).setJAXBContext(Context.jcDocPropsCustom);
+					((org.docx4j.openpackaging.parts.JaxbXmlPart)part).unmarshal( is );
+					
+				} else if (part instanceof org.docx4j.openpackaging.parts.DocPropsExtendedPart ) {
+
+					((org.docx4j.openpackaging.parts.JaxbXmlPart)part).setJAXBContext(Context.jcDocPropsExtended);
+					((org.docx4j.openpackaging.parts.JaxbXmlPart)part).unmarshal( is );
+				
+				} else if (part instanceof org.docx4j.openpackaging.parts.JaxbXmlPart) {
+					
+					// MainDocument part, Styles part, Font part etc
+
+					((org.docx4j.openpackaging.parts.JaxbXmlPart)part).setJAXBContext(Context.jc);
+					((org.docx4j.openpackaging.parts.JaxbXmlPart)part).unmarshal( is );
+					
+				} else if (part instanceof org.docx4j.openpackaging.parts.Dom4jXmlPart) {
+					
+					((org.docx4j.openpackaging.parts.Dom4jXmlPart)part).setDocument( is );
+
+				} else if (part instanceof org.docx4j.openpackaging.parts.WordprocessingML.BinaryPart) {
+					
+					log.debug("Detected BinaryPart " + part.getClass().getName() );
+					((BinaryPart)part).setBinaryData(is);
+										
+				} else {
+					// Shouldn't happen, since ContentTypeManagerImpl should
+					// return an instance of one of the above, or throw an
+					// Exception.
+					
+					log.error("No suitable part found for: " + resolvedPartUri);
+					return null;					
+				}
+			} catch (PartUnrecognisedException e) {
+				
+				// Try to get it as a binary part				
+				part = new BinaryPart(new PartName("/" + resolvedPartUri));
+				((BinaryPart) part).setBinaryData(is);
+					
+			}
+		} catch (Exception ex) {
+			// PathNotFoundException, ValueFormatException, RepositoryException, URISyntaxException
+			ex.printStackTrace();
+			throw new Docx4JException("Failed to getPart", ex);
+		} 
+		return part;
+	}
+		
+	
 
 }
