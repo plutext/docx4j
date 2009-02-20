208effc917cbb23915a0ff1b52699930677a1d26
Jason Harrop <jason@plutext.org>
2009-02-20 16:38:42 +0000

    Part name should start with CustomXml, not CustomXML

--- docx4j/src/main/java/org/docx4j/openpackaging/parts/CustomXmlDataStoragePart.java
+++ docx4j/src/main/java/org/docx4j/openpackaging/parts/CustomXmlDataStoragePart.java
@@ -31,40 +31,13 @@ import org.dom4j.Document;
 
 public final class CustomXmlDataStoragePart extends Dom4jXmlPart {
 	
-	/* Currently (2009 02 13), docx4j *requires* that if a part has the main document part as its source 
-	 * part, then its name should be relative to that part.
-	 * 
-	 * In other words, /word/customXML/item1.xml rather than /customXML/item1.xml,
-	 * which gives:
-	 * 
-13.02.2009 14:50:46 *INFO * SaveToZipFile: For Relationship Id=rId1 Source is /word/document.xml, Target is customXML/item1.xml (SaveToZipFile.java, line 247)
-13.02.2009 14:50:46 *INFO * SaveToZipFile: Getting part /word/customXML/item1.xml (SaveToZipFile.java, line 287)
-13.02.2009 14:50:46 *DEBUG* PartName: Trying to create part name /word/customXML/item1.xml (PartName.java, line 150)
-13.02.2009 14:50:46 *DEBUG* PartName: /word/customXML/item1.xml part name created. (PartName.java, line 170)
-13.02.2009 14:50:46 *ERROR* SaveToZipFile: Part word/customXML/item1.xml not found! (SaveToZipFile.java, line 292)
-	 * 
-	 * Note that if you open a docx containing /word/customXML/item1.xml in Word 2007, and re-save it, then
-	 * resulting docx will contain customXml/item1.xml (and it will also create itemProps1.xml etc)
-	 * ie Word moves the custom xml to another place in the package.
-	 * The rel has target "../customXml/item1.xml
-	 * 
-	 * If you use the WordML package (as opposed to the main document part) as the source to which
-	 * you attach the custom xml part, note that when you re-save the document in Word 2007, 
-	 * Word will drop the custom xml part entirely!!  
-	 * 
-	 * So it seems you are best to attach this part to your main document part, 
-	 * rather than the package itself. 
-	 * 
-	 */
-	
-	
 	public CustomXmlDataStoragePart(PartName partName) throws InvalidFormatException {
 		super(partName);
 		init();
 	}
 	
 	public CustomXmlDataStoragePart() throws InvalidFormatException {
-		super(new PartName("/customXML/item1.xml"));
+		super(new PartName("/customXml/item1.xml"));
 		init();
 	}
 
