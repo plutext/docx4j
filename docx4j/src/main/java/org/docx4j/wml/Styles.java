b5d7bf805d90f2a754a2df19c9fc2b1a0f73ac74
Jason Harrop <jason@plutext.org>
2009-04-15 02:57:08 +0000

    promote DocDefaults

--- docx4j/src/main/java/org/docx4j/wml/Styles.java
+++ docx4j/src/main/java/org/docx4j/wml/Styles.java
@@ -1,23 +1,3 @@
-/*
- *  Copyright 2007-2008, Plutext Pty Ltd.
- *   
- *  This file is part of docx4j.
-
-    docx4j is licensed under the Apache License, Version 2.0 (the "License"); 
-    you may not use this file except in compliance with the License. 
-
-    You may obtain a copy of the License at 
-
-        http://www.apache.org/licenses/LICENSE-2.0 
-
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, 
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-    See the License for the specific language governing permissions and 
-    limitations under the License.
-
- */
-
 
 package org.docx4j.wml;
 
@@ -44,38 +24,7 @@ import org.jvnet.jaxb2_commons.ppp.Child;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="docDefaults" minOccurs="0">
- *           &lt;complexType>
- *             &lt;complexContent>
- *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *                 &lt;sequence>
- *                   &lt;element name="rPrDefault" minOccurs="0">
- *                     &lt;complexType>
- *                       &lt;complexContent>
- *                         &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *                           &lt;sequence>
- *                             &lt;element name="rPr" type="{http://schemas.openxmlformats.org/wordprocessingml/2006/main}CT_RPr" minOccurs="0"/>
- *                           &lt;/sequence>
- *                         &lt;/restriction>
- *                       &lt;/complexContent>
- *                     &lt;/complexType>
- *                   &lt;/element>
- *                   &lt;element name="pPrDefault" minOccurs="0">
- *                     &lt;complexType>
- *                       &lt;complexContent>
- *                         &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *                           &lt;sequence>
- *                             &lt;element name="pPr" type="{http://schemas.openxmlformats.org/wordprocessingml/2006/main}CT_PPr" minOccurs="0"/>
- *                           &lt;/sequence>
- *                         &lt;/restriction>
- *                       &lt;/complexContent>
- *                     &lt;/complexType>
- *                   &lt;/element>
- *                 &lt;/sequence>
- *               &lt;/restriction>
- *             &lt;/complexContent>
- *           &lt;/complexType>
- *         &lt;/element>
+ *         &lt;element ref="{http://schemas.openxmlformats.org/wordprocessingml/2006/main}docDefaults" minOccurs="0"/>
  *         &lt;element name="latentStyles" minOccurs="0">
  *           &lt;complexType>
  *             &lt;complexContent>
@@ -126,7 +75,7 @@ public class Styles
     implements Child
 {
 
-    protected Styles.DocDefaults docDefaults;
+    protected DocDefaults docDefaults;
     protected Styles.LatentStyles latentStyles;
     protected List<Style> style;
     @XmlTransient
@@ -137,10 +86,10 @@ public class Styles
      * 
      * @return
      *     possible object is
-     *     {@link Styles.DocDefaults }
+     *     {@link DocDefaults }
      *     
      */
-    public Styles.DocDefaults getDocDefaults() {
+    public DocDefaults getDocDefaults() {
         return docDefaults;
     }
 
@@ -149,10 +98,10 @@ public class Styles
      * 
      * @param value
      *     allowed object is
-     *     {@link Styles.DocDefaults }
+     *     {@link DocDefaults }
      *     
      */
-    public void setDocDefaults(Styles.DocDefaults value) {
+    public void setDocDefaults(DocDefaults value) {
         this.docDefaults = value;
     }
 
@@ -233,305 +182,6 @@ public class Styles
      */
     public void afterUnmarshal(Unmarshaller unmarshaller, Object parent) {
         setParent(parent);
-    }
-
-
-    /**
-     * <p>Java class for anonymous complex type.
-     * 
-     * <p>The following schema fragment specifies the expected content contained within this class.
-     * 
-     * <pre>
-     * &lt;complexType>
-     *   &lt;complexContent>
-     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
-     *       &lt;sequence>
-     *         &lt;element name="rPrDefault" minOccurs="0">
-     *           &lt;complexType>
-     *             &lt;complexContent>
-     *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
-     *                 &lt;sequence>
-     *                   &lt;element name="rPr" type="{http://schemas.openxmlformats.org/wordprocessingml/2006/main}CT_RPr" minOccurs="0"/>
-     *                 &lt;/sequence>
-     *               &lt;/restriction>
-     *             &lt;/complexContent>
-     *           &lt;/complexType>
-     *         &lt;/element>
-     *         &lt;element name="pPrDefault" minOccurs="0">
-     *           &lt;complexType>
-     *             &lt;complexContent>
-     *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
-     *                 &lt;sequence>
-     *                   &lt;element name="pPr" type="{http://schemas.openxmlformats.org/wordprocessingml/2006/main}CT_PPr" minOccurs="0"/>
-     *                 &lt;/sequence>
-     *               &lt;/restriction>
-     *             &lt;/complexContent>
-     *           &lt;/complexType>
-     *         &lt;/element>
-     *       &lt;/sequence>
-     *     &lt;/restriction>
-     *   &lt;/complexContent>
-     * &lt;/complexType>
-     * </pre>
-     * 
-     * 
-     */
-    @XmlAccessorType(XmlAccessType.FIELD)
-    @XmlType(name = "", propOrder = {
-        "rPrDefault",
-        "pPrDefault"
-    })
-    public static class DocDefaults
-        implements Child
-    {
-
-        protected Styles.DocDefaults.RPrDefault rPrDefault;
-        protected Styles.DocDefaults.PPrDefault pPrDefault;
-        @XmlTransient
-        private Object parent;
-
-        /**
-         * Gets the value of the rPrDefault property.
-         * 
-         * @return
-         *     possible object is
-         *     {@link Styles.DocDefaults.RPrDefault }
-         *     
-         */
-        public Styles.DocDefaults.RPrDefault getRPrDefault() {
-            return rPrDefault;
-        }
-
-        /**
-         * Sets the value of the rPrDefault property.
-         * 
-         * @param value
-         *     allowed object is
-         *     {@link Styles.DocDefaults.RPrDefault }
-         *     
-         */
-        public void setRPrDefault(Styles.DocDefaults.RPrDefault value) {
-            this.rPrDefault = value;
-        }
-
-        /**
-         * Gets the value of the pPrDefault property.
-         * 
-         * @return
-         *     possible object is
-         *     {@link Styles.DocDefaults.PPrDefault }
-         *     
-         */
-        public Styles.DocDefaults.PPrDefault getPPrDefault() {
-            return pPrDefault;
-        }
-
-        /**
-         * Sets the value of the pPrDefault property.
-         * 
-         * @param value
-         *     allowed object is
-         *     {@link Styles.DocDefaults.PPrDefault }
-         *     
-         */
-        public void setPPrDefault(Styles.DocDefaults.PPrDefault value) {
-            this.pPrDefault = value;
-        }
-
-        /**
-         * Gets the parent object in the object tree representing the unmarshalled xml document.
-         * 
-         * @return
-         *     The parent object.
-         */
-        public Object getParent() {
-            return this.parent;
-        }
-
-        public void setParent(Object parent) {
-            this.parent = parent;
-        }
-
-        /**
-         * This method is invoked by the JAXB implementation on each instance when unmarshalling completes.
-         * 
-         * @param parent
-         *     The parent object in the object tree.
-         * @param unmarshaller
-         *     The unmarshaller that generated the instance.
-         */
-        public void afterUnmarshal(Unmarshaller unmarshaller, Object parent) {
-            setParent(parent);
-        }
-
-
-        /**
-         * <p>Java class for anonymous complex type.
-         * 
-         * <p>The following schema fragment specifies the expected content contained within this class.
-         * 
-         * <pre>
-         * &lt;complexType>
-         *   &lt;complexContent>
-         *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
-         *       &lt;sequence>
-         *         &lt;element name="pPr" type="{http://schemas.openxmlformats.org/wordprocessingml/2006/main}CT_PPr" minOccurs="0"/>
-         *       &lt;/sequence>
-         *     &lt;/restriction>
-         *   &lt;/complexContent>
-         * &lt;/complexType>
-         * </pre>
-         * 
-         * 
-         */
-        @XmlAccessorType(XmlAccessType.FIELD)
-        @XmlType(name = "", propOrder = {
-            "pPr"
-        })
-        public static class PPrDefault
-            implements Child
-        {
-
-            protected PPr pPr;
-            @XmlTransient
-            private Object parent;
-
-            /**
-             * Gets the value of the pPr property.
-             * 
-             * @return
-             *     possible object is
-             *     {@link PPr }
-             *     
-             */
-            public PPr getPPr() {
-                return pPr;
-            }
-
-            /**
-             * Sets the value of the pPr property.
-             * 
-             * @param value
-             *     allowed object is
-             *     {@link PPr }
-             *     
-             */
-            public void setPPr(PPr value) {
-                this.pPr = value;
-            }
-
-            /**
-             * Gets the parent object in the object tree representing the unmarshalled xml document.
-             * 
-             * @return
-             *     The parent object.
-             */
-            public Object getParent() {
-                return this.parent;
-            }
-
-            public void setParent(Object parent) {
-                this.parent = parent;
-            }
-
-            /**
-             * This method is invoked by the JAXB implementation on each instance when unmarshalling completes.
-             * 
-             * @param parent
-             *     The parent object in the object tree.
-             * @param unmarshaller
-             *     The unmarshaller that generated the instance.
-             */
-            public void afterUnmarshal(Unmarshaller unmarshaller, Object parent) {
-                setParent(parent);
-            }
-
-        }
-
-
-        /**
-         * <p>Java class for anonymous complex type.
-         * 
-         * <p>The following schema fragment specifies the expected content contained within this class.
-         * 
-         * <pre>
-         * &lt;complexType>
-         *   &lt;complexContent>
-         *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
-         *       &lt;sequence>
-         *         &lt;element name="rPr" type="{http://schemas.openxmlformats.org/wordprocessingml/2006/main}CT_RPr" minOccurs="0"/>
-         *       &lt;/sequence>
-         *     &lt;/restriction>
-         *   &lt;/complexContent>
-         * &lt;/complexType>
-         * </pre>
-         * 
-         * 
-         */
-        @XmlAccessorType(XmlAccessType.FIELD)
-        @XmlType(name = "", propOrder = {
-            "rPr"
-        })
-        public static class RPrDefault
-            implements Child
-        {
-
-            protected RPr rPr;
-            @XmlTransient
-            private Object parent;
-
-            /**
-             * Gets the value of the rPr property.
-             * 
-             * @return
-             *     possible object is
-             *     {@link RPr }
-             *     
-             */
-            public RPr getRPr() {
-                return rPr;
-            }
-
-            /**
-             * Sets the value of the rPr property.
-             * 
-             * @param value
-             *     allowed object is
-             *     {@link RPr }
-             *     
-             */
-            public void setRPr(RPr value) {
-                this.rPr = value;
-            }
-
-            /**
-             * Gets the parent object in the object tree representing the unmarshalled xml document.
-             * 
-             * @return
-             *     The parent object.
-             */
-            public Object getParent() {
-                return this.parent;
-            }
-
-            public void setParent(Object parent) {
-                this.parent = parent;
-            }
-
-            /**
-             * This method is invoked by the JAXB implementation on each instance when unmarshalling completes.
-             * 
-             * @param parent
-             *     The parent object in the object tree.
-             * @param unmarshaller
-             *     The unmarshaller that generated the instance.
-             */
-            public void afterUnmarshal(Unmarshaller unmarshaller, Object parent) {
-                setParent(parent);
-            }
-
-        }
-
     }
 
 
