00d2c10a893ec2616b726985001071bc4fc36929
Jason Harrop <jason@plutext.org>
2009-07-12 04:55:46 +0000

    @XmlRootElement

--- src/main/java/org/docx4j/wml/CTBookmark.java
+++ src/main/java/org/docx4j/wml/CTBookmark.java
@@ -25,6 +25,7 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 import org.jvnet.jaxb2_commons.ppp.Child;
@@ -49,6 +50,7 @@ import org.jvnet.jaxb2_commons.ppp.Child;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "CT_Bookmark")
+@XmlRootElement(name = "bookmarkStart")
 public class CTBookmark
     extends CTBookmarkRange
     implements Child
