8fb34a4fc49c48b22a88e3b7f85b80f65a03a20d
Jason Harrop <jason@plutext.org>
2010-01-16 10:35:43 +0000

    Cleanup imports

--- src/pptx4j/java/org/docx4j/openpackaging/packages/PresentationMLPackage.java
+++ src/pptx4j/java/org/docx4j/openpackaging/packages/PresentationMLPackage.java
@@ -21,54 +21,16 @@
 package org.docx4j.openpackaging.packages;
 
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.util.Map;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.PropertyException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.transform.Source;
-import javax.xml.transform.Templates;
-import javax.xml.transform.stream.StreamSource;
-
 import org.apache.log4j.Logger;
-import org.docx4j.XmlUtils;
-import org.docx4j.convert.out.flatOpcXml.FlatOpcXmlCreator;
-import org.docx4j.fonts.IdentityPlusMapper;
-import org.docx4j.fonts.Mapper;
-import org.docx4j.jaxb.Context;
-import org.docx4j.jaxb.NamespacePrefixMapperUtils;
-import org.docx4j.model.structure.DocumentModel;
-import org.docx4j.model.structure.HeaderFooterPolicy;
-import org.docx4j.model.structure.PageDimensions;
 import org.docx4j.openpackaging.contenttype.ContentType;
 import org.docx4j.openpackaging.contenttype.ContentTypeManager;
 import org.docx4j.openpackaging.contenttype.ContentTypes;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
-import org.docx4j.openpackaging.exceptions.InvalidFormatException;
-import org.docx4j.openpackaging.io.LoadFromZipNG;
-import org.docx4j.openpackaging.io.SaveToZipFile;
 import org.docx4j.openpackaging.parts.DocPropsCorePart;
 import org.docx4j.openpackaging.parts.DocPropsCustomPart;
 import org.docx4j.openpackaging.parts.DocPropsExtendedPart;
-import org.docx4j.openpackaging.parts.JaxbXmlPart;
 import org.docx4j.openpackaging.parts.Part;
-import org.docx4j.openpackaging.parts.WordprocessingML.FontTablePart;
-import org.docx4j.openpackaging.parts.WordprocessingML.GlossaryDocumentPart;
-import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
 import org.docx4j.openpackaging.parts.relationships.Namespaces;
-import org.docx4j.wml.Document;
-import org.docx4j.wml.SectPr;
-import org.docx4j.wml.Styles;
-
-
-
-
 
 
 
@@ -143,6 +105,11 @@ public class PresentationMLPackage  extends Package {
 		PresentationMLPackage presentationMLPackage = 
 			(PresentationMLPackage)PresentationMLPackage.load(new java.io.File(inputfilepath));		
 		
+		System.out.println("\n\n saving .. \n\n");
+		String outputfilepath = System.getProperty("user.dir") + "/sample-docs/pptx-out.pptx";
+		presentationMLPackage.save(new java.io.File(outputfilepath));
+
+		System.out.println("\n\n done .. \n\n");
 		
 	}	
 }
