c44e2a93193989269c02b40123787452683cb054
Jason Harrop <jason@plutext.org>
2010-08-25 12:44:54 +0000

    Example of adding a docx to a docx

---
+++ src/main/java/org/docx4j/samples/AltChunk.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright 2007-2008, Plutext Pty Ltd.
+ *   
+ *  This file is part of docx4j.
+
+    docx4j is licensed under the Apache License, Version 2.0 (the "License"); 
+    you may not use this file except in compliance with the License. 
+
+    You may obtain a copy of the License at 
+
+        http://www.apache.org/licenses/LICENSE-2.0 
+
+    Unless required by applicable law or agreed to in writing, software 
+    distributed under the License is distributed on an "AS IS" BASIS, 
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+    See the License for the specific language governing permissions and 
+    limitations under the License.
+
+ */
+
+package org.docx4j.samples;
+
+
+import java.io.FileInputStream;
+
+import org.docx4j.jaxb.Context;
+import org.docx4j.openpackaging.contenttype.ContentType;
+import org.docx4j.openpackaging.io.SaveToZipFile;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.PartName;
+import org.docx4j.openpackaging.parts.WordprocessingML.AlternativeFormatInputPart;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.relationships.Relationship;
+import org.docx4j.wml.CTAltChunk;
+
+
+public class AltChunk {
+	
+	public static void main(String[] args) throws Exception {
+
+		String inputfilepath = System.getProperty("user.dir") + "/sample-docs/Table.docx";
+		
+		String chunkPath = System.getProperty("user.dir") + "/sample-docs/Word2007-fonts.docx";
+		
+		boolean save = true;
+		String outputfilepath = System.getProperty("user.dir") + "/tmp/altChunk_out.docx";
+		
+		
+		// Open a document from the file system
+		// 1. Load the Package
+		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new java.io.File(inputfilepath));
+		MainDocumentPart main = wordMLPackage.getMainDocumentPart();
+		
+		AlternativeFormatInputPart afiPart = new AlternativeFormatInputPart(new PartName("/chunk.docx") );
+		afiPart.setBinaryData(
+				new FileInputStream(chunkPath) );
+		
+		afiPart.setContentType(new ContentType("application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml")); //docx
+		//afiPart.setContentType(new ContentType("application/xhtml+xml")); //xhtml
+
+		Relationship altChunkRel = main.addTargetPart(afiPart);
+		CTAltChunk ac = Context.getWmlObjectFactory().createCTAltChunk();
+		ac.setId(altChunkRel.getId());
+
+		main.addObject(ac);		
+		
+		// Save it
+		
+		if (save) {		
+			SaveToZipFile saver = new SaveToZipFile(wordMLPackage);
+			saver.save(outputfilepath);
+			System.out.println("Saved " + outputfilepath);
+		}
+	}
+		
+
+}
