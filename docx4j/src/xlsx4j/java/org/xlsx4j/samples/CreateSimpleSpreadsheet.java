a7f8a124cf4f98bed0a7dacf6278d1bb744c7001
Jason Harrop <jason@plutext.org>
2010-09-06 22:52:07 +0000

    Some xlsx examples

--- docx4j/src/xlsx4j/java/org/xlsx4j/samples/CreateSimpleSpreadsheet.java
+++ docx4j/src/xlsx4j/java/org/xlsx4j/samples/CreateSimpleSpreadsheet.java
@@ -3,6 +3,12 @@ package org.xlsx4j.samples;
 import org.docx4j.openpackaging.io.SaveToZipFile;
 import org.docx4j.openpackaging.packages.SpreadsheetMLPackage;
 import org.docx4j.openpackaging.parts.PartName;
+import org.docx4j.openpackaging.parts.SpreadsheetML.WorksheetPart;
+import org.xlsx4j.jaxb.Context;
+import org.xlsx4j.sml.Cell;
+import org.xlsx4j.sml.Row;
+import org.xlsx4j.sml.SheetData;
+import org.xlsx4j.sml.Worksheet;
 
 public class CreateSimpleSpreadsheet {
 	
@@ -12,12 +18,28 @@ public class CreateSimpleSpreadsheet {
 		
 		SpreadsheetMLPackage pkg = SpreadsheetMLPackage.createPackage();
 		
-		pkg.createWorksheetPart(new PartName("/xl/worksheets/sheet1.xml"), "Sheet1", 1);
+		WorksheetPart sheet = pkg.createWorksheetPart(new PartName("/xl/worksheets/sheet1.xml"), "Sheet1", 1);
+		
+		addContent(sheet);
 		
 		SaveToZipFile saver = new SaveToZipFile(pkg);
 		saver.save(outputfilepath);
 				
 		System.out.println("\n\n done .. " + outputfilepath);	
+	}
+	
+	private static void addContent(WorksheetPart sheet) {
+		
+		// Minimal content already present
+		SheetData sheetData = sheet.getJaxbElement().getSheetData();
+				
+		// Now add
+		Row row = Context.getsmlObjectFactory().createRow();
+		Cell cell = Context.getsmlObjectFactory().createCell();
+		cell.setV("1234");
+		
+		row.getC().add(cell);
+		sheetData.getRow().add(row);
 	}
 	
 }
