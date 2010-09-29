834e908a2f32f1175b3a53b44efc300a93f22cda
Jason Harrop <jason@plutext.org>
2010-09-29 01:48:56 +0000

    Comments about unwrap DSP: specify correct type SDP: don't need to override unmarshall

--- src/main/java/org/docx4j/samples/XPathQuery.java
+++ src/main/java/org/docx4j/samples/XPathQuery.java
@@ -2,6 +2,7 @@ package org.docx4j.samples;
 
 import java.util.List;
 
+import org.docx4j.XmlUtils;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
 
@@ -13,18 +14,43 @@ public class XPathQuery {
 	 */
 	public static void main(String[] args) throws Exception {
 
-		//String inputfilepath = System.getProperty("user.dir") + "/sample-docs/Images.docx";
-		String inputfilepath = System.getProperty("user.dir") + "//tmp//modelo.docx";
+		String inputfilepath = System.getProperty("user.dir") + "/sample-docs/sample-docx.xml";
+		//String inputfilepath = System.getProperty("user.dir") + "//tmp//modelo.docx";
 //    	String inputfilepath = "/home/dev/workspace/docx4j/sample-docs/fo-200912.xml";
 				
 		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new java.io.File(inputfilepath));		
 		MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
 				
-		String xpath = "//w:p";
+		//String xpath = "//w:t";
+		String xpath = "//w:r[w:t[contains(text(),'scaled')]]";
 		
 		List<Object> list = documentPart.getJAXBNodesViaXPath(xpath, false);
 		
 		System.out.println("got " + list.size() + " matching " + xpath );
+		
+		for (Object o : list) {
+			
+			System.out.println(o.getClass().getName() );
+			
+			Object o2 = XmlUtils.unwrap(o);
+			// this is ok, provided the results of the Callback
+			// won't be marshalled
+			
+			
+			if (o2 instanceof org.docx4j.wml.Text) {
+				
+				org.docx4j.wml.Text txt = (org.docx4j.wml.Text)o2;
+				
+				System.out.println( txt.getValue() );
+				
+				Object parent = txt.getParent();
+			
+				System.out.println( "parent: " +  XmlUtils.unwrap(parent).getClass().getName() );
+			}
+
+			
+			
+		}
 						
 	}
 	
