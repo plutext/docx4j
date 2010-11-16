26590eb2b274488ad555553dc10d210dd2748e09
Jason Harrop <jason@plutext.org>
2010-11-16 02:22:09 +0000

    corrected namespace not committed before

--- src/main/java/org/opendope/components/package-info.java
+++ src/main/java/org/opendope/components/package-info.java
@@ -1,2 +1,2 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://opendope.org/xpaths", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://opendope.org/components", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package org.opendope.components;
