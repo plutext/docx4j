d20ec4349c733c4093e76cfc5b9cf1a2ead84af6
Jason Harrop <jason@plutext.org>
2010-11-17 21:24:18 +0000

    @XmlRootElement(name = "pict")

--- src/main/java/org/docx4j/wml/Pict.java
+++ src/main/java/org/docx4j/wml/Pict.java
@@ -24,6 +24,7 @@ package org.docx4j.wml;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 import org.jvnet.jaxb2_commons.ppp.Child;
@@ -54,6 +55,7 @@ import org.jvnet.jaxb2_commons.ppp.Child;
     "movie",
     "control"
 })
+@XmlRootElement(name = "pict")
 public class Pict
     extends CTPictureBase
     implements Child
