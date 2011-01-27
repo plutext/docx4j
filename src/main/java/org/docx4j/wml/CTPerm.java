234d59a4005d119d491361992e65cee23ee378a2
Jason Harrop <jason@plutext.org>
2011-01-27 01:15:34 +0000

    @XmlRootElement(name = "permEnd")

--- src/main/java/org/docx4j/wml/CTPerm.java
+++ src/main/java/org/docx4j/wml/CTPerm.java
@@ -25,6 +25,7 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 import org.jvnet.jaxb2_commons.ppp.Child;
@@ -50,6 +51,7 @@ import org.jvnet.jaxb2_commons.ppp.Child;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "CT_Perm")
+@XmlRootElement(name = "permEnd")
 public class CTPerm
     implements Child
 {
