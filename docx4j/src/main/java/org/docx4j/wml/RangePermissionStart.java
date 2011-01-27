7c39ad50c258e1be193872559e9a1bf4c1034a24
Jason Harrop <jason@plutext.org>
2011-01-27 00:51:22 +0000

    @XmlRootElement(name = "permStart")

--- docx4j/src/main/java/org/docx4j/wml/RangePermissionStart.java
+++ docx4j/src/main/java/org/docx4j/wml/RangePermissionStart.java
@@ -26,6 +26,7 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 import org.jvnet.jaxb2_commons.ppp.Child;
@@ -65,6 +66,7 @@ import org.jvnet.jaxb2_commons.ppp.Child;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "CT_PermStart")
+@XmlRootElement(name = "permStart")
 public class RangePermissionStart
     extends CTPerm
     implements Child
