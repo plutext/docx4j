b098658e4e7710be3f5482e0bab0e68867c61395
Jason Harrop <jason@plutext.org>
2011-02-01 01:33:51 +0000

    AlteredParts unit tests WIP

--- docx4j/src/test/java/org/docx4j/openpackaging/parts/relationships/AlteredPartsTest.java
+++ docx4j/src/test/java/org/docx4j/openpackaging/parts/relationships/AlteredPartsTest.java
@@ -16,8 +16,8 @@ public class AlteredPartsTest {
 
 	protected static Logger log = Logger.getLogger(AlteredPartsTest.class);
 	
-	//private static String resourceDir = System.getProperty("user.dir") + "/src/test/resources/AlteredParts/";
-	private static String resourceDir = System.getProperty("user.dir") + "/sample-docs/";
+	private static String resourceDir = System.getProperty("user.dir") + "/src/test/resources/AlteredParts/";
+//	private static String resourceDir = System.getProperty("user.dir") + "/sample-docs/";
 	
 	/**
 	 * @throws java.lang.Exception
@@ -43,10 +43,11 @@ public class AlteredPartsTest {
 	
 	@Test
 	public void testSimpleDocx() throws Exception {
+		// Should return no differences
 		
-		log.warn("\n\n");
+		log.warn("\ntestSimpleDocx\n");
 		
-		String inputfilepath = resourceDir + "sample-docx.xml";
+		String inputfilepath = resourceDir + "paragraph-single.docx";
 		
 		WordprocessingMLPackage thisPackage = WordprocessingMLPackage.load(new java.io.File(inputfilepath));
 		WordprocessingMLPackage otherPackage = WordprocessingMLPackage.load(new java.io.File(inputfilepath));
@@ -57,14 +58,15 @@ public class AlteredPartsTest {
 	}
 
 	@Test
-	public void testDifferent() throws Exception {
+	public void testExtraParagraph() throws Exception {
+		// Only document.xml should be different
 
-		log.warn("\n\n");
+		log.warn("\ntestExtraParagraph\n");
 		
 		WordprocessingMLPackage thisPackage = WordprocessingMLPackage.load(
-				new java.io.File(resourceDir + "sample-docx.xml"));
+				new java.io.File(resourceDir + "paragraph-single.docx"));
 		WordprocessingMLPackage otherPackage = WordprocessingMLPackage.load(
-				new java.io.File(resourceDir + "Images.docx"));
+				new java.io.File(resourceDir + "paragraph-two.docx"));
 		
 		List<Part> alteredParts = AlteredParts.start(thisPackage, otherPackage);
 
@@ -72,19 +74,215 @@ public class AlteredPartsTest {
 			System.out.println(p.partName.getName() );
 		}
 				
-		assertTrue( true );
+		assertTrue( alteredParts.size()==1 );
+	}
+
+	@Test
+	public void testHyperlink() throws Exception {
+		// document.xml and rels should be different,
+		// and styles, since hyperlink style is now in use
+
+		log.warn("\ntestHyperlink\n");
+		
+		WordprocessingMLPackage thisPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "hyperlink.docx"));
+		WordprocessingMLPackage otherPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "paragraph-single.docx"));
+		
+		List<Part> alteredParts = AlteredParts.start(thisPackage, otherPackage);
+
+		for (Part p : alteredParts) {
+			System.out.println(p.partName.getName() );
+		}
+				
+		assertTrue( alteredParts.size()==3 );
+	}
+
+	@Test
+	public void testImageScaled() throws Exception {
+		// document.xml should be different,
+
+		log.warn("\ntestImageScaled\n");
+		
+		WordprocessingMLPackage thisPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "image-png1.docx"));
+		WordprocessingMLPackage otherPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "image-png1-scaled.docx"));
+		
+		List<Part> alteredParts = AlteredParts.start(thisPackage, otherPackage);
+
+		for (Part p : alteredParts) {
+			System.out.println(p.partName.getName() );
+		}
+				
+		assertTrue( alteredParts.size()==1 );
 	}
 	
-	/* Test cases:
-	 * 
-	 * - identical docx
-	 * - identical, except extra para
-	 * - one has hyperlink (external part)
-	 * - different images
-	 * - header with different image
-	 * - different header
-	 * - extra comment
-	 * 
-	 */
+	@Test
+	public void testImagesDifferentPng() throws Exception {
+		// document.xml and image part should be different,
+		// but rels unchanged
+
+		log.warn("\testImagesDifferentPng\n");
+		
+		WordprocessingMLPackage thisPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "image-png1.docx"));
+		WordprocessingMLPackage otherPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "image-png2.docx"));
+		
+		List<Part> alteredParts = AlteredParts.start(thisPackage, otherPackage);
+
+		for (Part p : alteredParts) {
+			System.out.println(p.partName.getName() );
+		}
+				
+		assertTrue( alteredParts.size()==2 );
+	}
+
+	@Test
+	public void testImagesRelId() throws Exception {
+		// a jpg inserted before, so the png's relId is now different
+		// document.xml and rels should be different,
+		// and extra image reported
+
+		log.warn("\ntestImagesDifferentPng\n");
+		
+		WordprocessingMLPackage thisPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "images-jpg then png2.docx"));
+		WordprocessingMLPackage otherPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "image-png2.docx"));
+		
+		List<Part> alteredParts = AlteredParts.start(thisPackage, otherPackage);
+
+		for (Part p : alteredParts) {
+			System.out.println(p.partName.getName() );
+		}
+				
+		assertTrue( alteredParts.size()==3 );
+	}
+
+	@Test
+	public void testImagesOrderSwapped() throws Exception {
+		// document.xml and rels should be different,
+
+		log.warn("\ntestImagesOrderSwapped\n");
+		
+		WordprocessingMLPackage thisPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "images-jpg then png2.docx"));
+		WordprocessingMLPackage otherPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "images-png2 then jpg.docx"));
+		
+		List<Part> alteredParts = AlteredParts.start(thisPackage, otherPackage);
+
+		for (Part p : alteredParts) {
+			System.out.println(p.partName.getName() );
+		}
+				
+		assertTrue( alteredParts.size()==2 );
+	}
+
+	@Test
+	public void testHeaderAdded() throws Exception {
+		// document.xml and rels should be different,
+		// and headers added: Word adds 3 x headers, 3 x footers, endnotes, and footnotes
+		// but 2 props files are as well
+
+		log.warn("\ntestHeaderAdded\n");
+		
+		WordprocessingMLPackage thisPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "header-simple.docx"));
+		WordprocessingMLPackage otherPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "paragraph-single.docx"));
+		
+		List<Part> alteredParts = AlteredParts.start(thisPackage, otherPackage);
+
+		for (Part p : alteredParts) {
+			System.out.println(p.partName.getName() );
+		}
+				
+		assertTrue( alteredParts.size()==12 );
+	}
+
+	@Test
+	public void testHeaderAddedSection2() throws Exception {
+		// document.xml and rels should be different,
+		// and headers added: doc now contains 2 x headers, 0 x footers, endnotes, and footnotes
+
+		log.warn("\ntestHeaderAddedSection2\n");
+		
+		WordprocessingMLPackage thisPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "header-section2.docx"));
+		WordprocessingMLPackage otherPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "header-simple.docx"));
+		
+		List<Part> alteredParts = AlteredParts.start(thisPackage, otherPackage);
+
+		for (Part p : alteredParts) {
+			System.out.println(p.partName.getName() );
+		}
+				
+		assertTrue( alteredParts.size()==3 );
+	}
+
+	@Test
+	public void testHeaderImageAdded() throws Exception {
+		// document.xml and rels should be different,
+		// plus image + header rels
+
+		log.warn("\ntestHeaderImageAdded\n");
+		
+		WordprocessingMLPackage thisPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "header-simple-plus-image.docx"));
+		WordprocessingMLPackage otherPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "header-simple.docx"));
+		
+		List<Part> alteredParts = AlteredParts.start(thisPackage, otherPackage);
+
+		for (Part p : alteredParts) {
+			System.out.println(p.partName.getName() );
+		}
+				
+		assertTrue( alteredParts.size()==4 );
+	}
+	
+	@Test
+	public void testComment() throws Exception {
+		// document.xml + rels + comments part should be different
+
+		log.warn("\ntestComment\n");
+		
+		WordprocessingMLPackage thisPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "comments-one.docx"));
+		WordprocessingMLPackage otherPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "paragraph-single.docx"));
+		
+		List<Part> alteredParts = AlteredParts.start(thisPackage, otherPackage);
+
+		for (Part p : alteredParts) {
+			System.out.println(p.partName.getName() );
+		}
+				
+		assertTrue( alteredParts.size()==3 );
+	}
+
+	@Test
+	public void testComment2() throws Exception {
+		// document.xml + comments part should be different
+
+		log.warn("\ntestComment2\n");
+		
+		WordprocessingMLPackage thisPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "comments-two.docx"));
+		WordprocessingMLPackage otherPackage = WordprocessingMLPackage.load(
+				new java.io.File(resourceDir + "comments-one.docx"));
+		
+		List<Part> alteredParts = AlteredParts.start(thisPackage, otherPackage);
+
+		for (Part p : alteredParts) {
+			System.out.println(p.partName.getName() );
+		}
+				
+		assertTrue( alteredParts.size()==2 );
+	}
 	
 }
