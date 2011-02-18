ddb01ad1a505ad749a49dbccb76a96120a449542
Jason Harrop <jason@plutext.org>
2011-02-18 00:16:48 +0000

    clean up

--- src/main/java/org/docx4j/samples/TemplateAttach.java
+++ src/main/java/org/docx4j/samples/TemplateAttach.java
@@ -21,40 +21,40 @@
 
 package org.docx4j.samples;
 
-import java.io.File;
-
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Marshaller;
-import javax.xml.namespace.QName;
 
 import org.docx4j.XmlUtils;
 import org.docx4j.convert.out.flatOpcXml.FlatOpcXmlCreator;
 import org.docx4j.jaxb.Context;
 import org.docx4j.jaxb.NamespacePrefixMapperUtils;
-import org.docx4j.model.table.TblFactory;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
-import org.docx4j.openpackaging.parts.PartName;
-import org.docx4j.openpackaging.parts.WordprocessingML.AlternativeFormatInputPart;
 import org.docx4j.openpackaging.parts.WordprocessingML.DocumentSettingsPart;
-import org.docx4j.openpackaging.parts.relationships.Namespaces;
 import org.docx4j.openpackaging.parts.relationships.RelationshipsPart;
-import org.docx4j.openpackaging.contenttype.CTDefault;
-import org.docx4j.openpackaging.contenttype.ContentType;
-import org.docx4j.openpackaging.contenttype.ContentTypes;
-import org.docx4j.openpackaging.contenttype.ObjectFactory;
-import org.docx4j.openpackaging.exceptions.InvalidFormatException;
-import org.docx4j.openpackaging.io.SaveToZipFile;
-import org.docx4j.relationships.Relationship;
-import org.docx4j.wml.CTAltChunk;
 import org.docx4j.wml.CTRel;
 import org.docx4j.wml.CTSettings;
-import org.docx4j.wml.CTTxbxContent;
-import org.docx4j.wml.Tbl;
 
 /**
  * Creates a WordprocessingML document from scratch,
  * and attaches a template
+ * 
+ * In Flat OPC terms, aim is to produce:
+ * 
+  <pkg:part pkg:name="/word/settings.xml" pkg:contentType="application/vnd.openxmlformats-officedocument.wordprocessingml.settings+xml">
+    <pkg:xmlData>
+      <w:settings xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:sl="http://schemas.openxmlformats.org/schemaLibrary/2006/main">
+        <w:attachedTemplate r:id="rId1"/>
+      </w:settings>
+    </pkg:xmlData>
+  </pkg:part>
+ *   <pkg:part pkg:name="/word/_rels/settings.xml.rels" pkg:contentType="application/vnd.openxmlformats-package.relationships+xml">
+    <pkg:xmlData>
+      <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+        <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/attachedTemplate" Target="file:///C:\Users\jharrop\AppData\Roaming\Microsoft\Templates\fabdocx-release-20101002B.dotm" TargetMode="External"/>
+      </Relationships>
+    </pkg:xmlData>
+  </pkg:part>
+
  * 
  * @author Jason Harrop
  * @version 1.0
