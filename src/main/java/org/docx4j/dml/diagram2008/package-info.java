c04e697a852f3a192c7ac320adc07a50b97ea488
Jason Harrop <jason@plutext.org>
2011-05-18 06:47:06 +0000

    JAXB model for dml-diagramDrawing

---
+++ src/main/java/org/docx4j/dml/diagram2008/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.microsoft.com/office/drawing/2008/diagram", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.docx4j.dml.diagram2008;
