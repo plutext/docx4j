98247211fb8038df87d73cbb67201cc1bf74611f
Jason Harrop <jason@plutext.org>
2011-06-02 04:27:41 +0000

    minOccurs & maxOccurs for items in nested lists.

--- src/main/java/org/opendope/SmartArt/dataHierarchy/Node.java
+++ src/main/java/org/opendope/SmartArt/dataHierarchy/Node.java
@@ -1,10 +1,11 @@
 
 package org.opendope.SmartArt.dataHierarchy;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
@@ -20,7 +21,7 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element ref="{http://opendope.org/SmartArt/DataHierarchy}node"/>
+ *         &lt;element ref="{http://opendope.org/SmartArt/DataHierarchy}node" maxOccurs="unbounded" minOccurs="0"/>
  *       &lt;/sequence>
  *       &lt;attribute name="id" use="required" type="{http://www.w3.org/2001/XMLSchema}unsignedByte" />
  *       &lt;attribute name="val" type="{http://www.w3.org/2001/XMLSchema}unsignedByte" />
@@ -41,8 +42,7 @@ import javax.xml.bind.annotation.XmlType;
 @XmlRootElement(name = "node")
 public class Node {
 
-    @XmlElement(required = true)
-    protected Node node;
+    protected List<Node> node;
     @XmlAttribute(required = true)
     @XmlSchemaType(name = "unsignedByte")
     protected short id;
@@ -59,25 +59,30 @@ public class Node {
     /**
      * Gets the value of the node property.
      * 
-     * @return
-     *     possible object is
-     *     {@link Node }
-     *     
-     */
-    public Node getNode() {
-        return node;
-    }
-
-    /**
-     * Sets the value of the node property.
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the node property.
      * 
-     * @param value
-     *     allowed object is
-     *     {@link Node }
-     *     
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getNode().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Node }
+     * 
+     * 
      */
-    public void setNode(Node value) {
-        this.node = value;
+    public List<Node> getNode() {
+        if (node == null) {
+            node = new ArrayList<Node>();
+        }
+        return this.node;
     }
 
     /**
