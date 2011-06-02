cd135acba67313d086263998230f0b02134cee36
Jason Harrop <jason@plutext.org>
2011-06-02 15:06:34 +0000

    Prospect of one template per level.

--- src/main/java/org/opendope/SmartArt/dataHierarchy/Node.java
+++ src/main/java/org/opendope/SmartArt/dataHierarchy/Node.java
@@ -7,6 +7,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -202,4 +203,14 @@ public class Node {
         this.sibTransContentRef = value;
     }
 
+	// Not generated
+	@XmlTransient
+	private int depth;
+	public int getDepth() {
+		return depth;
+	}
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+    
 }
