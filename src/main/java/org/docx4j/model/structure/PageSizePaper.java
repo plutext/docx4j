26521357d466927b6cfadd56621bddde46984b51
Jason Harrop <jason@plutext.org>
2011-06-10 08:16:16 +0000

    Support for common paper sizes.

---
+++ src/main/java/org/docx4j/model/structure/PageSizePaper.java
@@ -0,0 +1,23 @@
+package org.docx4j.model.structure;
+
+/**
+ * @since 2.7
+ */
+public enum PageSizePaper {
+
+		    LETTER("letter"),
+		    LEGAL("legal"),
+		    A3("A3"),
+		    A4("A4"),
+		    B4JIS("B4JIS");
+		    
+		    private final String value;
+
+		    PageSizePaper(String v) {
+		        value = v;
+		    }
+
+		    public String value() {
+		        return value;
+		    }
+	  }
