f55e23739126359fbba202c1fa2f95afe035e4fa
Jason Harrop <jason@plutext.org>
2011-06-12 09:55:15 +0000

    Extract a glox from an exemplar pptx or docx, plus a couple of supporting fixes.

--- docx4j/src/main/java/org/docx4j/dml/diagram/CTDiagramDefinitionHeader.java
+++ docx4j/src/main/java/org/docx4j/dml/diagram/CTDiagramDefinitionHeader.java
@@ -27,6 +27,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import org.docx4j.dml.CTOfficeArtExtensionList;
 
@@ -64,6 +65,7 @@ import org.docx4j.dml.CTOfficeArtExtensionList;
     "catLst",
     "extLst"
 })
+@XmlRootElement(name = "layoutDefHdr")
 public class CTDiagramDefinitionHeader {
 
     @XmlElement(required = true)
