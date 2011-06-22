1239d51e79680b4bafb7c91080884ac490caba2d
Jason Harrop <jason@plutext.org>
2011-06-22 03:32:17 +0000

    XPath enhancer tests

---
+++ docx4j/src/test/java/org/docx4j/xpathextend/grammar/TestConfiguration.java
@@ -0,0 +1,21 @@
+package org.docx4j.xpathextend.grammar;
+
+public class TestConfiguration {
+
+	private String prefix;
+	private int index;
+	
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public TestConfiguration(String prefix, int index) {
+		super();
+		this.prefix = prefix;
+		this.index = index;
+	}
+}
