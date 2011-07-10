79a4d94f4a4a0f82f8a273b89c93d5bbdfdb8d5d
Jason Harrop <jason@plutext.org>
2011-07-10 07:14:20 +0000

    Test class name must end in "Test"

--- docx4j/src/test/java/org/docx4j/openpackaging/parts/WordprocessingML/AlternateContentPreprocessorTest.java
+++ docx4j/src/test/java/org/docx4j/openpackaging/parts/WordprocessingML/AlternateContentPreprocessorTest.java
@@ -12,14 +12,14 @@ import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.junit.Test;
 import org.w3c.dom.Node;
 
-public class AlternateContentPreprocessor {
+public class AlternateContentPreprocessorTest {
 
 	/**
 	 * This tests that an oval wrapped in AlternateContent 
 	 * is resolved to the Fallback.
 	 */
 	@Test
-	public void testInvoice() throws Exception {
+	public void testAlternateContent() throws Exception {
 		
 		String inputfilepath = System.getProperty("user.dir") 
 			+ "/sample-docs/word/2010/2010-mcAlternateContent.docx";	    	
@@ -43,6 +43,31 @@ public class AlternateContentPreprocessor {
 				
 	}
 
+
+	@Test
+	public void testGlowAndAlternateContent() throws Exception {
+		
+		String inputfilepath = System.getProperty("user.dir") 
+			+ "/sample-docs/word/2010/2010-glow-then-AlternateContent.docx";	    	
+		
+		
+		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new java.io.File(inputfilepath));				
+
+		// Since the JAXB binding stuff seems to remember
+		// old artifacts, we'll first create a 'clean' object here
+		// from something we've marshalled to.  
+		org.w3c.dom.Document xmlNode = XmlUtils.marshaltoW3CDomDocument(
+				wordMLPackage.getMainDocumentPart().getJaxbElement());				
+		Binder<Node> binder = Context.jc.createBinder();
+		Object jaxbElement =  (org.docx4j.wml.Document) binder.unmarshal(xmlNode);
+						
+		List<Object> list = XmlUtils.getJAXBNodesViaXPath(binder, jaxbElement, 
+				"//w:pict/v:oval", false );		
+		int count = list.size();
+		
+		assertTrue("expected oval but got " + count, count==1 );
+				
+	}
 	
 	
 }
