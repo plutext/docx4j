1c80fc6fddf5b2791e39be2239d1892ca93f3255
Jason Harrop <jason@plutext.org>
2011-07-16 06:34:48 +0000

    Only set up FontMapper once.

--- docx4j/src/test/java/org/docx4j/convert/out/pdf/PdfMultipleThreads.java
+++ docx4j/src/test/java/org/docx4j/convert/out/pdf/PdfMultipleThreads.java
@@ -32,133 +32,140 @@ import org.docx4j.samples.AbstractSample;
  */
 public class PdfMultipleThreads extends AbstractSample {
 	
+	final static int TOTAL=6; // <---- number of threads
 	
-		public static boolean abort = false;
-	    
-	    public static void main(String[] args) 
-	            throws Exception {
-	    	
-	    	final int TOTAL=2;
-	    	Thread[] t = {null,null, null,null,null, null,null, null,null, null};
-	    	
-	    	JAXBContext blgh = org.docx4j.jaxb.Context.jc;
-	    	
-	    	long startTime = System.currentTimeMillis();
-	    	
+	public static boolean abort = false;
+    
+	// Set up font mapper, this can be shared between
+	// threads
+	static Mapper fontMapper = new IdentityPlusMapper();
+	
+    public static void main(String[] args) 
+            throws Exception {
+    	    	
+    	Thread[] t = new Thread[TOTAL+1];
+    	
+    	JAXBContext blgh = org.docx4j.jaxb.Context.jc;
+
+		PhysicalFont font 
+				= PhysicalFonts.getPhysicalFonts().get("Comic Sans MS");
+		fontMapper.getFontMappings().put("Calibri", font);
+    	
+		// OK, initial overhead done; let's start the test
+		
+    	long startTime = System.currentTimeMillis();
+    	
+    	for (int i=1; i<=TOTAL; i++) {
+	        t[i] = new Thread(new CreatePdf());
+	        t[i].setName("fo"+i);
+	        t[i].start();
+    	}
+    		        
+        boolean alive = true;
+        
+        while(alive) {
+
+        	alive = false;
 	    	for (int i=1; i<=TOTAL; i++) {
-		        t[i] = new Thread(new CreatePdf());
-		        t[i].setName("fo"+i);
-		        t[i].start();
+		        if (t[i].isAlive() ) {
+		        	alive = true;
+			    	if (abort) {
+			        	System.out.println(i + " is alive; trying to interrupt");
+			        	//t[i].interrupt();
+			        	t[i].stop();
+			    	} else {
+			        	System.out.println(i + " is alive");				    		
+			    	}
+		        	
+		        } else {
+		        	System.out.println(i + " is finished");			        	
+		        }
+		        
 	    	}
 	    	
-	        System.out.println("Waiting for CreatePdf thread to finish");
-	        //loop until MessageLoop thread exits
-	        
-	        boolean alive = true;
-	        
-	        while(alive) {
-
-	        	alive = false;
-		    	for (int i=1; i<=TOTAL; i++) {
-			        if (t[i].isAlive() ) {
-			        	alive = true;
-				    	if (abort) {
-				        	System.out.println(i + " is alive; trying to interrupt");
-				        	//t[i].interrupt();
-				        	t[i].stop();
-				    	} else {
-				        	System.out.println(i + " is alive");				    		
-				    	}
-			        	
-			        } else {
-			        	System.out.println(i + " is finished");			        	
-			        }
-			        
-		    	}
-		    	
-		    	
-		        System.out.println( reportMemory() );
-		    	Thread.sleep(4000);
-		    	
-	        }	
-	        long timeElapsed = System.currentTimeMillis() - startTime;
-	        int sec = Math.round(timeElapsed/ (TOTAL*1000) );
-        	System.out.println("All done!  " + timeElapsed);
-        	System.out.println("Average " + sec + "sec per thread");
-        	
-        	System.out.println( reportMemory() );
-        	
-        	System.out.println( "gc..");
-        	System.gc();        	
-        	System.out.println( reportMemory() );
-        	
-        	System.out.println( "gc..");
-        	System.gc();        	
-        	System.out.println( reportMemory() );
-        	
-	    }
-	    
-	    public static String reportMemory() {
 	    	
-	    	Runtime runtime = Runtime.getRuntime();
-
-	        NumberFormat format = NumberFormat.getInstance();
-
-	        StringBuilder sb = new StringBuilder();
-	        long maxMemory = runtime.maxMemory();
-	        long allocatedMemory = runtime.totalMemory();
-	        long freeMemory = runtime.freeMemory();
-
-	        //sb.append("free memory: " + format.format(freeMemory / (1024*1024)) + "M\n");
-	        sb.append("allocated memory: " + format.format(allocatedMemory / (1024*1024)) + "M\n");
-	        //sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / (1024*1024)) + "M\n");
-	        sb.append("--------------\n");	        
-	        sb.append("[ Xmx: " + format.format(maxMemory / (1024*1024)) + "M ]\n");
-	        
-	        return sb.toString();
-	    }
-	    
-    	 private static class CreatePdf implements Runnable {
-    		 
-    	 public void run() {	 
-    		 
-    		 try {
-    	        	
-	    	boolean save = true;
+	        System.out.println( reportMemory() );
+	    	Thread.sleep(4000);
 	    	
-	    	inputfilepath = System.getProperty("user.dir") + "/sample-docs/word/sample-docx.xml";
-	    	
-	    	
-			WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new java.io.File(inputfilepath));
-			
-			// Set up font mapper
-			Mapper fontMapper = new IdentityPlusMapper();
-			wordMLPackage.setFontMapper(fontMapper);
-			
-			// Example of mapping missing font Algerian to installed font Comic Sans MS
-			PhysicalFont font 
-					= PhysicalFonts.getPhysicalFonts().get("Comic Sans MS");
-			fontMapper.getFontMappings().put("Calibri", font);
-						
-			org.docx4j.convert.out.pdf.PdfConversion c 
-				= new org.docx4j.convert.out.pdf.viaXSLFO.Conversion(wordMLPackage);
-			
-			if (save) {
-				((org.docx4j.convert.out.pdf.viaXSLFO.Conversion)c).setSaveFO(
-						new java.io.File(
-									inputfilepath + Thread.currentThread().getName() + ".fo"));
-				OutputStream os = new java.io.FileOutputStream(
-									inputfilepath + Thread.currentThread().getName() + ".pdf");			
-				c.output(os);
-				System.out.println("Saved " + inputfilepath + Thread.currentThread().getName() + ".pdf");
-			} 
-    		 } catch (InterruptedException e) {
-    			 e.printStackTrace();
-    		 } catch (Exception e) {
-    			 PdfMultipleThreads.abort=true;
-    			 e.printStackTrace();
-    		 }
-	    }
+        }	
+        long timeElapsed = System.currentTimeMillis() - startTime;
+        int sec = Math.round(timeElapsed/ (TOTAL*1000) );
+    	System.out.println("All done!  " + timeElapsed);
+    	System.out.println("Average " + sec + "sec per thread");
+    	
+    	System.out.println( reportMemory() );
+    	
+    	System.out.println( "gc..");
+    	System.gc();        	
+    	System.out.println( reportMemory() );
+    	
+    	System.out.println( "gc..");
+    	System.gc();        	
+    	System.out.println( reportMemory() );
+    	
+    }
+    
+    public static String reportMemory() {
+    	
+    	Runtime runtime = Runtime.getRuntime();
+
+        NumberFormat format = NumberFormat.getInstance();
+
+        StringBuilder sb = new StringBuilder();
+        long maxMemory = runtime.maxMemory();
+        long allocatedMemory = runtime.totalMemory();
+        long freeMemory = runtime.freeMemory();
+
+        //sb.append("free memory: " + format.format(freeMemory / (1024*1024)) + "M\n");
+        sb.append("allocated memory: " + format.format(allocatedMemory / (1024*1024)) + "M\n");
+        //sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / (1024*1024)) + "M\n");
+        sb.append("--------------\n");	        
+        sb.append("[ Xmx: " + format.format(maxMemory / (1024*1024)) + "M ]\n");
+        
+        return sb.toString();
+    }
+    
+	 private static class CreatePdf implements Runnable {
+		 
+	 public void run() {	 
+		 
+		 try {
+	        	
+    	boolean save = true;
+    	
+    	inputfilepath = System.getProperty("user.dir") + "/sample-docs/word/sample-docx.xml";
+    	
+    	
+		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new java.io.File(inputfilepath));
+		
+//			// Set up font mapper
+//			Mapper fontMapper = new IdentityPlusMapper();
+		wordMLPackage.setFontMapper(fontMapper);
+//			
+//			// Example of mapping missing font Algerian to installed font Comic Sans MS
+//			PhysicalFont font 
+//					= PhysicalFonts.getPhysicalFonts().get("Comic Sans MS");
+//			fontMapper.getFontMappings().put("Calibri", font);
+					
+		org.docx4j.convert.out.pdf.PdfConversion c 
+			= new org.docx4j.convert.out.pdf.viaXSLFO.Conversion(wordMLPackage);
+		
+		if (save) {
+			((org.docx4j.convert.out.pdf.viaXSLFO.Conversion)c).setSaveFO(
+					new java.io.File(
+								inputfilepath + Thread.currentThread().getName() + ".fo"));
+			OutputStream os = new java.io.FileOutputStream(
+								inputfilepath + Thread.currentThread().getName() + ".pdf");			
+			c.output(os);
+			System.out.println("Saved " + inputfilepath + Thread.currentThread().getName() + ".pdf");
+		} 
+		 } catch (InterruptedException e) {
+			 e.printStackTrace();
+		 } catch (Exception e) {
+			 PdfMultipleThreads.abort=true;
+			 e.printStackTrace();
+		 }
+    }
 	    
 	    
 	}
