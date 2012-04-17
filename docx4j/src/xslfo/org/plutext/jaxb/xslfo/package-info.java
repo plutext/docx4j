663eac309efd77fa6d3630a91941e441ee16c53e
Jason Harrop <jason@plutext.org>
2012-04-17 22:21:36 +0000

    JAXB representation of XSL FO is now in a separate project, so remove src/xslfo

--- docx4j/src/xslfo/org/plutext/jaxb/xslfo/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.w3.org/1999/XSL/Format", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.plutext.jaxb.xslfo;
