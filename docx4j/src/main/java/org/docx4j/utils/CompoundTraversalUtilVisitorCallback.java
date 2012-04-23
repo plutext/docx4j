e191827fa7a86c422e99bf6245cc975bf91db582
Jason Harrop <jason@plutext.org>
2012-04-23 17:54:43 +0000

    Arnaud Kleinpeter's patch allowing the CompoundTraversalUtilVisitorCallback to handle inheritance, just as in the Single one.

--- docx4j/src/main/java/org/docx4j/utils/CompoundTraversalUtilVisitorCallback.java
+++ docx4j/src/main/java/org/docx4j/utils/CompoundTraversalUtilVisitorCallback.java
@@ -4,26 +4,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Map.Entry;
 
 /** 
  * Use this if there is more than one object type (eg Tables and Paragraphs)
  * you are interested in doing something with during the traversal.
  * 
- *  @author alberto */
+ * @author alberto
+ */
 public class CompoundTraversalUtilVisitorCallback extends
 		AbstractTraversalUtilVisitorCallback {
-	
+
 	Map<Class, List<TraversalUtilVisitor>> visitorMap = null;
 
 	public CompoundTraversalUtilVisitorCallback(
 			List<TraversalUtilVisitor> visitorList) {
-		
+
 		visitorMap = setupTraversalVistorMap(visitorList);
 	}
 
 	protected Map<Class, List<TraversalUtilVisitor>> setupTraversalVistorMap(
 			List<TraversalUtilVisitor> visitorList) {
-		
+
 		Map<Class, List<TraversalUtilVisitor>> ret = new HashMap<Class, List<TraversalUtilVisitor>>();
 		Class visitorClass = null;
 		List<TraversalUtilVisitor> classVisitorList = null;
@@ -46,12 +48,15 @@ public class CompoundTraversalUtilVisitorCallback extends
 
 	@Override
 	protected List<Object> apply(Object child, Object parent, List siblings) {
-		
-		List<TraversalUtilVisitor> classVisitorList = visitorMap.get(child
-				.getClass());
-		if (classVisitorList != null) {
-			for (TraversalUtilVisitor visitor : classVisitorList) {
-				visitor.apply(child, parent, siblings);
+		for (final Entry<Class, List<TraversalUtilVisitor>> entrySet : visitorMap.entrySet()) {
+			final Class currentClass = entrySet.getKey();
+			if (currentClass.isAssignableFrom(child.getClass())) {
+				List<TraversalUtilVisitor> classVisitorList = entrySet.getValue();
+				if (null != classVisitorList) {
+					for (TraversalUtilVisitor visitor : classVisitorList) {
+						visitor.apply(child, parent, siblings);
+					}
+				}
 			}
 		}
 		return null;
