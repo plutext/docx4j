40056bc4309d7d1bf369e72899505eab5c2b1a83
Jason Harrop <jason@plutext.org>
2012-05-18 14:22:11 +1000

    Convenience method getSharedStrings()

--- src/xlsx4j/java/org/docx4j/openpackaging/parts/SpreadsheetML/WorkbookPart.java
+++ src/xlsx4j/java/org/docx4j/openpackaging/parts/SpreadsheetML/WorkbookPart.java
@@ -1,14 +1,14 @@
 package org.docx4j.openpackaging.parts.SpreadsheetML;
 
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
-import org.docx4j.openpackaging.parts.JaxbXmlPart;
+import org.docx4j.openpackaging.parts.Part;
 import org.docx4j.openpackaging.parts.PartName;
 import org.docx4j.openpackaging.parts.relationships.Namespaces;
-import org.xlsx4j.jaxb.Context;
-import org.xlsx4j.sml.CTCalcChain;
 import org.xlsx4j.sml.Workbook;
 
 public class WorkbookPart  extends JaxbSmlPart<Workbook> {
+	
+	protected SharedStrings sharedStrings;
 	
 	public WorkbookPart(PartName partName) throws InvalidFormatException {
 		super(partName);
@@ -29,6 +29,44 @@ public class WorkbookPart  extends JaxbSmlPart<Workbook> {
 		// Used when this Part is added to a rels 
 		setRelationshipType(Namespaces.SPREADSHEETML_WORKBOOK);
 		
+	}
+	
+	public SharedStrings getSharedStrings() {
+		return sharedStrings;
+	}
+	
+	
+	public boolean setPartShortcut(Part part) {
+		
+		if (part == null ){
+			return false;
+		} else {
+			return setPartShortcut(part, part.getRelationshipType() );
+		}
+		
+	}	
+		
+	public boolean setPartShortcut(Part part, String relationshipType) {
+		
+		// Since each part knows its relationshipsType,
+		// why is this passed in as an arg?
+		// Answer: where the relationshipType is ascertained
+		// from the rel itself, it is the most authoritative.
+		// Note that we normally use the info in [Content_Types]
+		// to create a part of the correct type.  This info
+		// will not necessary correspond to the info in the rel!
+		
+		if (relationshipType==null) {
+			log.warn("trying to set part shortcut against a null relationship type.");
+			return false;
+		}
+		
+		if (relationshipType.equals(Namespaces.SPREADSHEETML_SHARED_STRINGS)) {
+			sharedStrings = (SharedStrings)part;
+			return true;			
+		} else {	
+			return false;
+		}
 	}
 
 }
