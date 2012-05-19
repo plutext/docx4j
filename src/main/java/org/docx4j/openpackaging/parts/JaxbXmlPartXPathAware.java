02d9f18ae48ed5f526beb52440080a25899d5512
Jason Harrop <jason@plutext.org>
2012-05-19 13:30:12 +1000

    Need to unwrap when unmarshalling, so we get CTEndnote instead of JAXBElement.

--- src/main/java/org/docx4j/openpackaging/parts/JaxbXmlPartXPathAware.java
+++ src/main/java/org/docx4j/openpackaging/parts/JaxbXmlPartXPathAware.java
@@ -3,6 +3,7 @@ package org.docx4j.openpackaging.parts;
 import java.util.List;
 
 import javax.xml.bind.Binder;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.UnmarshalException;
 import javax.xml.bind.Unmarshaller;
@@ -126,72 +127,82 @@ public abstract class JaxbXmlPartXPathAware<E> extends JaxbXmlPart<E> {
 			binder.setEventHandler(eventHandler);
 			
 			try {
-				jaxbElement =  (E) binder.unmarshal( doc );
+				jaxbElement =  (E) XmlUtils.unwrap(binder.unmarshal( doc ));
+					// Unwrap, so we have eg CTEndnotes, not JAXBElement
 			} catch (UnmarshalException ue) {
-				log.info("encountered unexpected content; pre-processing");
-				/* Always try our preprocessor, since if what is first encountered is
-				 * eg:
-				 * 
-			          <w14:glow w14:rad="101600"> ...
-				 *
-				 * the error would be:
-				 *  
-				 *    unexpected element (uri:"http://schemas.microsoft.com/office/word/2010/wordml", local:"glow")
-				 *
-				 * but there could well be mc:AlternateContent somewhere 
-				 * further down in the document.
-				 */
-
-				// mimic docx4j 2.7.0 and earlier behaviour; this will 
-				// drop w14:glow etc; the preprocessor doesn't need to 
-				// do that				
-				eventHandler.setContinue(true);
 				
-				// There is no JAXBResult(binder),
-				// so use a 
-				DOMResult result = new DOMResult();
+				if (is.markSupported() ) {
+					// When reading from zip, we use a ByteArrayInputStream,
+					// which does support this.
 				
-				Templates mcPreprocessorXslt = JaxbValidationEventHandler.getMcPreprocessor();
-				XmlUtils.transform(doc, mcPreprocessorXslt, null, result);
-				
-				doc = (org.w3c.dom.Document)result.getNode();
-				try {				
-					jaxbElement =  (E) binder.unmarshal( doc );
-				} catch (ClassCastException cce) {
-					log.warn("Binder not available for this docx");
-					Unmarshaller u = jc.createUnmarshaller();
-					jaxbElement = (E) u.unmarshal( doc );		
-					/* 
-					 * Work around for issue with JAXB binder, in Java 1.6 
-					 * encountered with /src/test/resources/jaxb-binder-issue.docx 
-					 * See http://old.nabble.com/BinderImpl.associativeUnmarshal-ClassCastException-casting-to-JAXBElement-td32456585.html
-					 * and  http://java.net/jira/browse/JAXB-874
+					log.info("encountered unexpected content; pre-processing");
+					/* Always try our preprocessor, since if what is first encountered is
+					 * eg:
 					 * 
-					 * java.lang.ClassCastException: org.docx4j.wml.PPr cannot be cast to javax.xml.bind.JAXBElement
-						at com.sun.xml.internal.bind.v2.runtime.ElementBeanInfoImpl$IntercepterLoader.intercept(Unknown Source)
-						at com.sun.xml.internal.bind.v2.runtime.unmarshaller.UnmarshallingContext.endElement(Unknown Source)
-						at com.sun.xml.internal.bind.v2.runtime.unmarshaller.InterningXmlVisitor.endElement(Unknown Source)
-						at com.sun.xml.internal.bind.v2.runtime.unmarshaller.SAXConnector.endElement(Unknown Source)
-						at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
-						at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
-						at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
-						at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
-						at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
-						at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
-						at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
-						at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
-						at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
-						at com.sun.xml.internal.bind.unmarshaller.DOMScanner.scan(Unknown Source)
-						at com.sun.xml.internal.bind.unmarshaller.DOMScanner.scan(Unknown Source)
-						at com.sun.xml.internal.bind.unmarshaller.DOMScanner.scan(Unknown Source)
-						at com.sun.xml.internal.bind.v2.runtime.BinderImpl.associativeUnmarshal(Unknown Source)
-						at com.sun.xml.internal.bind.v2.runtime.BinderImpl.unmarshal(Unknown Source)
-						at org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart.unmarshal(MainDocumentPart.java:321)
+				          <w14:glow w14:rad="101600"> ...
+					 *
+					 * the error would be:
+					 *  
+					 *    unexpected element (uri:"http://schemas.microsoft.com/office/word/2010/wordml", local:"glow")
+					 *
+					 * but there could well be mc:AlternateContent somewhere 
+					 * further down in the document.
 					 */
+	
+					// mimic docx4j 2.7.0 and earlier behaviour; this will 
+					// drop w14:glow etc; the preprocessor doesn't need to 
+					// do that				
+					eventHandler.setContinue(true);
 					
+					// There is no JAXBResult(binder),
+					// so use a 
+					DOMResult result = new DOMResult();
+					
+					Templates mcPreprocessorXslt = JaxbValidationEventHandler.getMcPreprocessor();
+					XmlUtils.transform(doc, mcPreprocessorXslt, null, result);
+					
+					doc = (org.w3c.dom.Document)result.getNode();
+					try {				
+						jaxbElement =  (E) XmlUtils.unwrap(binder.unmarshal( doc ));
+					} catch (ClassCastException cce) {
+						/* 
+						 * Work around for issue with JAXB binder, in Java 1.6 
+						 * encountered with /src/test/resources/jaxb-binder-issue.docx 
+						 * See http://old.nabble.com/BinderImpl.associativeUnmarshal-ClassCastException-casting-to-JAXBElement-td32456585.html
+						 * and  http://java.net/jira/browse/JAXB-874
+						 * 
+						 * java.lang.ClassCastException: org.docx4j.wml.PPr cannot be cast to javax.xml.bind.JAXBElement
+							at com.sun.xml.internal.bind.v2.runtime.ElementBeanInfoImpl$IntercepterLoader.intercept(Unknown Source)
+							at com.sun.xml.internal.bind.v2.runtime.unmarshaller.UnmarshallingContext.endElement(Unknown Source)
+							at com.sun.xml.internal.bind.v2.runtime.unmarshaller.InterningXmlVisitor.endElement(Unknown Source)
+							at com.sun.xml.internal.bind.v2.runtime.unmarshaller.SAXConnector.endElement(Unknown Source)
+							at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
+							at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
+							at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
+							at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
+							at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
+							at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
+							at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
+							at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
+							at com.sun.xml.internal.bind.unmarshaller.DOMScanner.visit(Unknown Source)
+							at com.sun.xml.internal.bind.unmarshaller.DOMScanner.scan(Unknown Source)
+							at com.sun.xml.internal.bind.unmarshaller.DOMScanner.scan(Unknown Source)
+							at com.sun.xml.internal.bind.unmarshaller.DOMScanner.scan(Unknown Source)
+							at com.sun.xml.internal.bind.v2.runtime.BinderImpl.associativeUnmarshal(Unknown Source)
+							at com.sun.xml.internal.bind.v2.runtime.BinderImpl.unmarshal(Unknown Source)
+							at org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart.unmarshal(MainDocumentPart.java:321)
+						 */
+	
+						log.warn("Binder not available for this docx");
+						Unmarshaller u = jc.createUnmarshaller();
+						jaxbElement = (E) XmlUtils.unwrap(u.unmarshal( doc ));		
+						
+					}
+				} else {
+					log.error(ue);
+					log.error(".. and mark not supported");
+					throw ue;
 				}
-				
-				
 			}
 			
 			return jaxbElement;
@@ -216,11 +227,11 @@ public abstract class JaxbXmlPartXPathAware<E> extends JaxbXmlPart<E> {
 			binder.setEventHandler(eventHandler);
 			
 			try {
-				jaxbElement =  (E) binder.unmarshal( el );
+				jaxbElement =  (E) XmlUtils.unwrap(binder.unmarshal( el ));
 			} catch (UnmarshalException ue) {
 				log.info("encountered unexpected content; pre-processing");
+				org.w3c.dom.Document doc = null;
 				try {
-					org.w3c.dom.Document doc;
 					if (el instanceof org.w3c.dom.Document) {
 						doc = (org.w3c.dom.Document) el;
 					} else {
@@ -235,8 +246,11 @@ public abstract class JaxbXmlPartXPathAware<E> extends JaxbXmlPart<E> {
 							.getMcPreprocessor();
 					XmlUtils.transform(doc, mcPreprocessorXslt, null, result);
 					doc = (org.w3c.dom.Document) result.getNode();
-					jaxbElement = (E) binder
-							.unmarshal(doc);
+					jaxbElement = (E) XmlUtils.unwrap(binder.unmarshal(doc));
+				} catch (ClassCastException cce) {
+					log.warn("Binder not available for this docx");
+					Unmarshaller u = jc.createUnmarshaller();
+					jaxbElement = (E) XmlUtils.unwrap(u.unmarshal( doc ));		
 				} catch (Exception e) {
 					throw new JAXBException("Preprocessing exception", e);
 				}
