12192714b3787f11857c0d93ac0a753d9cf23eaa
Jason Harrop <jason@plutext.org>
2012-05-20 12:45:34 +1000

    Change log pattern to include method name (%M) and line number (%L)

--- src/main/java/org/docx4j/utils/Log4jConfigurator.java
+++ src/main/java/org/docx4j/utils/Log4jConfigurator.java
@@ -2,13 +2,26 @@ package org.docx4j.utils;
 
 import java.util.Enumeration;
 
+import org.apache.log4j.Appender;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.docx4j.Docx4jProperties;
 
-//From http://wiki.apache.org/logging-log4j/UsefulCode
+/* From http://wiki.apache.org/logging-log4j/UsefulCode
+ * 
+ * This will be used if log4.xml config file is
+ * not found on your path, unless 
+ * Property("docx4j.Log4j.Configurator.disabled") is set 
+ * to true.
+ * 
+ * There is a log4j config file in src/main/resources;
+ * if that is on your path, that is where your log4j
+ * config will come from.
+ * 
+ */
 public class Log4jConfigurator {
 
     public synchronized static void configure() {
@@ -24,6 +37,10 @@ public class Log4jConfigurator {
 	        // So now we should have a ConsoleAppender
 	        // we don't want debug level logging.
 	        Logger.getRootLogger().setLevel(Level.INFO);
+
+	        PatternLayout layout = new PatternLayout("%p %c %x.%M line %L - %m%n");
+	    	((Appender)Logger.getRootLogger().getAllAppenders().nextElement()).setLayout(layout);
+	        
 	        Logger log = Logger.getLogger(Log4jConfigurator.class);
 	        log.info("Since your log4j configuration (if any) was not found, docx4j has configured log4j automatically.");
 	        
