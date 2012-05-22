e768125226fbd5d6179379addc6bc224b81d98d6
Jason Harrop <jason@plutext.org>
2012-05-22 17:55:49 +1000

    Tidy up sample

--- src/main/java/org/docx4j/samples/XPathQuery.java
+++ src/main/java/org/docx4j/samples/XPathQuery.java
@@ -10,19 +10,36 @@ public class XPathQuery {
 
 	
 	/**
-	 * @param args
+	 * Example of how to find an object in document.xml
+	 * via XPath.
+	 * 
+	 * As explained in Getting Started, there are limitations 
+	 * in the JAXB reference implementation which make this
+	 * approach buggy:
+	 * 
+	 * 1. the xpath expressions are evaluated against the XML 
+	 * document as it was when first opened in docx4j.  You 
+	 * can update the associated XML document once only, by
+	 * passing true into getJAXBNodesViaXPath. Updating it again 
+	 * (with current JAXB 2.1.x or 2.2.x) will cause an error.
+	 * 
+	 * 2. For some objects,JAXB can’t get parent (with getParent)
+	 * 
+	 * 3. For some document, JAXB can’t set up the XPath at all!
+	 * 
+	 * If these problems affect you, consider using TraversalUtil;
+	 * see the OpenMainDocumentAndTraverse sample.
+	 * 
 	 */
 	public static void main(String[] args) throws Exception {
 
 		String inputfilepath = System.getProperty("user.dir") + "/sample-docs/word/sample-docx.xml";
-		//String inputfilepath = System.getProperty("user.dir") + "//tmp//modelo.docx";
-//    	String inputfilepath = "/home/dev/workspace/docx4j/sample-docs/fo-200912.xml";
 				
 		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new java.io.File(inputfilepath));		
 		MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
 				
-		//String xpath = "//w:t";
-		String xpath = "//w:r[w:t[contains(text(),'scaled')]]";
+		String xpath = "//w:t[contains(text(),'scaled')]";
+		//String xpath = "//w:r[w:t[contains(text(),'scaled')]]";
 		
 		List<Object> list = documentPart.getJAXBNodesViaXPath(xpath, false);
 		
@@ -30,12 +47,11 @@ public class XPathQuery {
 		
 		for (Object o : list) {
 			
-			System.out.println(o.getClass().getName() );
+			//System.out.println(o.getClass().getName() );
 			
 			Object o2 = XmlUtils.unwrap(o);
 			// this is ok, provided the results of the Callback
-			// won't be marshalled
-			
+			// won't be marshalled			
 			
 			if (o2 instanceof org.docx4j.wml.Text) {
 				
@@ -43,9 +59,9 @@ public class XPathQuery {
 				
 				System.out.println( txt.getValue() );
 				
-				Object parent = txt.getParent();
-			
-				System.out.println( "parent: " +  XmlUtils.unwrap(parent).getClass().getName() );
+				// Demonstrate the getParent bug
+				//Object parent = txt.getParent();			
+				//System.out.println( "parent: " +  XmlUtils.unwrap(parent).getClass().getName() );
 			} else {
 				System.out.println( XmlUtils.marshaltoString(o, true, true));
 			}
