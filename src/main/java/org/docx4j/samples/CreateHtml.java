12bf8f44969fff4b3fd9cf54ed1573e29842477f
Jason Harrop <jason@plutext.org>
2012-05-22 18:25:41 +1000

    tidy up this sample

--- src/main/java/org/docx4j/samples/CreateHtml.java
+++ src/main/java/org/docx4j/samples/CreateHtml.java
@@ -26,12 +26,16 @@ import org.docx4j.convert.out.Containerization;
 import org.docx4j.convert.out.html.AbstractHtmlExporter;
 import org.docx4j.convert.out.html.HtmlExporterNG2;
 import org.docx4j.convert.out.html.SdtWriter;
-import org.docx4j.convert.out.html.TagClass;
 import org.docx4j.convert.out.html.TagSingleBox;
 import org.docx4j.convert.out.html.AbstractHtmlExporter.HtmlSettings;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 
 /**
+ * This sample uses XSLT (and Xalan) to 
+ * produce HTML output.  (There is also
+ * HtmlExporterNonXSLT for environments where 
+ * that is not desirable eg Android).
+ * 
  * If the source docx contained a WMF, that
  * will get converted to inline SVG.  In order
  * to see the SVG in your browser, you'll need 
@@ -40,56 +44,66 @@ import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
  *
  */
 public class CreateHtml extends AbstractSample {
-	    
-	    public static void main(String[] args) 
-	            throws Exception {
-	    	
-			try {
-				getInputFilePath(args);
-			} catch (IllegalArgumentException e) {
-		    	//inputfilepath = System.getProperty("user.dir") + "/docs/Docx4j_GettingStarted.xml";
-		    	inputfilepath = System.getProperty("user.dir") + "/sample-docs/word/sample-docx.xml";
-			}
-			System.out.println(inputfilepath);	    	
-	    	
-	    	boolean save = true;	    	
-			
-			// Load .docx or Flat OPC .xml
-			WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new java.io.File(inputfilepath));
-	    	
-			AbstractHtmlExporter exporter = new HtmlExporterNG2(); 	
-			
-	    	HtmlSettings htmlSettings = new HtmlSettings();
-	    	
-	    	htmlSettings.setImageDirPath(inputfilepath + "_files"); 
-	    	htmlSettings.setImageTargetUri(inputfilepath.substring(inputfilepath.lastIndexOf("/")+1) 
-	    			+ "_files");
-	    	
-	    	htmlSettings.setUserBodyTop("<H1>TOP!</H1>");
-	    	htmlSettings.setUserBodyTail("<H1>TAIL!</H1>");
-			
-			// Sample sdt tag handler (tag handlers insert specific
-			// html depending on the contents of an sdt's tag).  
-			// This will only have an effect if the sdt tag contains
-			// the string @class=XXX
-//			SdtWriter.registerTagHandler("@class", new TagClass() );
-			
-			SdtWriter.registerTagHandler(Containerization.TAG_BORDERS, new TagSingleBox() );
-			SdtWriter.registerTagHandler(Containerization.TAG_SHADING, new TagSingleBox() );
-			
-			OutputStream os; 
-			if (save) {
-				os = new java.io.FileOutputStream(inputfilepath + ".html");
-			} else {
-				os = System.out;
-
-			}
-			
-			javax.xml.transform.stream.StreamResult result = new javax.xml.transform.stream.StreamResult(os);
-			exporter.html(wordMLPackage, result, htmlSettings);
-			if (save) {
-				System.out.println("Saved: " + inputfilepath + ".html using " +  exporter.getClass().getName() );
-			}
-	        	        
-	    }
+	
+	// Config for non-command line version
+	static {
+		
+    	//inputfilepath = System.getProperty("user.dir") + "/docs/Docx4j_GettingStarted.xml";
+    	inputfilepath = System.getProperty("user.dir") + "/sample-docs/word/sample-docx.xml";
+		
+		save = true;
 	}
+	
+	static boolean save;
+	    
+    public static void main(String[] args) 
+            throws Exception {
+
+		try {
+			getInputFilePath(args);
+		} catch (IllegalArgumentException e) {
+		}
+    	
+		System.out.println(inputfilepath);	    	
+		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new java.io.File(inputfilepath));
+
+		// HTML exporter setup
+		AbstractHtmlExporter exporter = new HtmlExporterNG2(); 	
+		
+		// .. the HtmlSettings object
+    	HtmlSettings htmlSettings = new HtmlSettings();
+    	
+    	htmlSettings.setImageDirPath(inputfilepath + "_files"); 
+    	htmlSettings.setImageTargetUri(inputfilepath.substring(inputfilepath.lastIndexOf("/")+1) 
+    			+ "_files");
+    	
+    	htmlSettings.setUserBodyTop("<H1>TOP!</H1>");
+    	htmlSettings.setUserBodyTail("<H1>TAIL!</H1>");
+		
+		// Sample sdt tag handler (tag handlers insert specific
+		// html depending on the contents of an sdt's tag).  
+		// This will only have an effect if the sdt tag contains
+		// the string @class=XXX
+//			SdtWriter.registerTagHandler("@class", new TagClass() );
+		
+		SdtWriter.registerTagHandler(Containerization.TAG_BORDERS, new TagSingleBox() );
+		SdtWriter.registerTagHandler(Containerization.TAG_SHADING, new TagSingleBox() );
+		
+		// exporter writes to a Result object.		
+		OutputStream os; 
+		if (save) {
+			os = new java.io.FileOutputStream(inputfilepath + ".html");
+		} else {
+			os = System.out;
+		}		
+		javax.xml.transform.stream.StreamResult result = new javax.xml.transform.stream.StreamResult(os);
+		
+		// OK, do it
+		exporter.html(wordMLPackage, result, htmlSettings);
+		
+		if (save) {
+			System.out.println("Saved: " + inputfilepath + ".html using " +  exporter.getClass().getName() );
+		}
+        	        
+    }
+}
