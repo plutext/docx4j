cf46ae413a5f5bbf664357f03022efaf2529773a
Jason Harrop <jason@plutext.org>
2012-08-15 18:30:21 +1000

    OpenDoPE utility methods

--- src/main/java/org/docx4j/openpackaging/parts/opendope/ConditionsPart.java
+++ src/main/java/org/docx4j/openpackaging/parts/opendope/ConditionsPart.java
@@ -1,8 +1,13 @@
 package org.docx4j.openpackaging.parts.opendope;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.xml.bind.JAXBContext;
 
 import org.apache.log4j.Logger;
+import org.docx4j.XmlUtils;
+import org.docx4j.model.datastorage.InputIntegrityException;
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
 import org.docx4j.openpackaging.parts.CustomXmlDataStoragePart;
 import org.docx4j.openpackaging.parts.PartName;
@@ -36,4 +41,56 @@ public class ConditionsPart extends JaxbCustomXmlDataStoragePart<org.opendope.co
 		return null;
 	}
 
+	/**
+	 * Extracts xpath object from condition.  
+	 * 
+	 * Note: this does NOT build an xpath corresponding to the condition
+	 * value, but rather, is typically used to identify
+	 * questions associated with the condition.
+	 * @param c
+	 * @param xPaths
+	 * @return
+	 * @throws InputIntegrityException
+	 */
+	public static List<org.opendope.xpaths.Xpaths.Xpath> getXPathsUsedInCondition(Condition c, org.opendope.xpaths.Xpaths xPaths) 
+		throws InputIntegrityException {
+		
+		List<org.opendope.xpaths.Xpaths.Xpath> xpaths = new ArrayList<org.opendope.xpaths.Xpaths.Xpath>(); 
+		
+		if (c.getXpathref()!=null) {
+			xpaths.add(XPathsPart.getXPathById(xPaths, c.getXpathref().getId())); 	
+			return xpaths;
+		} else if (c.getNot()!=null) {
+			
+			if (c.getNot().getXpathref()!=null) {
+				xpaths.add(XPathsPart.getXPathById(xPaths, c.getNot().getXpathref().getId())); 	
+				return xpaths;
+			} else {
+				throw new InputIntegrityException("Couldn't extract xpath from not in condition " + XmlUtils.marshaltoString(c, true) );							
+			}
+		}
+		throw new InputIntegrityException("Couldn't extract xpath from condition " + XmlUtils.marshaltoString(c, true) );
+		// TODO handle complex conditions
+	}	
+	
+	public static String buildEffectiveXPath(Condition c, org.opendope.xpaths.Xpaths xPaths) 
+			throws InputIntegrityException {
+			
+			//StringBuilder effectiveXPath = new StringBuilder(); 
+			
+			if (c.getXpathref()!=null) {
+				return XPathsPart.getXPathById(xPaths, c.getXpathref().getId()).getDataBinding().getXpath(); 	
+			} else if (c.getNot()!=null) {
+				
+				if (c.getNot().getXpathref()!=null) {
+					return "not(" + XPathsPart.getXPathById(xPaths, c.getNot().getXpathref().getId()).getDataBinding().getXpath()
+								+ ")";
+				} else {
+					throw new InputIntegrityException("Couldn't extract xpath from not in condition " + XmlUtils.marshaltoString(c, true) );							
+				}
+			}
+			throw new InputIntegrityException("Couldn't extract xpath from condition " + XmlUtils.marshaltoString(c, true) );
+			// TODO handle complex conditions
+		}	
+	
 }
