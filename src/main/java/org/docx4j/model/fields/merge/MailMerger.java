5e6dfec8cca87a5e10488d952119919d6693d738
Jason Harrop <jason@plutext.org>
2012-08-15 18:25:23 +1000

    Move main method to samples.

--- src/main/java/org/docx4j/model/fields/merge/MailMerger.java
+++ src/main/java/org/docx4j/model/fields/merge/MailMerger.java
@@ -303,42 +303,5 @@ public class MailMerger {
 		}
 	}
 	
-	/**
-	 * @param args
-	 * @throws Exception 
-	 */
-	public static void main(String[] args) throws Exception {
-		
-		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(
-				new java.io.File(
-						System.getProperty("user.dir") + "/src/test/resources/MERGEFIELD.docx"));
-		
-		List<Map<DataFieldName, String>> data = new ArrayList<Map<DataFieldName, String>>();
-
-		Map<DataFieldName, String> map = new HashMap<DataFieldName, String>();
-		map.put( new DataFieldName("KundenNAme"), "Daffy duck");
-		map.put( new DataFieldName("Kundenname"), "Plutext");
-		map.put(new DataFieldName("Kundenstrasse"), "Bourke Street");
-		
-		data.add(map);
-				
-		map = new HashMap<DataFieldName, String>();
-		map.put( new DataFieldName("Kundenname"), "Jason");
-		map.put(new DataFieldName("Kundenstrasse"), "Collins Street");
-		
-		data.add(map);		
-		
-		
-		System.out.println(XmlUtils.marshaltoString(wordMLPackage.getMainDocumentPart().getJaxbElement(), true, true));
-
-		WordprocessingMLPackage output = org.docx4j.model.fields.merge.MailMerger.getConsolidatedResultCrude(wordMLPackage, data);
-		
-		
-		System.out.println(XmlUtils.marshaltoString(output.getMainDocumentPart().getJaxbElement(), true, true));
-		
-		output.save(new java.io.File(
-				System.getProperty("user.dir") + "/mergefield1-OUT.docx") );
-		
-	}
 
 }
