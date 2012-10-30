a5e7ca897d5b17501c20e2291859b715cae01207
Jeff Beard <jeff.beard1@gmail.com>
2012-10-30 17:16:52 -0600

    Added XmlRootElement annotation that was causing an Exception when attempting to write an Excel spreadsheet to a disk file. In this use case, the spreadsheet was embedded in a PowerPoint slide as the backing store for a chart.

--- src/xlsx4j/java/org/xlsx4j/sml/CTTable.java
+++ src/xlsx4j/java/org/xlsx4j/sml/CTTable.java
@@ -25,6 +25,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
 
@@ -82,6 +83,7 @@ import javax.xml.bind.annotation.XmlType;
     "tableStyleInfo",
     "extLst"
 })
+@XmlRootElement
 public class CTTable {
 
     protected CTAutoFilter autoFilter;
