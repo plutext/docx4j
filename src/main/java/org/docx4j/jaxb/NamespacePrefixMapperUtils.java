7c7f8beb459a0cc53f1650a54110bac936c15abe
Jason Harrop <jason@plutext.org>
2012-11-03 08:06:02 +1100

    tune logging (not an error)

--- src/main/java/org/docx4j/jaxb/NamespacePrefixMapperUtils.java
+++ src/main/java/org/docx4j/jaxb/NamespacePrefixMapperUtils.java
@@ -56,11 +56,11 @@ public class NamespacePrefixMapperUtils {
 			prefixMapper = new NamespacePrefixMapperSunInternal();
 			return prefixMapper;
 		} catch (java.lang.NoClassDefFoundError notJava6) {
-			log.error(notJava6.getMessage() + " .. trying RI.");
+			log.warn(notJava6.getMessage() + " .. trying RI.");
 			return tryUsingRI(m);			
 		} catch (javax.xml.bind.PropertyException notJava6) {
 			// OpenJDK (1.6.0_23) does this
-			log.error(notJava6.getMessage() + " .. trying RI.");
+			log.warn(notJava6.getMessage() + " .. trying RI.");
 			return tryUsingRI(m);			
 		}
 	}
