b4d9cb753899bb191f5f96bd1e8b9d345ce51963
Jason Harrop <jason@plutext.org>
2012-11-04 11:58:07 +1100

    Use file URL

--- src/main/java/org/docx4j/convert/out/pdf/viaXSLFO/PDFConversionImageHandler.java
+++ src/main/java/org/docx4j/convert/out/pdf/viaXSLFO/PDFConversionImageHandler.java
@@ -1,12 +1,17 @@
 package org.docx4j.convert.out.pdf.viaXSLFO;
 
 import java.io.File;
+import java.net.MalformedURLException;
 
+import org.apache.log4j.Logger;
 import org.docx4j.model.images.FileConversionImageHandler;
 
 /** This is a File-based ImageHandler, for generating images used in FO/PDF-documents
  */
 public class PDFConversionImageHandler extends FileConversionImageHandler {
+	
+	private final static Logger log = Logger.getLogger(PDFConversionImageHandler.class);
+	
 
 	/** Create a PDFConversionImageHandler, 
 	 * the images are generated in the java.io.tmpdir directory, runs are differentiated with an uuid
@@ -26,6 +31,12 @@ public class PDFConversionImageHandler extends FileConversionImageHandler {
 
 	@Override
 	protected String setupImageUri(File imageFile) {
-		return imageFile.getAbsolutePath();
+		// file:///"
+		try {
+			return imageFile.toURI().toURL().toString();
+		} catch (MalformedURLException e) {
+			log.error(e);
+			return imageFile.getName();
+		}
 	}
 }
