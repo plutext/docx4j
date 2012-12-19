8c075eefeb805a0cf2b018bc6524757eba099c0f
Jason Harrop <jason@plutext.org>
2012-12-19 15:23:50 +1100

    No change

--- src/main/java/org/docx4j/openpackaging/contenttype/ContentTypeManager.java
+++ src/main/java/org/docx4j/openpackaging/contenttype/ContentTypeManager.java
@@ -228,7 +228,6 @@ public class ContentTypeManager  {
 		overrideCT.setContentType(contentType );
 		
 		overrideContentType.put(partUri, overrideCT);
-		
 	}
 
 //	public String getOverrideContentType(URI partUri) {
