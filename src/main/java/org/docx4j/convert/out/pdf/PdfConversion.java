d7bac52725506e68015f181f156042d5511c4902
Jason Harrop <jason@plutext.org>
2012-12-19 15:19:38 +1100

    Javadoc

--- src/main/java/org/docx4j/convert/out/pdf/PdfConversion.java
+++ src/main/java/org/docx4j/convert/out/pdf/PdfConversion.java
@@ -10,15 +10,18 @@ import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 /**
  * There are 3 ways a package can be converted to PDF:
  * 
- * 1. via HTML, using docX2HTML.xslt
+ * 1. via XSL FO
+ * 1a - using XSLT to generate the XSL FO
+ * 1b - using TraversalUtils to generate the XSL FO
  * 
- * 2. via XSL FO
+ * 2. via HTML, using docX2HTML.xslt
  * 
  * 3. via iText
  * 
- * Since none of these methods produce perfect results (yet),
- * all three are provided.  You can use (and extend) the technique
- * you are most comfortable with.
+ * Method 1a is the standard way of doing things
+ * Method 1b is experimental (Dec 2012)
+ * 
+ * Methods 2 & 3 are in docx4j extras
  * 
  * @author jharrop
  *
