94c5f3e4ae1bb7990f8dad4b36ef0f5b0c70e273
Jason Harrop <jason@plutext.org>
2013-07-27 10:26:35 +1000

    Improved field diagnostic output

--- src/samples/docx4j/org/docx4j/samples/FieldsDiagnostics.java
+++ src/samples/docx4j/org/docx4j/samples/FieldsDiagnostics.java
@@ -1,5 +1,6 @@
 package org.docx4j.samples;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,17 +14,20 @@ import org.docx4j.model.fields.FieldsPreprocessor;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.JaxbXmlPart;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
 import org.docx4j.openpackaging.parts.relationships.Namespaces;
 import org.docx4j.openpackaging.parts.relationships.RelationshipsPart;
 import org.docx4j.relationships.Relationship;
 import org.docx4j.wml.ContentAccessor;
 import org.docx4j.wml.P;
+import org.docx4j.wml.Text;
 
 /**
  * List all field instructions found in docx (main document part,
  * headers, footers).
  * 
- * Doesn't list nested fields eg PAGEREF in results part of a TOC field.
+ * Shortcoming: Doesn't handle fields which cross paragraph boundaries eg TOC field,
+ * 
  *
  * @author jharrop
  *
@@ -37,16 +41,46 @@ public class FieldsDiagnostics {
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
+		
+		// A docx or a dir containing docx files
+		String inputpath = System.getProperty("user.dir") + "/dd/";
 
-		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(
-				new java.io.File(
-						System.getProperty("user.dir") + "/Test_TOC.docx"));
+		StringBuilder sb = new StringBuilder(); 
+		
+		File dir = new File(inputpath);
+		
+		if (dir.isDirectory()) {
+			
+			String[] files = dir.list();
+			
+			for (int i = 0; i<files.length; i++  ) {
+				
+				if (files[i].endsWith("docx")) {
+					sb.append("\n\n" + files[i] + "\n");
+					WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new java.io.File(inputpath + "/" + files[i]));		
+					pkgReport(wordMLPackage, sb);
+				}
+			}
+			
+		} else if (inputpath.endsWith("docx")) {
+			sb.append("\n\n" + inputpath + "\n");
+			WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new java.io.File(inputpath ));		
+			pkgReport(wordMLPackage, sb);			
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
+	
+	private static void pkgReport(WordprocessingMLPackage wordMLPackage, StringBuilder sb) throws Docx4JException {
 
+
+		
 		FieldsPreprocessor.complexifyFields(wordMLPackage.getMainDocumentPart() );
 
-		System.out.println("\n" + wordMLPackage.getMainDocumentPart().getPartName() + "\n");
-		performOnInstance(wordMLPackage.getMainDocumentPart().getPartName().getName(), 
-				wordMLPackage.getMainDocumentPart().getContent() );
+//		System.out.println("\n" + wordMLPackage.getMainDocumentPart().getPartName() + "\n");
+		listFieldsInPart(wordMLPackage.getMainDocumentPart().getPartName().getName(), 
+				wordMLPackage.getMainDocumentPart().getContent(), sb );
 
 		{ // Headers, footers
 
@@ -61,69 +95,56 @@ public class FieldsDiagnostics {
 					FieldsPreprocessor.complexifyFields(part );
 
 					System.out.println("\n" + part.getPartName() + "\n");
-					performOnInstance(part.getPartName().getName(),
-							((ContentAccessor)part).getContent() );
+					listFieldsInPart(part.getPartName().getName(),
+							((ContentAccessor)part).getContent(), sb );
 
 				}
 			}
 
 
 		}
+		
 
 	}
 
-	private static void performOnInstance(String partName, List<Object> contentList) throws Docx4JException {
-
-		StringBuilder sb = new StringBuilder(); 
+	private static void listFieldsInPart(String partName, List<Object> contentList, StringBuilder sb) throws Docx4JException {
 
 		// find fields
 		ComplexFieldLocator fl = new ComplexFieldLocator();
 		new TraversalUtil(contentList, fl);
-		sb.append("In " + partName + " there are " + fl.getStarts().size() + " paragraphs containing the following fields: ");
+		
+		if (fl.getStarts().size()==0) {
+			sb.append("\n\n" + partName + ": no fields detected.");
+			return;
+		}
 
+		sb.append("\n\n" + partName + ": " + fl.getStarts().size() + " paragraphs containing the following fields: ");
 
 		// canonicalise and setup fieldRefs
 		List<FieldRef> fieldRefs = new ArrayList<FieldRef>();
 		for( P p : fl.getStarts() ) {
-			int index;
-			P newP;
-			if (p.getParent() instanceof ContentAccessor) {
-				index = ((ContentAccessor)p.getParent()).getContent().indexOf(p);
-				newP = FieldsPreprocessor.canonicalise(p, fieldRefs);
-//				log.debug("NewP length: " + newP.getContent().size() );
-				((ContentAccessor)p.getParent()).getContent().set(index, newP);
-			} else if (p.getParent() instanceof java.util.List) {
-				index = ((java.util.List)p.getParent()).indexOf(p);
-				newP = FieldsPreprocessor.canonicalise(p, fieldRefs);
-//				log.debug("NewP length: " + newP.getContent().size() );
-//				((java.util.List)p.getParent()).set(index, newP);
-			} else {
-				throw new Docx4JException ("Unexpected parent: " + p.getParent().getClass().getName() );
-			}
-			
-			System.out.println(index+ "  " + XmlUtils.marshaltoString(p, true, true));
-			System.out.println("RESULT  " + XmlUtils.marshaltoString(newP, true, true));
+			FieldsPreprocessor.canonicalise(p, fieldRefs);
 		}
 
 		// Populate
 		for (FieldRef fr : fieldRefs) {
-
-			String instr = fr.getFldName();
-			sb.append("\n" + instr);
-			
 			recurse(sb, fr, "    ");
 		}
 
-		System.out.println(sb.toString());
 	}
 	
 	private static void recurse(StringBuilder sb, FieldRef fr, String indent) {
 		
 		for(Object o : fr.getInstructions() ) {
 			if (o instanceof FieldRef) {
-				sb.append("\n" + indent +  ((FieldRef)o).getFldName());
 				recurse(sb, ((FieldRef)o), indent + "    ");
-				
+			} else {
+				o = XmlUtils.unwrap(o);
+				if (o instanceof Text) {
+					sb.append("\n" + indent + ((Text)o).getValue() );									
+				} else {
+					sb.append("\n" + indent + XmlUtils.unwrap(o).getClass().getName() );
+				}
 			}
 		}
 		
