2f11ef9a214fb1f824746cdf3b2e5751d7e088df
Jason Harrop <jason@plutext.org>
2013-08-17 20:12:18 +1000

    Alberto contrib 2013 08 16 - -	Setting up/call to the ConversionHTMLStyleElementHandler and ConversionHTMLScriptElementHandler.

--- src/main/java/org/docx4j/convert/out/html/HTMLConversionContext.java
+++ src/main/java/org/docx4j/convert/out/html/HTMLConversionContext.java
@@ -19,24 +19,62 @@
  */
 package org.docx4j.convert.out.html;
 
-import org.docx4j.convert.out.AbstractWmlConversionContext;
 import org.docx4j.convert.out.AbstractConversionSettings;
-import org.docx4j.convert.out.AbstractModelRegistry;
-import org.docx4j.convert.out.ConversionSectionWrappers;
+import org.docx4j.convert.out.ConversionHTMLScriptElementHandler;
+import org.docx4j.convert.out.ConversionHTMLStyleElementHandler;
+import org.docx4j.convert.out.HTMLSettings;
+import org.docx4j.convert.out.common.AbstractModelRegistry;
+import org.docx4j.convert.out.common.AbstractWmlConversionContext;
+import org.docx4j.convert.out.common.ConversionSectionWrappers;
 import org.docx4j.convert.out.common.writer.AbstractMessageWriter;
-import org.docx4j.convert.out.html.AbstractHtmlExporter.HtmlSettings;
 import org.docx4j.fonts.Mapper;
 import org.docx4j.model.images.ConversionImageHandler;
+import org.docx4j.openpackaging.packages.OpcPackage;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * See /docs/developer/Convert_Out.docx for an overview of
  * the design.
  * 
- * @author Alberto Zerolo
+ * @author Alberto
  *
  */
 public class HTMLConversionContext extends AbstractWmlConversionContext {
-	protected Mapper fontMapper = null;
+	protected Mapper fontMapper;
+	protected ConversionHTMLStyleElementHandler styleElementHandler;
+	protected ConversionHTMLScriptElementHandler scriptElementHandler;
+	protected String userCSS;
+	protected String userScript;
+	protected String userBodyTop;
+	protected String userBodyTail;
+	
+	protected static final ConversionHTMLStyleElementHandler DEFAULT_STYLE_ELEMENT_HANDLER = new ConversionHTMLStyleElementHandler() {
+		@Override
+		public Element createStyleElement(OpcPackage opcPackage, Document document, String styleDefinition) {
+			Element ret = null;
+			if ((styleDefinition != null) && (styleDefinition.length() > 0)) {
+				ret = document.createElement("style");
+				ret.appendChild(document.createComment(styleDefinition));
+			}
+			return ret;
+		}
+	};
+
+	protected static final ConversionHTMLScriptElementHandler DEFAULT_SCRIPT_ELEMENT_HANDLER = new ConversionHTMLScriptElementHandler() {
+		
+		@Override
+		public Element createScriptElement(OpcPackage opcPackage, Document document, String scriptDefinition) {
+			Element ret = null;
+			if ((scriptDefinition != null) && (scriptDefinition.length() > 0)) {
+				ret = document.createElement("script");
+				ret.setAttribute("type", "text/javascript");
+				ret.appendChild(document.createComment(scriptDefinition));
+			}
+			return ret;
+		}
+	};
 	
 	//The model registry is per output type a singleton
 	protected static final AbstractModelRegistry HTML_MODEL_REGISTRY = 
@@ -45,6 +83,7 @@ public class HTMLConversionContext extends AbstractWmlConversionContext {
 			protected void registerDefaultConverterInstances() {
 				registerConverter(new TableWriter());
 				registerConverter(new SymbolWriter());
+				registerConverter(new BrWriter());
 				registerConverter(new FldSimpleWriter());
 				registerConverter(new BookmarkStartWriter());
 				registerConverter(new HyperlinkWriter());
@@ -65,30 +104,42 @@ public class HTMLConversionContext extends AbstractWmlConversionContext {
 			}
 		};		
 	
-	public HTMLConversionContext(HtmlSettings settings, ConversionSectionWrappers conversionSectionWrappers) {
-		super(HTML_MODEL_REGISTRY, HTML_MESSAGE_WRITER, settings, conversionSectionWrappers);
+	public HTMLConversionContext(HTMLSettings settings, WordprocessingMLPackage preprocessedPackage, ConversionSectionWrappers conversionSectionWrappers) {
+		super(HTML_MODEL_REGISTRY, HTML_MESSAGE_WRITER, settings, preprocessedPackage, conversionSectionWrappers);
 	}
 
 	@Override
-	protected void initializeSettings(AbstractConversionSettings settings) {
-	HtmlSettings htmlSettings = null;
+	protected void initializeSettings(AbstractConversionSettings settings, OpcPackage localOpcPackage) {
+	HTMLSettings htmlSettings = null;
 		if (settings == null) {
-			settings = new HtmlSettings();
+			settings = new HTMLSettings();
 		}
-		else if (!(settings instanceof HtmlSettings)) {
+		else if (!(settings instanceof HTMLSettings)) {
 			throw new IllegalArgumentException("The class of the settings isn't HtmlSettings, it is " + settings.getClass().getName());
 		}
-		htmlSettings = (HtmlSettings)settings;
-		super.initializeSettings(htmlSettings);
+		htmlSettings = (HTMLSettings)settings;
+		super.initializeSettings(htmlSettings, localOpcPackage);
 		fontMapper = htmlSettings.getFontMapper();
 		if (fontMapper == null) {
 			fontMapper = getWmlPackage().getFontMapper();
+		}
+		userCSS = htmlSettings.getUserCSS();
+		userScript = htmlSettings.getUserScript();
+		userBodyTop = htmlSettings.getUserBodyTop();
+		userBodyTail = htmlSettings.getUserBodyTail();
+		styleElementHandler = htmlSettings.getStyleElementHandler();
+		if (styleElementHandler == null) {
+			styleElementHandler = DEFAULT_STYLE_ELEMENT_HANDLER;
+		}
+		scriptElementHandler = htmlSettings.getScriptElementHandler();
+		if (scriptElementHandler == null) {
+			scriptElementHandler = DEFAULT_SCRIPT_ELEMENT_HANDLER;
 		}
 	}
 
 	@Override
 	protected ConversionImageHandler initializeImageHandler(AbstractConversionSettings settings, ConversionImageHandler handler) {
-	HtmlSettings htmlSettings = (HtmlSettings)settings;
+	HTMLSettings htmlSettings = (HTMLSettings)settings;
 		if (handler == null) {
 			handler = new HTMLConversionImageHandler(
 						htmlSettings.getImageDirPath(), 
@@ -100,5 +151,29 @@ public class HTMLConversionContext extends AbstractWmlConversionContext {
 
 	public Mapper getFontMapper() {
 		return fontMapper;
+	}
+	
+	public Element createStyleElement(Document document, String styleDefinition) {
+		return styleElementHandler.createStyleElement(getWmlPackage(), document, styleDefinition);
+	}
+	
+	public Element createScriptElement(Document document, String scriptDefinition) {
+		return scriptElementHandler.createScriptElement(getWmlPackage(), document, scriptDefinition);
+	}
+	
+	public String getUserCSS() {
+		return userCSS;
+	}
+	
+	public String getUserScript() {
+		return userScript;
+	}
+	
+	public String getUserBodyTop() {
+		return userBodyTop;
+	}
+	
+	public String getUserBodyTail() {
+		return userBodyTail;
 	}
 }
