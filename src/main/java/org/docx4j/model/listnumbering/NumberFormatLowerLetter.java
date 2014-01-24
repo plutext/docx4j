076ad9a08321cb5337fb4013188f50831bd6fce0
Anton Reshetnikov <batonez@gmail.com>
2014-01-24 16:31:06 +0400

    Fixed lower letter numbering format

--- src/main/java/org/docx4j/model/listnumbering/NumberFormatLowerLetter.java
+++ src/main/java/org/docx4j/model/listnumbering/NumberFormatLowerLetter.java
@@ -3,35 +3,15 @@ package org.docx4j.model.listnumbering;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NumberFormatLowerLetter extends NumberFormat {
-	
+public class NumberFormatLowerLetter extends NumberFormat
+{
 	protected static Logger log = LoggerFactory.getLogger(NumberFormatLowerLetter.class);
 	
-	public String format( int in ) {
+	public String format( int in )
+	{
+		char result = Character.forDigit(in + 9, 36);
+		log.debug(in + " --> " + result);
 		
-		String str = Integer.toString(in);
-		StringBuilder out = new StringBuilder();
-		
-		for (int i = 0; i < str.length(); i++) {
-			int dig = Character.digit(str.charAt(i), 36);
-			char cdig = Character.forDigit(dig + 9, 36);
-			log.debug(str.charAt(i) + " --> " + cdig);
-			out.append(cdig);
-		}		
-		return out.toString();		
+		return Character.toString(result);		
 	}
-	
-	  public static void main (String [] args)
-	   {
-		   
-		  NumberFormatLowerLetter format = new NumberFormatLowerLetter();
-		  
-		  format.format(1);
-		  format.format(2);
-		  log.debug(format.format(3));
-		  
-		  
-		  
-	   }
-
 }
