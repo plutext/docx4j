1b455d77dd5d32af603d31c6143117d239016528
Jason Harrop <jason@plutext.org>
2014-07-21 13:27:55 +1000

    get|set WordCompatSetting

--- src/main/java/org/docx4j/openpackaging/parts/WordprocessingML/DocumentSettingsPart.java
+++ src/main/java/org/docx4j/openpackaging/parts/WordprocessingML/DocumentSettingsPart.java
@@ -24,6 +24,8 @@ import javax.xml.bind.annotation.XmlElement;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.docx4j.jaxb.Context;
+import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
 import org.docx4j.openpackaging.parts.JaxbXmlPart;
 import org.docx4j.openpackaging.parts.JaxbXmlPartXPathAware;
@@ -34,6 +36,8 @@ import org.docx4j.w14.CTLongHexNumber;
 import org.docx4j.w14.CTOnOff;
 import org.docx4j.w15.CTGuid;
 import org.docx4j.wml.BooleanDefaultTrue;
+import org.docx4j.wml.CTCompat;
+import org.docx4j.wml.CTCompatSetting;
 import org.docx4j.wml.CTSettings;
 
 
@@ -98,5 +102,61 @@ public final class DocumentSettingsPart extends JaxbXmlPartXPathAware<CTSettings
 		
 		this.jaxbElement.setIgnorable(mceIgnorableVal);
     }
+	
+	/**
+	 * Get a compatibility setting in the Word namespace, by name
+	 * @param name
+	 * @return
+	 * @throws Docx4JException
+	 */
+	public CTCompatSetting getWordCompatSetting(String name) throws Docx4JException {
+	
+		CTCompat compat = this.getContents().getCompat();
+		if (compat==null) {
+			log.warn("No w:settings/w:compat element");
+			return null;
+		}
+		/* w:name="overrideTableStyleFontSizeAndJustification" 
+		 * w:uri="http://schemas.microsoft.com/office/word" 
+		 * w:val="1"
+		 */
+		CTCompatSetting theSetting = null;
+		for (CTCompatSetting setting : compat.getCompatSetting() ) {
+			if (setting.getUri().equals("http://schemas.microsoft.com/office/word")
+					&& setting.getName().equals(name)) {
+				theSetting = setting;
+				break;
+			}
+		}
+		
+		return theSetting;
+	}
 
+	public void setWordCompatSetting(String name, String val) throws Docx4JException {
+		
+		CTCompat compat = this.getContents().getCompat();
+		if (compat==null) {
+			log.debug("No w:settings/w:compat element; creating..");
+		}
+		compat = Context.getWmlObjectFactory().createCTCompat();
+		this.getContents().setCompat(compat);
+		
+		CTCompatSetting theSetting = null;
+		for (CTCompatSetting setting : compat.getCompatSetting() ) {
+			if (setting.getUri().equals("http://schemas.microsoft.com/office/word")
+					&& setting.getName().equals(name)) {
+				theSetting = setting;
+				break;
+			}
+		}
+		
+		if (theSetting==null) {
+			theSetting = Context.getWmlObjectFactory().createCTCompatSetting();
+			theSetting.setUri("http://schemas.microsoft.com/office/word");
+			theSetting.setName(name);
+			compat.getCompatSetting().add(theSetting);
+		}
+		theSetting.setVal(val);
+	}
+	
 }
