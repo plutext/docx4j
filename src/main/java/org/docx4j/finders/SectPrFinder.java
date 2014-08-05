ff84cf01ef43afdf973f60b62b24da817913d088
Jason Harrop <jason@plutext.org>
2014-08-05 10:16:38 +1000

    SectPrFinder:  should return the body level sectPr at the end of the list, so new method to that effect. DocumentModel: use SectPrFinder to find the sectPr (which means it will now find sectPr inside content controls)

--- src/main/java/org/docx4j/finders/SectPrFinder.java
+++ src/main/java/org/docx4j/finders/SectPrFinder.java
@@ -10,30 +10,90 @@ import org.docx4j.wml.SectPr;
 
 public class SectPrFinder extends CallbackImpl {
 	
+	/*
+	 * There can be a body level sectPr.
+	 * 
+	 * Other sectPr are typically in a top level paragraph.
+	 * 
+	 * They can also be in a rich text content control.
+	 * 
+	 * (Word won't let you copy/paste a sectPr into a table cell.
+	 *  And if you put one there in the XML, on opening, Word will
+	 *  display the table, but gets a bit confused - it may show it
+	 *  twice the second time on a separate page.  On save, the 
+	 *  sectPr will be retained.  If you put the sectPr in a
+	 *  content control in a tc, the behaviour is similar.  )
+	 *  
+	 * If you put a sectPr in a text box, Word can open the
+	 * docx, and shows it as a page break.   But trying to 
+	 * do that from within the Word UI results in:
+	 * "You cannot put section breaks into a header, footer,
+	 *  footnote, endnote, comment, text box, callout, or macro."
+	 * 
+	 * Based on the above, this finder looks for the sectPr
+	 * only:
+	 * - body level sectPr
+	 * - top level P
+	 * - inside a p in a content control (but not in a 
+	 *   table)  
+	 */
+	
+	
 	private List<SectPr> sectPrList = new ArrayList<SectPr>(); 
 	
+	private SectPr bodyLevel = null;
+	
 	public SectPrFinder(MainDocumentPart mdp) {
+		
 		// Handle the body level one
 		if (mdp.getJaxbElement().getBody().getSectPr()!=null) {
-			sectPrList.add(mdp.getJaxbElement().getBody().getSectPr());
+			bodyLevel = mdp.getJaxbElement().getBody().getSectPr();
 		}
+		
 	}
 	
 	private SectPrFinder(){};
 	
 	/**
+	 * Note that the body level sectPr (if there is one)
+	 * is at the start of the list.
+	 * 
 	 * @return the sectPrList
 	 */
+	@Deprecated
 	public List<SectPr> getSectPrList() {
+		
+		if (bodyLevel!=null ) {
+			sectPrList.remove(bodyLevel); 		
+			sectPrList.add(0, bodyLevel);
+		}
+		
 		return sectPrList;
 	}
 
+	/**
+	 * The body level sectPr (if there is one)
+	 * is at the end of the list.
+	 * 
+	 * @return the sectPrList
+	 */
+	public List<SectPr> getOrderedSectPrList() {
+		
+		if (bodyLevel!=null ) {
+			sectPrList.remove(bodyLevel); // in case user has previously called this method or the deprecated getSectPrList()			
+			sectPrList.add(bodyLevel);
+		}
+		
+		return sectPrList;
+	}
+	
 	@Override
 	public List<Object> apply(Object o) {
 		
 		if (o instanceof P) {
 			P p = (P)o;
 			if ( p.getPPr()!=null && p.getPPr().getSectPr()!=null ) {
+				System.out.println("Found sectpr!");
 				sectPrList.add(p.getPPr().getSectPr());
 			}
 		}
@@ -41,7 +101,11 @@ public class SectPrFinder extends CallbackImpl {
 	}
 	
 	public boolean shouldTraverse(Object o) {
-		return !(o instanceof P); 
+		
+		System.out.println(o.getClass().getName() );
+		
+		return !(o instanceof P
+				|| o instanceof org.docx4j.wml.Tbl); 
 	}
 	
 }
