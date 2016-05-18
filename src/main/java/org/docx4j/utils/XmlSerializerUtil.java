7b82da23dde500dbac5d51cf5d297968f4a7905a
Jason Harrop <jason@plutext.org>
2016-05-18 16:18:35 +1000

    Support pretty printing

--- src/main/java/org/docx4j/utils/XmlSerializerUtil.java
+++ src/main/java/org/docx4j/utils/XmlSerializerUtil.java
@@ -11,6 +11,22 @@ public class XmlSerializerUtil {
 	public static void serialize(Source source, Result result,
 			boolean omit_xml_declaration, boolean method_xml) throws Docx4JException {
 		
+		serialize( source, result, omit_xml_declaration,  method_xml, false);
+	}
+
+	/**
+	 * @param source
+	 * @param result
+	 * @param omit_xml_declaration
+	 * @param method_xml
+	 * @param indent
+	 * @throws Docx4JException
+	 * 
+	 * @since 3.3.1
+	 */
+	public static void serialize(Source source, Result result,
+			boolean omit_xml_declaration, boolean method_xml, boolean indent) throws Docx4JException {
+		
 		// Xalan <= 2.7.2 can't handle astral characters: https://issues.apache.org/jira/browse/XALANJ-2419
 		// but org.docx4j.org.apache.xalan repackaging is patched to fix this
 		
@@ -23,13 +39,16 @@ public class XmlSerializerUtil {
 			if (method_xml) {
 				serializer.setOutputProperty(javax.xml.transform.OutputKeys.METHOD, "xml");	// probably the default anyway?			
 			}
-			//serializer.setOutputProperty(javax.xml.transform.OutputKeys.INDENT, "yes");
-			
+			if (indent) {
+				serializer.setOutputProperty(javax.xml.transform.OutputKeys.INDENT, "yes");
+				// actual indent amount is set in org/docx4j/org/apache/xml/serializer/docx4j_xalan_output_xml.properties 
+			}
+						
 			serializer.transform( source, result );	
 			
 		} catch (Exception e) {
 			throw new Docx4JException("Exception writing Document to OutputStream: " + e.getMessage(), e);
 		}
 	}
-
+	
 }
