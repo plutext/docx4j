41bef8692526170301060960528dd0dd0a2bc986
roy@vanderkuil.nl <roy@vanderkuil.nl>
2017-04-21 15:29:20 +0200

    fix possible exception on logging

--- src/main/java/org/docx4j/model/fields/FieldRef.java
+++ src/main/java/org/docx4j/model/fields/FieldRef.java
@@ -18,12 +18,12 @@ import org.slf4j.LoggerFactory;
  * The objective of this class is to represent a complex field
  * (containing nested fields, if any; nested fields are 
  * represented by FieldRef object of their own).
- * 
+ *
  * TODO, consider whether to make this abstract, with 
  * differing concrete implementations for top level and nested fields.
- * 
+ *
  * Background. There are simple fields:
- * 
+ *
       <w:fldSimple w:instr=" DATE ">
         <w:r>
           <w:rPr>
@@ -32,9 +32,9 @@ import org.slf4j.LoggerFactory;
           <w:instrText>4/12/2011</w:instrText>  <!-- ???? -->
         </w:r>
       </w:fldSimple>
- * 
+ *
  * and there are complex fields:
- * 
+ *
     <w:p>
       <w:r>
         <w:fldChar w:fldCharType="begin"/>
@@ -52,7 +52,7 @@ import org.slf4j.LoggerFactory;
     
     
  * A simple field can also take the complex form:
- * 
+ *
       <w:r>
         <w:fldChar w:fldCharType="begin"/>
         <w:instrText xml:space="preserve">DATE </w:instrText>
@@ -67,13 +67,13 @@ import org.slf4j.LoggerFactory;
 
  * A complex field can contain nested fields, in either
  * its instruction part, or result part.
- * 
+ *
  * we need to represent nest fields in the instructions
  * part only (since nested fields in the results part
  * get re-generated).
- * 
+ *
  * An example of a nested field in the instructions part:
- * 
+ *
     <w:p>
       <w:r>
         <w:fldChar w:fldCharType="begin"/>
@@ -107,7 +107,7 @@ import org.slf4j.LoggerFactory;
     </w:p>
     
  * An example of nested fields in the results part:
- * 
+ *
  *     <w:p w:rsidR="00BA0732" w:rsidRDefault="00BA0732">
       <w:pPr>
         <w:pStyle w:val="TOC1"/>
@@ -258,244 +258,243 @@ import org.slf4j.LoggerFactory;
         <w:fldChar w:fldCharType="end"/>
       </w:r>
     </w:p>
- * 
+ *
  * In general, you can "canonicalise" the field representation
  * to be 
  * (i)   instructions, contained within a single run 
  * (ii)  results, immediately following, though not nec just as following siblings
  * (iii) the final <w:fldChar w:fldCharType="end"/>
- * 
+ *
  * Since the purpose of our field support is to update the field 
  * results, we can delete (ii) and (iii) before adding them in again.
- * 
- * The document is preprocessed to put it into this form. 
- * 
+ *
+ * The document is preprocessed to put it into this form.
+ *
  * @author jharrop
  *
  */
 public class FieldRef {
-	
-	private static Logger log = LoggerFactory.getLogger(FieldRef.class);			
-	
-	public FieldRef(FldChar fldCharBegin) {
-		this.fldCharBegin = fldCharBegin;
-	}
-	
-	private FldChar fldCharBegin;
-	
-	protected String fldName = null;
-	/**
-	 * The name of the (outer most) field, for example DATE, MERGEFIELD.
-	 * 
-	 * Assume for now that this is contained in instructions.get(0).
-	 * 
+
+    private static Logger log = LoggerFactory.getLogger(FieldRef.class);
+
+    public FieldRef(FldChar fldCharBegin) {
+        this.fldCharBegin = fldCharBegin;
+    }
+
+    private FldChar fldCharBegin;
+
+    protected String fldName = null;
+    /**
+     * The name of the (outer most) field, for example DATE, MERGEFIELD.
+     *
+     * Assume for now that this is contained in instructions.get(0).
+     *
 	 * @see <a href="http://webapp.docx4java.org/OnlineDemo/ecma376/WordML/file_2.html">field syntax</a>
-	 * @return
-	 */
-	public String getFldName() {
-		Object o = XmlUtils.unwrap(instructions.get(0));
-		if (o instanceof Text) {
+     * @return
+     */
+    public String getFldName() {
+        Object o = XmlUtils.unwrap(instructions.get(0));
+        if (o instanceof Text) {
 			return FormattingSwitchHelper.getFldSimpleName( ((Text)o).getValue() );
-		} else {
-			log.error("TODO: extract field name from " + o.getClass().getName() );
-			if (o instanceof FieldRef) {
-				// contains a nested field?!
-				FieldRef nested = (FieldRef)o;
-				log.error("Nested field " + nested.getFldName() );				
-				
-			} else {
-                if(log.isErrorEnabled()) {
-                    log.error(XmlUtils.marshaltoString(instructions.get(0), true, true));
-                }
-			}
-			return null;
-		}
-	}	
-	
-	private ContentAccessor parent;
-	public ContentAccessor getParent() {
-		return parent;
-	}
-	public void setParent(ContentAccessor parent) {
-		this.parent = parent;
-	}
+        } else {
+            log.error("TODO: extract field name from " + o.getClass().getName());
+            //if (o instanceof FieldRef) {
+            //    // contains a nested field?!
+            //    FieldRef nested = (FieldRef) o;
+            //    log.error("Nested field " + nested.getFldName());
+
+            //} else {
+            if(log.isErrorEnabled()) {
+                log.error(XmlUtils.marshaltoString(instructions.get(0), true, true));
+            }
+            return null;
+        }
+    }
+
+    private ContentAccessor parent;
+    public ContentAccessor getParent() {
+        return parent;
+    }
+    public void setParent(ContentAccessor parent) {
+        this.parent = parent;
+    }
 
 	
-	/**
-	 * The run 
-	 * 
+    /**
+     * The run
+     *
 	 *       <w:r>
                 <w:fldChar w:fldCharType="begin"/>
                 <w:instrText xml:space="preserve"> ... </w:instrText>
                 <w:fldChar w:fldCharType="separate"/>
             </w:r>
             
-	 */
-	private R beginRun;
-	public R getBeginRun() {
-		return beginRun;
-	}
-	public void setBeginRun(R beginRun) {
-		this.beginRun = beginRun;
-	}
+     */
+    private R beginRun;
+    public R getBeginRun() {
+        return beginRun;
+    }
+    public void setBeginRun(R beginRun) {
+        this.beginRun = beginRun;
+    }
 
 	private boolean seenSeparate=false; 
-	public boolean haveSeenSeparate() {
-		return seenSeparate;
-	}
-	public void setSeenSeparate(boolean seenSeparate) {
-		this.seenSeparate = seenSeparate;
-	}
-	
-	private void processFldBegin() {
-		formFieldProperties = fldCharBegin.getFfData();
-		customFieldData = fldCharBegin.getFldData();
-		dirty = fldCharBegin.isDirty();
-		lock = fldCharBegin.isFldLock();
-	}
-	
-	private Boolean mergeFormat;
-	/**
-	 * @return whether \* MERGEFORMAT is set
-	 */
-	public Boolean isMergeFormat() {
-		
+    public boolean haveSeenSeparate() {
+        return seenSeparate;
+    }
+    public void setSeenSeparate(boolean seenSeparate) {
+        this.seenSeparate = seenSeparate;
+    }
+
+    private void processFldBegin() {
+        formFieldProperties = fldCharBegin.getFfData();
+        customFieldData = fldCharBegin.getFldData();
+        dirty = fldCharBegin.isDirty();
+        lock = fldCharBegin.isFldLock();
+    }
+
+    private Boolean mergeFormat;
+    /**
+     * @return whether \* MERGEFORMAT is set
+     */
+    public Boolean isMergeFormat() {
+
 		if (mergeFormat==null) {
-			//Work it out. Assume for now that this is contained in instructions.get(0).
-			mergeFormat = Boolean.FALSE;
-			Object o = XmlUtils.unwrap(instructions.get(0));
-			if (o instanceof Text) {
+            //Work it out. Assume for now that this is contained in instructions.get(0).
+            mergeFormat = Boolean.FALSE;
+            Object o = XmlUtils.unwrap(instructions.get(0));
+            if (o instanceof Text) {
 				String instr = ((Text)o).getValue();
-				if (instr.contains("MERGEFORMAT")) {
-					mergeFormat = Boolean.TRUE;
-				}
-			} else {
-                if(log.isErrorEnabled()) {
-                    log.error("TODO: extract field name from " + o.getClass().getName());
-                    log.error(XmlUtils.marshaltoString(instructions.get(0), true, true));
+                if (instr.contains("MERGEFORMAT")) {
+                    mergeFormat = Boolean.TRUE;
                 }
-			}
+            } else {
+                if(log.isErrorEnabled()) {
+                log.error("TODO: extract field name from " + o.getClass().getName());
+                log.error(XmlUtils.marshaltoString(instructions.get(0), true, true));
+            }
+        }
 		}
-		return mergeFormat;
-	}
+        return mergeFormat;
+    }
 
-	private boolean dirty;
+    private boolean dirty;
 
-	/**
+    /**
 	 * Specifies that this field has been flagged by an application to indicate that its current results
 	 * are invalid (stale) due to other modifications made to the document, and these contents should be 
 	 * updated before they are displayed.
-	 * @return whether stale
+     * @return whether stale
 	 * @see <a href="http://webapp.docx4java.org/OnlineDemo/ecma376/WordML/fldChar.html">the spec</a>
-	 */
-	public boolean isDirty() {
-		return dirty;
-	}
-	/**
-	 * @param whether stale
+     */
+    public boolean isDirty() {
+        return dirty;
+    }
+    /**
+     * @param whether stale
 	 * @see <a href="http://webapp.docx4java.org/OnlineDemo/ecma376/WordML/fldChar.html">the spec</a>
-	 */
-	public void setDirty(boolean dirty) {
-		this.dirty = dirty;
-		fldCharBegin.setDirty(dirty);
-		
-		// Note that this doesn't set dirty on any nested fields.  TODO: Consider whether it should.
-	}
+     */
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
+        fldCharBegin.setDirty(dirty);
 
-	private boolean lock;
-	
-	/**
-	 * @return the lock
+        // Note that this doesn't set dirty on any nested fields.  TODO: Consider whether it should.
+    }
+
+    private boolean lock;
+
+    /**
+     * @return the lock
 	 * @see <a href="http://webapp.docx4java.org/OnlineDemo/ecma376/WordML/fldChar.html">the spec</a>
-	 */
-	public boolean isLock() {
-		return lock;
-	}
-	/**
+     */
+    public boolean isLock() {
+        return lock;
+    }
+    /**
 	 * Specifies that the parent complex field shall not have its field result recalculated, even if 
 	 * an application attempts to recalculate the results of all fields in the document or a 
 	 * recalculation is explicitly requested.
-	 * 
-	 * @param lock the lock to set
+     *
+     * @param lock the lock to set
 	 * @see <a href="http://webapp.docx4java.org/OnlineDemo/ecma376/WordML/fldChar.html">the spec</a>
-	 */
-	public void setLock(boolean lock) {
-		this.lock = lock;
-		fldCharBegin.setFldLock(lock);
-	}
+     */
+    public void setLock(boolean lock) {
+        this.lock = lock;
+        fldCharBegin.setFldLock(lock);
+    }
 
-	private Text customFieldData;
-	
-	/**
-	 * application-specific data associated with this field.
-	 * 
-	 * @return the customFieldData
+    private Text customFieldData;
+
+    /**
+     * application-specific data associated with this field.
+     *
+     * @return the customFieldData
 	 * @see <a href="http://webapp.docx4java.org/OnlineDemo/ecma376/WordML/fldData_1.html">the spec</a>
-	 */
-	public Text getCustomFieldData() {
-		return customFieldData;
-	}
-	/**
-	 * @param customFieldData the customFieldData to set
+     */
+    public Text getCustomFieldData() {
+        return customFieldData;
+    }
+    /**
+     * @param customFieldData the customFieldData to set
 	 * @see <a href="http://webapp.docx4java.org/OnlineDemo/ecma376/WordML/fldData_1.html">the spec</a>
-	 */
-	public void setCustomFieldData(Text customFieldData) {
-		this.customFieldData = customFieldData;
-		fldCharBegin.setFldData(customFieldData);
-	}
+     */
+    public void setCustomFieldData(Text customFieldData) {
+        this.customFieldData = customFieldData;
+        fldCharBegin.setFldData(customFieldData);
+    }
 
-	private CTFFData formFieldProperties;
+    private CTFFData formFieldProperties;
 
-	/**
-	 * Properties specific to FORMCHECKBOX, FORMDROPDOWN, FORMTEXT
-	 * 
-	 * @return the formFieldProperties
+    /**
+     * Properties specific to FORMCHECKBOX, FORMDROPDOWN, FORMTEXT
+     *
+     * @return the formFieldProperties
 	 * @see <a href="http://webapp.docx4java.org/OnlineDemo/ecma376/WordML/ffData.html">the spec</a>
-	 */
-	public CTFFData getFormFieldProperties() {
-		return formFieldProperties;
-	}
-	/**
-	 * @param formFieldProperties the formFieldProperties to set
+     */
+    public CTFFData getFormFieldProperties() {
+        return formFieldProperties;
+    }
+    /**
+     * @param formFieldProperties the formFieldProperties to set
 	 * @see <a href="http://webapp.docx4java.org/OnlineDemo/ecma376/WordML/ffData.html">the spec</a>
-	 */
-	public void setFormFieldProperties(CTFFData formFieldProperties) {
-		this.formFieldProperties = formFieldProperties;
-		fldCharBegin.setFfData(formFieldProperties);
-	}
+     */
+    public void setFormFieldProperties(CTFFData formFieldProperties) {
+        this.formFieldProperties = formFieldProperties;
+        fldCharBegin.setFfData(formFieldProperties);
+    }
 
-	/**
-	 * The run 
-	 * 
+    /**
+     * The run
+     *
             <w:r>
                 <w:fldChar w:fldCharType="end"/>
             </w:r>
             
         Store a reference to it so we can delete it.
-	 */
-	private R endRun;
-	public R getEndRun() {
-		return endRun;
-	}
-	public void setEndRun(R endRun) {
-		this.endRun = endRun;
-	}
+     */
+    private R endRun;
+    public R getEndRun() {
+        return endRun;
+    }
+    public void setEndRun(R endRun) {
+        this.endRun = endRun;
+    }
+
 	
-	
-	/**
+    /**
 	 * A list of the content between the outermost w:fldChar begin and separate elements;
 	 * in the simplest case, this will be a single w:instrText object;
 	 * in a more general case it will be a mixture of w:instrText and FieldRef objects
 	 * (and possibly other things such as w:br).
-	 */
-	private List<Object> instructions = new ArrayList<Object>();
-	/**
-	 * @return the instructions
-	 */
-	public List<Object> getInstructions() {
-		return instructions;
-	}
-	
+     */
+    private List<Object> instructions = new ArrayList<Object>();
+    /**
+     * @return the instructions
+     */
+    public List<Object> getInstructions() {
+        return instructions;
+    }
+
 //	private JAXBElement<Text> instrText;
 //	public String getInstr() {
 //		return instrText.getValue().getValue();
@@ -511,38 +510,38 @@ public class FieldRef {
 //	}
 
 
-	private R resultsSlot;
-	public R getResultsSlot() {
-		return resultsSlot;
-	}
-	public void setResultsSlot(R resultsSlot) {
-		this.resultsSlot = resultsSlot;
-	}
-	
-	public void setResult(String val) {
-		
-		resultsSlot.getContent().clear();
-		StringTokenizer st = new StringTokenizer(val, "\n\r\f"); // tokenize on the newline character, the carriage-return character, and the form-feed character
-		
-		// our docfrag may contain several runs
-		boolean firsttoken = true;
-		while (st.hasMoreTokens()) {						
-			String line = (String) st.nextToken();
-			
-			if (firsttoken) {
-				firsttoken = false;
-			} else {
-				resultsSlot.getContent().add(Context.getWmlObjectFactory().createBr());
-			}
-			
-			org.docx4j.wml.Text text = Context.getWmlObjectFactory().createText();
-			resultsSlot.getContent().add(text);
+    private R resultsSlot;
+    public R getResultsSlot() {
+        return resultsSlot;
+    }
+    public void setResultsSlot(R resultsSlot) {
+        this.resultsSlot = resultsSlot;
+    }
+
+    public void setResult(String val) {
+
+        resultsSlot.getContent().clear();
+        StringTokenizer st = new StringTokenizer(val, "\n\r\f"); // tokenize on the newline character, the carriage-return character, and the form-feed character
+
+        // our docfrag may contain several runs
+        boolean firsttoken = true;
+        while (st.hasMoreTokens()) {
+            String line = (String) st.nextToken();
+
+            if (firsttoken) {
+                firsttoken = false;
+            } else {
+                resultsSlot.getContent().add(Context.getWmlObjectFactory().createBr());
+            }
+
+            org.docx4j.wml.Text text = Context.getWmlObjectFactory().createText();
+            resultsSlot.getContent().add(text);
 			if (line.startsWith(" ") || line.endsWith(" ") ) {
-				// TODO: tab character?
-				text.setSpace("preserve");
-			}
-			text.setValue(line);
-		}
-	}
+                // TODO: tab character?
+                text.setSpace("preserve");
+            }
+            text.setValue(line);
+        }
+    }
 
 }
