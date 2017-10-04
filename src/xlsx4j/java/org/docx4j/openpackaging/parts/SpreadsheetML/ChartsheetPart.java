4bd467d59205d6ecd230bd925cd305f8b6a68868
Jason <jason@plutext.org>
2017-10-04 12:46:25 +0100

    Chartsheetpart: detect on load; remove extraneous methods

--- src/xlsx4j/java/org/docx4j/openpackaging/parts/SpreadsheetML/ChartsheetPart.java
+++ src/xlsx4j/java/org/docx4j/openpackaging/parts/SpreadsheetML/ChartsheetPart.java
@@ -1,9 +1,6 @@
 package org.docx4j.openpackaging.parts.SpreadsheetML;
 
-import javax.xml.bind.JAXBException;
-
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
-import org.docx4j.openpackaging.packages.SpreadsheetMLPackage;
 import org.docx4j.openpackaging.parts.PartName;
 import org.docx4j.openpackaging.parts.relationships.Namespaces;
 import org.xlsx4j.sml.CTChartsheet;
@@ -31,38 +28,4 @@ public class ChartsheetPart extends JaxbSmlPart<CTChartsheet> {
 		
 	}
 	
-	@Override
-	public void setJaxbElement(CTChartsheet jaxbElement) {
-		super.setJaxbElement(jaxbElement);
-		jaxbElement.setParent(this); // if you create a new ChartsheetPart
-	}
-	
-	@Override
-    public CTChartsheet unmarshal( java.io.InputStream is ) throws JAXBException {
-		
-		CTChartsheet w = super.unmarshal(is);
-		w.setParent(this); // workaround for JAXB in Java 8 setting parent to JAXBElement!
-		return w;
-	}
-	
-	@Override
-    public CTChartsheet unmarshal(org.w3c.dom.Element el) throws JAXBException {
-
-		CTChartsheet w = super.unmarshal(el);
-		w.setParent(this); // presume JAXB gets in wrong here too
-		return w;
-		
-	}
-	
-	/**
-	 * Get the WorkbookPart.
-	 * 
-	 * @return
-	 * @since 3.3.3
-	 */
-	public WorkbookPart getWorkbookPart() {
-		
-		return ((SpreadsheetMLPackage)this.getPackage()).getWorkbookPart();
-		
-	}
 }
