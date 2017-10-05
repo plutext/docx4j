de14b445c31f4f9719c614515f1ac917dd4c228f
Jason Harrop <jason@plutext.org>
2017-10-05 08:18:30 +0100

    Add @XmlRootElement

--- src/main/java/org/docx4j/dml/chart/x2007/CTInvertSolidFillFmt.java
+++ src/main/java/org/docx4j/dml/chart/x2007/CTInvertSolidFillFmt.java
@@ -4,6 +4,7 @@ package org.docx4j.dml.chart.x2007;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import org.docx4j.dml.CTShapeProperties;
 
@@ -31,6 +32,7 @@ import org.docx4j.dml.CTShapeProperties;
 @XmlType(name = "CT_InvertSolidFillFmt", propOrder = {
     "spPr"
 })
+@XmlRootElement(name = "invertSolidFillFmt")
 public class CTInvertSolidFillFmt {
 
     @XmlElement(required = true)
