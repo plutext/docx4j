0b3c92b823ea4573977b3f70dc0d3fd6d5226a00
Jason Harrop <jason@plutext.org>
2017-10-06 16:59:58 +0100

    package-info

---
+++ src/main/java/org/docx4j/dml/chart/x2007/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.microsoft.com/office/drawing/2007/8/2/chart", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.docx4j.dml.chart.x2007;
