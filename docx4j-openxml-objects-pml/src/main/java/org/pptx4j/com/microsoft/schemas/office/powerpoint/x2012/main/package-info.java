d6d602397d3efb9a1a99eaab5f512ec5a0a259cf
Jason Harrop <jason@plutext.org>
2019-04-30 09:06:06 +1000

    Generate [MS-PPTX] classes

---
+++ docx4j-openxml-objects-pml/src/main/java/org/pptx4j/com/microsoft/schemas/office/powerpoint/x2012/main/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.microsoft.com/office/powerpoint/2012/main", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.pptx4j.com.microsoft.schemas.office.powerpoint.x2012.main;
