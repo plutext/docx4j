2633378adfdd053ff8164102e7875ca831345237
Jason Harrop <jason@plutext.org>
2019-06-02 18:02:57 +1000

    working module-info for export fo

--- docx4j-export-fo/src/main/java/module-info.java
+++ docx4j-export-fo/src/main/java/module-info.java
@@ -3,9 +3,12 @@ module org.docx4j.export_fo {
 	requires org.slf4j;
 	requires org.docx4j.core;
 	requires org.docx4j.openxml_objects;
+	requires jaxb.xslfo;
 
-	// TODO ADD fop, batik etc
-	
+	requires fop;
+	requires avalon.framework.api;
+	requires avalon.framework.impl;
+		
 	exports org.docx4j.convert.out.fo;
 	exports org.docx4j.convert.out.fo.renderers;
 	exports org.docx4j.convert.out.pdf;
