9b450761f386f6b8d50013f83452181ede92c956
Jason Harrop <jason@plutext.org>
2019-08-04 13:58:39 +1000

    fixes #361

--- docx4j-openxml-objects-sml/src/main/java/module-info.java
+++ docx4j-openxml-objects-sml/src/main/java/module-info.java
@@ -7,5 +7,11 @@ module org.docx4j.openxml_objects_sml {
     exports org.xlsx4j.sml;
     exports org.xlsx4j.schemas.microsoft.com.office.excel_2006.main;
     exports org.xlsx4j.schemas.microsoft.com.office.excel_2008_2.main;
+    exports org.xlsx4j.schemas.microsoft.com.office.excel.x2010.spreadsheetDrawing;
 
+    opens org.xlsx4j.sml;
+    opens org.xlsx4j.schemas.microsoft.com.office.excel_2006.main;
+    opens org.xlsx4j.schemas.microsoft.com.office.excel_2008_2.main;
+    opens org.xlsx4j.schemas.microsoft.com.office.excel.x2010.spreadsheetDrawing;
+    
 }
