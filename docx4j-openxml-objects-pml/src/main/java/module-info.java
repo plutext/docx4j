298068b6f44676020ac8b9a1a1fe67756daf4149
Jason Harrop <jason@plutext.org>
2019-08-04 13:54:16 +1000

    widen opens

--- docx4j-openxml-objects-pml/src/main/java/module-info.java
+++ docx4j-openxml-objects-pml/src/main/java/module-info.java
@@ -19,19 +19,20 @@ module org.docx4j.openxml_objects_pml {
     exports org.pptx4j.com.microsoft.schemas.office.powerpoint.x201703.main;
     exports org.pptx4j.com.microsoft.schemas.office.powerpoint.x201804.main;
 
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x201606.main to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2010.main to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2012.main to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2013.main.command to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x201509.main to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x201510.main to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2015.main to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2016.sectionzoom to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2016.slidezoom to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2016.summaryzoom to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x201710.main to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x201703.main to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
-    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x201804.main to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.pml;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x201606.main; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2010.main; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2012.main; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2013.main.command; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x201509.main; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x201510.main; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2015.main; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2016.sectionzoom; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2016.slidezoom; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x2016.summaryzoom; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x201710.main; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x201703.main; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
+    opens org.pptx4j.com.microsoft.schemas.office.powerpoint.x201804.main; //  to java.xml.bind, com.sun.xml.bind, org.eclipse.persistence.moxy, org.eclipse.persistence.core;
     
 
 }
