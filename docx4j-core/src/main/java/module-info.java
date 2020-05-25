db9542682e3da9a9c7004bb98f7f3f0623a06b01
Jason Harrop <jason@plutext.org>
2020-05-25 17:54:59 +1000

    prepare for 11.1.7 release

--- docx4j-core/src/main/java/module-info.java
+++ docx4j-core/src/main/java/module-info.java
@@ -57,7 +57,6 @@ module org.docx4j.core {
     exports org.docx4j.fonts;
     exports org.docx4j.fonts.fop;
     exports org.docx4j.fonts.fop.apps;
-    exports org.docx4j.fonts.fop.configuration;
     exports org.docx4j.fonts.fop.fonts;
     exports org.docx4j.fonts.fop.fonts.apps;
     exports org.docx4j.fonts.fop.fonts.autodetect;
