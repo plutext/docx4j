9e3633b47bee3376920606c3e9f403160e7497bd
Jason Harrop <jason@plutext.org>
2020-09-04 09:58:22 +1000

    Integrate PDF conversion via Microsoft Graph

---
+++ docx4j-core/src/main/java/org/docx4j/convert/out/MicrosoftGraphConversionSettings.java
@@ -0,0 +1,32 @@
+/**
+ *  Copyright 2020, Plutext Pty Ltd.
+ *   
+ *  This file is part of docx4j.
+
+    docx4j is licensed under the Apache License, Version 2.0 (the "License"); 
+    you may not use this file except in compliance with the License. 
+
+    You may obtain a copy of the License at 
+
+        http://www.apache.org/licenses/LICENSE-2.0 
+
+    Unless required by applicable law or agreed to in writing, software 
+    distributed under the License is distributed on an "AS IS" BASIS, 
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+    See the License for the specific language governing permissions and 
+    limitations under the License.
+
+ **/
+package org.docx4j.convert.out;
+
+import org.docx4j.convert.out.AbstractConversionSettings;
+
+/**
+ * Mechanism by which docx, pptx or xlsx package is passed to the converter.
+ * 
+ * @author jharrop
+ *
+ */
+public class MicrosoftGraphConversionSettings extends AbstractConversionSettings {
+
+}
