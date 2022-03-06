b5bf33d2648c672426a25ec038d81e5fb854a5be
Jason Harrop <jason@plutext.org>
2022-03-06 17:59:01 +1100

    Comment re FOP substitutions

--- docx4j-samples-docx-export-fo/src/main/java/org/docx4j/samples/ConvertOutPDFviaXSLFO.java
+++ docx4j-samples-docx-export-fo/src/main/java/org/docx4j/samples/ConvertOutPDFviaXSLFO.java
@@ -166,6 +166,10 @@ public class ConvertOutPDFviaXSLFO {
 //			fontMapper.put("Arial", font);
 //		}
 //		fontMapper.put("Libian SC Regular", PhysicalFonts.get("SimSun"));
+		
+		// Alternatively, use https://xmlgraphics.apache.org/fop/2.1/fonts.html#font_substitution
+		// Use docx4j property docx4j.fonts.fop.util.FopConfigUtil.substitutions to provide
+		// a file name (on your class path) containing these definitions 
 
 		// Config - step 1
 		// FO exporter setup (required)
