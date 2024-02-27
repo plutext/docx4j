b5f0963d347133914ee4f972a6ac3f0da06fcd9a
Jason Harrop <jason@plutext.org>
2024-02-28 10:23:54 +1100

    Merge relevant changes to docx4j-core/src from VERSION_11_4_10 (added files)

---
+++ docx4j-core/src/main/java/org/docx4j/openpackaging/parts/DrawingML/ChartColorStylePart.java
@@ -0,0 +1,33 @@
+package org.docx4j.openpackaging.parts.DrawingML;
+
+import org.docx4j.openpackaging.exceptions.InvalidFormatException;
+import org.docx4j.openpackaging.parts.JaxbXmlPart;
+import org.docx4j.openpackaging.parts.PartName;
+import org.docx4j.openpackaging.parts.relationships.Namespaces;
+
+/**
+ * @since 11.4.10
+ */
+ public class ChartColorStylePart<E> extends JaxbXmlPart<E> {
+
+	public ChartColorStylePart(PartName partName) throws InvalidFormatException {
+		super(partName);
+		init();
+	}
+
+	private ChartColorStylePart() throws InvalidFormatException {
+		super(new PartName(null));
+		init();
+	}
+	
+	public void init() {
+				
+		// Used if this Part is added to [Content_Types].xml 
+		setContentType(new  org.docx4j.openpackaging.contenttype.ContentType( 
+				org.docx4j.openpackaging.contenttype.ContentTypes.CHART_COLOR_STYLE));
+
+		// Used when this Part is added to a rels 
+		setRelationshipType(Namespaces.CHART_COLOR_STYLE);
+		
+	}	
+}
