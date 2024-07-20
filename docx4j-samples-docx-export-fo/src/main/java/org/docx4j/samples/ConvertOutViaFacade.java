a0181d60d9de82966dadd51183080de5d5e070b1
Jason Harrop <jason@plutext.org>
2024-07-21 08:42:18 +1000

    to use Docx4J.toFO instead, uncomment

--- docx4j-samples-docx-export-fo/src/main/java/org/docx4j/samples/ConvertOutViaFacade.java
+++ docx4j-samples-docx-export-fo/src/main/java/org/docx4j/samples/ConvertOutViaFacade.java
@@ -23,6 +23,7 @@ package org.docx4j.samples;
 import java.io.OutputStream;
 
 import org.docx4j.Docx4J;
+import org.docx4j.convert.out.FOSettings;
 import org.docx4j.fonts.BestMatchingMapper;
 import org.docx4j.fonts.IdentityPlusMapper;
 
@@ -127,6 +128,19 @@ public class ConvertOutViaFacade {
 
 		Docx4J.toPDF(wordMLPackage, os);
 		
+		// to use Docx4J.toFO instead, uncomment:
+		/*
+			FOSettings foSettings =Docx4J.createFOSettings();
+			foSettings.setOpcPackage(wordMLPackage);
+			
+			// docx4j 8.3.9 and 11.4.9 (?!)	
+			// Not required in 11.4.11 
+//			FopFactoryBuilder fopFactoryBuilder = FORendererApacheFOP.getFopFactoryBuilder(foSettings) ;
+//			FopFactory fopFactory = fopFactoryBuilder.build();
+//			FOUserAgent foUserAgent = FORendererApacheFOP.getFOUserAgent(foSettings, fopFactory);
+			
+			Docx4J.toFO(foSettings, os, Docx4J.FLAG_EXPORT_PREFER_NONXSL);
+		*/
     }
     
 	protected static void getInputFilePath(String[] args) throws IllegalArgumentException {
