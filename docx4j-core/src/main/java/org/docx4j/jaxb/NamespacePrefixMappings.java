64482dec56f3bfc4a25bad318f8b40c227e62a73
Jason Harrop <jason@plutext.org>
2024-11-27 10:25:26 +1100

    Add xmlns:w16sdtfl="http://schemas.microsoft.com/office/word/2024/wordml/sdtformatlock"

--- docx4j-core/src/main/java/org/docx4j/jaxb/NamespacePrefixMappings.java
+++ docx4j-core/src/main/java/org/docx4j/jaxb/NamespacePrefixMappings.java
@@ -247,10 +247,15 @@ public class NamespacePrefixMappings implements NamespaceContext, org.docx4j.org
     	if (namespaceUri.equals("http://schemas.microsoft.com/office/word/2020/wordml/sdtdatahash")) {
     		return "w16sdtdh";
     	}
-
-	if (namespaceUri.equals("http://schemas.microsoft.com/office/word/2023/wordml/word16du")) {
-		return "w16du";
-	}
+    	
+    	if (namespaceUri.equals("http://schemas.microsoft.com/office/word/2023/wordml/word16du")) {
+    		return "w16du";
+    	}
+    	
+    	// since 11.5.1    	
+    	if (namespaceUri.equals("http://schemas.microsoft.com/office/word/2024/wordml/sdtformatlock")) {
+    		return "w16sdtfl";
+    	}
 
     	if (namespaceUri.equals("http://schemas.microsoft.com/office/powerpoint/2010/main")) {
     		return "p14";
@@ -694,6 +699,10 @@ public class NamespacePrefixMappings implements NamespaceContext, org.docx4j.org
 		if (prefix.equals("w16du"))
 			return "http://schemas.microsoft.com/office/word/2023/wordml/word16du";
 
+		// since 11.5.1
+		if (prefix.equals("w16sdtfl"))
+			return "http://schemas.microsoft.com/office/word/2024/wordml/sdtformatlock";		
+		
 		if (prefix.equals("p14"))
 			return "http://schemas.microsoft.com/office/powerpoint/2010/main";
 
