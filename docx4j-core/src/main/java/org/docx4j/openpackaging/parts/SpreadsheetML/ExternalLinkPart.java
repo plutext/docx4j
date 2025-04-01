7d3dbc544aaf992813901d1284c824b50d8f0eb7
Nikolay Vorobyev <n.vorobyev@intechcore.com>
2025-04-01 12:23:35 +0200

    Fixed wrong Generic type for ExternalLinkPart

--- docx4j-core/src/main/java/org/docx4j/openpackaging/parts/SpreadsheetML/ExternalLinkPart.java
+++ docx4j-core/src/main/java/org/docx4j/openpackaging/parts/SpreadsheetML/ExternalLinkPart.java
@@ -1,13 +1,11 @@
 package org.docx4j.openpackaging.parts.SpreadsheetML;
 
-import javax.xml.bind.JAXBElement;
-
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
 import org.docx4j.openpackaging.parts.PartName;
 import org.docx4j.openpackaging.parts.relationships.Namespaces;
 import org.xlsx4j.sml.CTExternalLink;
 
-public class ExternalLinkPart  extends JaxbSmlPart<JAXBElement<CTExternalLink>> {
+public class ExternalLinkPart  extends JaxbSmlPart<CTExternalLink> {
 	
 	
 	public ExternalLinkPart(PartName partName) throws InvalidFormatException {
