17bf2f18caed171a8d6194c6b1207608847f4647
Jason Harrop <jason@plutext.org>
2026-06-25 12:30:06 +1000

    Update license headers to be explicit that these files were contributed under the ASL v2; see https://github.com/plutext/docx4j/pull/676

--- docx4j-xjc-copy/src/main/java/org/docx4j/xjc/copy/Copyable.java
+++ docx4j-xjc-copy/src/main/java/org/docx4j/xjc/copy/Copyable.java
@@ -1,4 +1,17 @@
-/* Copyright © 2026, Oracle and/or its affiliates. */
+/* Copyright © 2026, Oracle and/or its affiliates,
+ * and Contributed under the terms and conditions of Apache License 
+ * Version 2.0 (the "License"), without any additional terms or conditions.
+ * 
+ * The contributor licenses this file to You under the License;
+ * you may not use this file except in compliance with
+ * the License. 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
 package org.docx4j.xjc.copy;
 
 /**
